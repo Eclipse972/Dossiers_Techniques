d919edb4d93794864d5fc11a312e5b259dc426f3
ChristopHe <dossiers.techniques.free.fr>
2017-02-03 00:33:29 +0100

    MAJ butée 5 axes

---
+++ Supports/butee5axes/nomenclature.php
@@ -0,0 +1,13 @@
+<?php // nomenclature de la butée 5 axes
+Ligne_nomenclature( 1, 1, 'socle',			'EN AW - 2017', '', 		'socle');
+Ligne_nomenclature( 2, 1, 'contre embase',	'EN AW - 2017', '', 		'contreembase');
+Ligne_nomenclature( 3, 1, 'module bis',		'EN AW - 2017', '', 		'module');
+Ligne_nomenclature( 4, 1, 'axe',			'EN AW - 2017', '', 		'axe4');
+Ligne_nomenclature( 5, 1, 'vis CHc M4 - 55','EN AW - 2017',	'',			'vis4x55');
+Ligne_nomenclature( 6, 1, 'embase',			'EN AW - 2017',	'commerce', 'embase');
+Ligne_nomenclature( 7, 1, 'intermédiaire',	'EN AW - 2017',	'', 		'intermediaire');
+Ligne_nomenclature( 8, 1, 'bouton',			'EN AW - 2017',	'', 		'bouton');
+Ligne_nomenclature( 9, 1, 'tige filetée',	'EN AW - 2017',	'commerce', 'tige_filetee');
+Ligne_nomenclature(10, 1, 'axe',			'EN AW - 2017',	'', 		'axe10');
+Ligne_nomenclature(11, 1, 'embout',			'EN AW - 2017',	'', 		'embout');
+Ligne_nomenclature(12, 1, 'tige',			'EN AW - 2017',	'', 		'tige');
