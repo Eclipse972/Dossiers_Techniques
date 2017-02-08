5cfdf2a813ac0244e3dfb2b558589dbb30342364
ChristopHe <dossiers.techniques.free.fr>
2017-02-08 01:07:48 +0100

    MAJ étau

---
+++ Supports/etau/creation_menu.php
@@ -0,0 +1,6 @@
+<?php // menu de l'étau
+$this->Ajoute_item('fonctionnement',	'Fonctionnement');
+$this->Ajoute_item('dessin_densemble',	'Dessin d&#145;ensemble');
+$this->Ajoute_item('nomenclature',		'Nomenclature');
+$this->Ajoute_item('eclate',				'&Eacute;clat&eacute;');
+$this->Ajoute_item('CE',					'Classes d&#145;&eacute;quivalence');
---
+++ Supports/etau/nomenclature.php
@@ -0,0 +1,16 @@
+<?php // nomenclature de l'étau
+$this->Ajouter_pièce( 1, 1, 'Mors mobile',							'','', 'mors-mobile');
+$this->Ajouter_pièce( 2, 1, 'Mors fixe',								'','', 'mors-fixe');
+$this->Ajouter_pièce( 3, 1, 'Garniture de mors mobile',			'','', 'garniture-mors-mobile');
+$this->Ajouter_pièce( 4, 4, 'Vis FS M5-20 5-6',						'','', 'vis_FS_M5-20');
+$this->Ajouter_pièce( 5, 1, 'Garniture de mors fixe',				'','', 'garniture-mors-fixe');
+$this->Ajouter_pièce( 6, 1, 'Vis de manoeuvre',						'','', 'vis2manoeuvre');
+$this->Ajouter_pièce( 7, 1, '&Eacute;crou H M12-8',				'','', 'ecrou-H-M12');
+$this->Ajouter_pièce( 8, 1, 'Bague de renfort',						'','', 'bague2renfort');
+$this->Ajouter_pièce( 9, 1, 'Tige de poign&eacute;e',				'','', 'tige2poignee');
+$this->Ajouter_pièce(10, 1, 'Semelle',									'','', 'semelle');
+$this->Ajouter_pièce(11, 2, 'Vis CHc M5-10 - 8.8',					'','', 'vis_CHC_M5-10');
+$this->Ajouter_pièce(12, 2, 'Tige guide',								'','', 'tige_guide');
+$this->Ajouter_pièce(13, 2, 'Vis sans t&ecirc;te HC M4-6',		'','', 'vis_HC_M4-6');
+$this->Ajouter_pièce(14, 1, 'Goupille élastique',					'','', 'goupille_elastique_3x16');
+$this->Ajouter_pièce(15, 2, 'Embout de tige de poign&eacute;e','','', 'embout2poignee',;
