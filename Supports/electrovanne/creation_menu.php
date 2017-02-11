04cde71f11940cbd65f2aef8731cab379ebf4796
ChristopHe <dossiers.techniques.free.fr>
2017-02-11 12:34:55 +0100

    MAJ électrovanne

---
+++ Supports/electrovanne/creation_menu.php
@@ -0,0 +1,7 @@
+<?php // menu de l'électrovanne
+$this->Ajoute_item('MES',					'Mise en situation');
+$this->Ajoute_item('fonctionnement',	'Fonctionnement');
+$this->Ajoute_item('AF',					'Analyse fonctionnelle');
+$this->Ajoute_item('dessin_densemble',	'Dessin d&#145;ensemble');
+$this->Ajoute_item('eclate',				'&Eacute;clat&eacute;');
+$this->Ajoute_item('nomenclature',		'Nomenclature');
