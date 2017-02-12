252c5bc599da37be664f457a68e7c87e361831d1
ChristopHe <dossiers.techniques.free.fr>
2017-02-12 02:20:58 +0100

    correction menu de la pompe à palettes

--- Supports/pompeApalettes/creation_menu.php
+++ Supports/pompeApalettes/creation_menu.php
@@ -1,4 +1,5 @@
 <?php // menu de la pompe à palettes
+$this->Ajoute_item('MES', 'Mise en situation');
 $this->Ajoute_item('dessin_densemble', 'Dessin d&#145;ensemble');
 $this->Ajoute_item('nomenclature', 'Nomenclature');
 $this->Ajoute_item('eclate', '&Eacute;clat&eacute;');
