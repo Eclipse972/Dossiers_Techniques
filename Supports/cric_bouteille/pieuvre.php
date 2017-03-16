c9eafde8ef28b7d837a497ad13545ddac3f8bec4
Eclipse972 <christophe.hervi@gmail.com>
2017-03-16 13:59:13 +0100

    Les fonctions

--- Supports/cric_bouteille/pieuvre.php
+++ Supports/cric_bouteille/pieuvre.php
@@ -1,1 +1,8 @@
 page en construction
+<p><b>FP</b> : Soulever une charge</p>
+<p><b>FC1</b> : Transformer l&apos;&eacute;nergie m&eacute;canique de l&apos;utilisateur</p>
+<p><b>FC2</b> : S&apos;adapter aux dimensions de la charge</p>
+<p><b>FC3</b> : R&eacute;sister aux efforts transmissibles</p>
+<p><b>FC4</b> : &Ecirc;tre facilement manipulable</p>
+<p><b>FC5</b> : &Eacute;viter le levage de charges trop lourdes</p>
+<p><b>FC6</b> : S&apos;adapter au milieu ext&eacute;rieur
