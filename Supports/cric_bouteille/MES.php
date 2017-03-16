ea65c0fcaab5e020e50521aab36144da7fc081a8
Eclipse972 <christophe.hervi@gmail.com>
2017-03-16 14:50:26 +0100

    complément sur la mise en situation

--- Supports/cric_bouteille/MES.php
+++ Supports/cric_bouteille/MES.php
@@ -1,3 +1,5 @@
 <h1>Mise en situation</h1>
 <p>Le cric bouteille est uniquement un appareil de levage, pour effectuer des r&eacute;parations sur un v&eacute;hicule, utiliser des chandelles de s&eacute;curit&eacute;. Il doit &ecirc;tre utilis&eacute; sur une surface plane et dure.</p>
-<?php Ajouter_image('mes.png', 'Mise en situation', 'image_centree');
+<?php Ajouter_image('mes.png', 'Mise en situation');?>
+<p> Il est doté d'une soupape de sécurité en cas de surcharge</p>
+<?php Ajouter_image('cric_reel.png', 'cric réel');
