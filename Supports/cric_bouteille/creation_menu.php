27f3e20e430937c4c8d626f93da6753ed6707d13
Eclipse972 <christophe.hervi@gmail.com>
2017-03-16 12:34:07 +0100

    ajout diagramme pieuvre

--- Supports/cric_bouteille/creation_menu.php
+++ Supports/cric_bouteille/creation_menu.php
@@ -3,6 +3,7 @@ $this->Ajoute_item('MES',				'Mise en situation');
 $this->Ajoute_item('fonctionnement','Fonctionnement');
 $this->Ajoute_sous_item('montee',	'Mont&eacute;e');
 $this->Ajoute_sous_item('descente',	'Descente');
+$this->Ajoute_item('pieuvre',   'Analyse fonctionnelle');
 $this->Ajoute_item('dessin_densemble',	'Dessin d&#145;ensemble');
 $this->Ajoute_item('nomenclature',	'Nomenclature');
 $this->Ajoute_item('eclate',			'&Eacute;clat&eacute;');
