a926a90f49a3c24775e7cfa08eaead5046e97f3e
ChristopHe <dossiers.techniques.free.fr>
2017-03-20 22:43:51 +0100

    MAJ nomenclature du bouton poussoir

--- Supports/BP/nomenclature.php
+++ Supports/BP/nomenclature.php
@@ -1,12 +1,12 @@
 <?php // nomenclature du bouton poussoir
-Ligne_nomenclature( 1, 1, 'Corps',						'','','Corps');
-Ligne_nomenclature( 2, 2, 'Borne',						'','','borne');
-Ligne_nomenclature( 3, 4, 'Vis CS M2-4',				'','','Vis_CS');
-Ligne_nomenclature( 4, 1, 'Fourreau',					'','','Fourreau');
-Ligne_nomenclature( 5, 1, '&Eacute;crou H M12-1',		'','','Ecrou_H');
-Ligne_nomenclature( 6, 1, '&Eacute;crou molet&eacute;',	'','','Ecrou_molete');
-Ligne_nomenclature( 7, 1, 'Ressort',					'','','Ressort');
-Ligne_nomenclature( 8, 1, 'Rondelle de contact',		'','','Rondelle');
-Ligne_nomenclature( 9, 1, 'Cylindre de pouss&eacute;e',	'','','Cylindre');
-Ligne_nomenclature(10, 1, 'Vis FS M2.5-18',				'','','Vis_FS');
-Ligne_nomenclature(11, 1, 'Poussoir',					'','','Poussoir');
+Ligne_nomenclature( 1, 1, 'Corps',							'PF 21 (Bak&eacute;lite)',	'',							'Corps');
+Ligne_nomenclature( 2, 2, 'Borne',							'Cu Zn 15',						'Cadmi&eacute;',			'borne');
+Ligne_nomenclature( 3, 4, 'Vis CS M2-4',					'Cu Zn 39 Pb 2',				'',							'Vis_CS');
+Ligne_nomenclature( 4, 1, 'Fourreau',						'Cu Zn 39 Pb 2',				'Cadmi&eacute;',			'Fourreau');
+Ligne_nomenclature( 5, 1, '&Eacute;crou H M12-1',		'Cu Zn 15',						'Cadmi&eacute;',			'Ecrou_H');
+Ligne_nomenclature( 6, 1, '&Eacute;crou molet&eacute;','Cu Zn 15',					'Cadmi&eacute;',			'Ecrou_molete');
+Ligne_nomenclature( 7, 1, 'Ressort',						'45 Si 8',						'',							'Ressort');
+Ligne_nomenclature( 8, 1, 'Rondelle de contact',		'Cu Zn 15',						'&Eacute;tam&eacute;e',	'Rondelle');
+Ligne_nomenclature( 9, 1, 'Cylindre de pouss&eacute;e','PF 21 (Bak&eacute;lite)','Coll&eacute; dans 11',	'Cylindre');
+Ligne_nomenclature(10, 1, 'Vis FS M2.5-18',				'Cu Zn 39 Pb 2',				'',							'Vis_FS');
+Ligne_nomenclature(11, 1, 'Poussoir',						'S 235 (E24)',					'Chrom&eacute;',			'Poussoir');
