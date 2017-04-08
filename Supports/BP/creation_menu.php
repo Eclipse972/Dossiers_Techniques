fa8e87c15a44723e58a09f68b060a5d2784740b9
ChristopHe <dossiers.techniques.free.fr>
2017-04-08 23:09:03 +0200

    validation w3c du BP

--- Supports/BP/creation_menu.php
+++ Supports/BP/creation_menu.php
@@ -3,4 +3,4 @@ $this->Ajoute_item('MES',				'Mise en situation');
 $this->Ajoute_item('pieuvre',			'Diagramme pieuvre');
 $this->Ajoute_item('dessin_densemble',	'Dessin d&apos;ensemble');
 $this->Ajoute_item('eclate',			'&Eacute;clat&eacute;');
-$this->Ajoute_item('nomenclature',		'Nomenclature');
+$this->Ajoute_item('nomenclature',	'Nomenclature');
