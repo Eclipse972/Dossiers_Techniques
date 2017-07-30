a20b8c75053d887a4bf77ce843166d0c89b2bd73
ChristopHe <dossiers.techniques.free.fr>
2017-07-30 06:53:57 +0200

    rendre l'entête de la nomenclature fixe

--- Vue/nomenclature.php
+++ Vue/nomenclature.php
@@ -1,27 +1,26 @@
 <?php
 function Ligne_nomenclature($repere, $nombre, $designation, $matiere, $observation, $image, $fichier = null, $extension = '.EPRT') {
-	echo "<tr>\n";
-	echo "<td>", $repere, "</td>\n";
+	echo '<tr>',"\n";
+	echo '<td>', $repere, '</td>',"\n";
 
 	if(!isset($fichier)) $fichier = $image;	// si le nom de fichier n'est pas précisé on prend le même que celui de l'image
 	echo '<td>', Lien_image_fichier($image, $fichier, $extension, $designation), '</td>',"\n";	// on ajoute le lien image-fichier
 
-	echo "<td>", $designation;					// désignation
+	echo '<td>', $designation;					// désignation
 	if($nombre > 1) echo ' (x', $nombre, ')';	// si plusieurs exemplaires alors on rajoute la quantité
-	echo "</td>\n";								// on ferme la cellule
+	echo '</td>',"\n";							// on ferme la cellule
 	
-	echo "<td>", $matiere, "</td>\n";		// matière
-	echo "<td>", $observation, "</td>\n";	// observation	
-	echo "</tr>\n\n";						// fin de la ligne
+	echo '<td>', $matiere, '</td>',"\n";		// matière
+	echo '<td>', $observation, '</td>',"\n";	// observation	
+	echo '</tr>',"\n\n";						// fin de la ligne
 }	
 ?>
 
 <h1>Nomenclature</h1>
-<p>Cliquez sur l&apos;image de la pi&egrave;ce pour la t&eacute;l&eacute;charger.</p>
+<p>Cliquez sur l&apos;image de la pi&egrave;ce pour la t&eacute;l&eacute;charger au format eDrawing.</p>
 <p>Attention: les images ne sont pas &agrave; l&apos;&eacute;chelle.</p>
-<div id="nomenclature">
 
-<table>
+<table id="nomenclature">
 
 <thead>
 <tr>
@@ -38,4 +37,3 @@ function Ligne_nomenclature($repere, $nombre, $designation, $matiere, $observati
 </tbody>
 
 </table>
-</div>
