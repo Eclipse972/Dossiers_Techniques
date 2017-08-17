e64566d2693d597d4fb36fc82dcd59558276d532
Eclipse972 <christophe.hervi@gmail.com>
2017-08-18 00:20:02 +0200

    constructeur de l'association image-fichier

--- Modele/classe associations.php
+++ Modele/classe associations.php
@@ -1,2 +1,14 @@
 <?php
+class association_image_fichier {
+var $image;
+var $fichier;
+var $dossier;
+
+function association_image_fichier($dossier, $image, $fichier, $extension) { // constructeur
+	$this->image = Image($dossier, $image);
+	$this->fichier = Fichier($dossier, $fichier, $extension);
+	$this->dossier = $dossier;
+}
+
+}
 ?>
