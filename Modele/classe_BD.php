8df03d2ffb8d8f6f9d2c8a37a7e2fdf0b45b621d
ChristopHe <dossiers.techniques.free.fr>
2017-08-25 00:53:51 +0200

    création de l'objet base de données

---
+++ Modele/classe_BD.php
@@ -0,0 +1,17 @@
+<?pjp
+class base2donnees {
+var $serveur;
+var $login;
+var $mdp;
+var $base;
+var $reponse;
+
+function base2donnees { // constructeur
+	// ouverture et connexion à la base
+	
+}
+// simulation des tables, requếtes et jointures --------------------------------------------------
+// table support
+// table articles
+}
+?>
