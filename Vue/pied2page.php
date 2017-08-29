776a6d4e3e0e4af62048e82153b6d57e56b7252c
ChristopHe <dossiers.techniques.free.fr>
2017-08-29 05:42:27 +0200

    ajout de MySQL dans le pied de page

--- Vue/pied2page.php
+++ Vue/pied2page.php
@@ -3,7 +3,7 @@ et <img src="Vue/images/firefox.png" alt="Firefox">
 - Cod&eacute; avec <img src="Vue/images/geany.png" alt="Geany"> 
 en <img src="Vue/images/html5.png" alt="html5"> 
 et <img src="Vue/images/php4.png" alt="php4"> 
-<!-- coupl&eacute; avec <img src="Vue/images/mysql.png" alt="MySQL"> -->
+coupl&eacute; avec <img src="Vue/images/mysql.png" alt="MySQL">
 et mise en page gr&acirc;ce &agrave; <img src="Vue/images/css3.png" alt="css3">.  
 Code disponible sur <img src="Vue/images/github.png" alt="github">
 </p>
