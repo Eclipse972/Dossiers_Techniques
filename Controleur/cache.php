9ced4f1e1ebd020db39b81d12da6e46eb47be356
ChristopHe <dossiers.techniques.free.fr>
2017-09-10 02:09:03 +0200

    éliminer le code php5 en commentaire

--- Controleur/cache.php
+++ Controleur/cache.php
@@ -1,6 +1,6 @@
 <?php
 function GererCache($script, $cache, $delai = 2) {
-	//  $script sans l'extension .php
+	// $script sans l'extension .php amis avec le chemin d'accès
 	// $cache doit contenir le chemin d'accès sans l'extension. Par exemple  'Vue/listeDsupports(.cache)';
 	// délai en heures
 	$cache .= '.cache';
@@ -13,13 +13,10 @@ function GererCache($script, $cache, $delai = 2) {
 		$page = ob_get_contents(); // copie du contenu du tampon dans une chaîne
 		ob_end_clean(); // effacement du contenu du tampon et arrêt de son fonctionnement
 
-		// file_put_contents($cache, $page) ; // on écrit la chaîne précédemment récupérée ($page) dans un fichier ($cache)
-		// mais cette fonction n'existe en php4 donc cette unique instruction est remplacée par les lignes qui suivent.
 		if($fp = fopen($cache,'w')) {	// voir le manuel pour le paramètre w 
 			$ok = fwrite($fp,$page);
 			fclose($fp);
 		}
-		
-		echo $page ; // affichage de la page
+		echo $page; // affichage de la page
 	}
 }
