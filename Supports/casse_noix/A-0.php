835badb9695dcb2da19a8c87ca5d77ef3f7d5657
Eclipse972 <christophe.hervi@gmail.com>
2018-01-05 20:31:28 +0100

    ajout du casse noix

---
+++ Supports/casse_noix/A-0.php
@@ -0,0 +1,2 @@
+<h1>Diagramme A-0</h1>
+<?php Ajouter_image('diagrammeA0', 'diagramme A-0 du casse noix');
---
+++ Supports/casse_noix/MES.php
@@ -0,0 +1,12 @@
+<h1>Mise en situation</h1>
+<p>Ce syst&egrave;me permet de casser des noix. Pour cela, il suffit de placer une noix sur la r&eacute;hausse (rep&egrave;re 7 voir la nomenclature) et de baisser le levier (rep&egrave;re 2). Le piston (rep&egrave;re 5) va alors descendre et &eacute;craser la noix contre la r&eacute;hausse et la casser.</p>
+
+<div class="colonne">
+<h2>levier lev&eacute;</h2>
+<?php Ajouter_image('levier_leve', 'levier lev&eacute;', null, 'width:300px'); ?>
+</div>
+
+<div class="colonne">
+<h2>levier baiss&eacute;e</h2>
+<?php Ajouter_image('levier_baisse', 'levier baiss&eacute;e', null, 'width:300px'); ?>
+</div>
