39dd7ec100b65d0653e0729a7591632b7768e2fc
Eclipse972 <christophe.hervi@gmail.com>
2018-01-19 08:42:05 +0100

    modification diagrame pieuvre

--- Supports/cric_bouteille/pieuvre.php
+++ Supports/cric_bouteille/pieuvre.php
@@ -1,5 +1,5 @@
-<h1>diagramme pieuvre</h1>
-<?php Ajouter_image('pieuvre', 'Diagramme pieuvre', 'image_centree', 'width:400px');?>
+<h1>Diagramme pieuvre</h1>
+<?php Ajouter_image('pieuvre', 'Diagramme pieuvre');?>
 <p><b>FP</b> : Soulever une charge</p>
 <p><b>FC1</b> : Transformer l&apos;&eacute;nergie m&eacute;canique de l&apos;utilisateur</p>
 <p><b>FC2</b> : S&apos;adapter aux dimensions de la charge</p>
