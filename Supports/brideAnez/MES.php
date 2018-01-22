b171d99b4f8af33d446be4874b26b26990ca46e9
Eclipse972 <christophe.hervi@gmail.com>
2018-01-22 21:53:44 +0100

    ajout de la bride à nez

---
+++ Supports/brideAnez/MES.php
@@ -0,0 +1,19 @@
+<h1>Bo&icirc;tier de raccord rotatif</h1>
+<p>Le bo&icirc;tier de raccord rotatif est un &eacute;l&eacute;ment de l&apos;ensemble raccord rotatif destin&eacute; &agrave; &eacute;quiper une unit&eacute; de distribution d&apos;eau min&eacute;rale.</p>
+<div class="colonne">
+<h2>Unit&eacute; de distribution d&apos;eau</h2>
+<?php Ajouter_image('unite_distribution_eau', 'Unit&eacute; de distribution d&apos;eau', null, 'width:500px'); ?>
+</div>
+<div class="colonne">
+<h2>Raccord rotaatif</h2>
+<?php Ajouter_image('raccord', 'raccord rotatif virtuel', null, 'width:300px'); ?>
+</div>
+<h1>Mise en situation</h1>
+<p>Un montage de bridage permet d&apos;assurer la mise et le maintient en position des bo&icirc;tiers des raccords tournants pour l&apos;ensemble des op&eacute;rations (perçages, taraudages et lamages radiaux).</p>
+<ul>Ce montage est constitu&eacute; de (voir FIG 1)
+<li>Un bras articul&eacute; fix&eacute; sur le montage qui vient en appui sur la pi&egrave;ce.</li>
+<li>Une bride &agrave; nez r&eacute;tractable fix&eacute;e sur le montage.</li>
+</ul>
+<p>L&apos;ensemble est mont&eacute; sur un plateau tournant d&apos;un centre d&apos;usinage 4 axes.</p>
+<p>L&apos;alimentation hydraulique se fait par les tuyaux flexibles.</p>
+<?php Ajouter_image('figure1', 'bride dans son contexte', null, 'width:900px'); ?>
