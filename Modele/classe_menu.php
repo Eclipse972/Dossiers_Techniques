f5021d1c7d7e29e323cc72ca8947b1b91fbaf418
Eclipse972 <christophe.hervi@gmail.com>
2018-02-25 13:58:30 +0100

    ajout d'un test d'erreur

--- Modele/classe_menu.php
+++ Modele/classe_menu.php
@@ -10,6 +10,26 @@ function Menu($support, $item, $sous_item) {	// constructeur
 	$this->sous_item	= $sous_item;
 }
 function Afficher_menu() {
-	
+	$connexionBD	= new base2donnees;
+	$T_items		= $connexionBD->Liste_item($this->ID_support,$this->item); // tableau contenant les items
+	// test de l'existence des items à faire
+	if(!isset($T_items)) {
+		echo 'MENU INEXISTANT';
+		$connexionBD->Fermer();
+		return;		// o sort de la finction
+	}
+	$T_sous_items 	= $connexionBD->Liste_sous_item($this->ID_support,$this->item,$this->sous_item);
+	$connexionBD->Fermer();
+	echo '<ul>',"\n";
+	foreach($T_items as $i => $item) {	// affichage du menu
+		echo '<li>',$item,'</li>',"\n";	// lien
+		if (($i==$this->item) && isset($T_sous_items)) { // si item courant = item sélectionné et sous-menu existe alors affichage du sous-menu
+			echo "\n\t",'<ul>',"\n";
+			foreach($T_sous_items as $sous_item)	echo "\t",'<li>',$sous_item,'</li>',"\n";
+			echo "\t",'</ul>',"\n";
+		}
+	}
+	echo '</ul>',"\n";
+	echo '<a href="index.php">SOMMAIRE</a>',"\n";	
 }
 }
