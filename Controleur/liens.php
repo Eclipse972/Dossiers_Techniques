f7b927799f9aba045a51a0bb5a78870786853bd7
Eclipse972 <christophe.hervi@gmail.com>
2018-02-27 04:36:50 +0100

    ajout d'une fonction

--- Controleur/liens.php
+++ Controleur/liens.php
@@ -1,10 +1,11 @@
 <?php // fonction pour les liens
-function Balise_a($support, $item = null, $sous_item = null) { // l'existence de la page correpondante doit être vérifiée en amont
+function Lien($texte, $support, $item = null, $sous_item = null) { // l'existence de la page correpondante doit être vérifiée en amont
 	$lien = '<a href="index.php?support='.$support;
 	if (isset($item)) {
 		$lien .= '&item='.$item;
 		if (isset($sous_item)) $lien .= '&sous_item='.$sous_item;
 	} // Si l'iteme n'existe pas il n'y a pas de sous-item
-	$lien .= '>';
+	$lien .= '">'.$texte.'</a>';
 	return $lien;
 }
+function Lien_item_selectionne($texte, $support, $item) { return '<a id="item_selectionne" '.substr(Lien($texte, $support, $item), 3); }
