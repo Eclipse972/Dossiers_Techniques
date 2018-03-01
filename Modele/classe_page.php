5e4c9b4c1b967a178a0260dda5b14e8800abf803
Eclipse972 <christophe.hervi@gmail.com>
2018-03-01 23:16:07 +0100

    création de la classe page

--- Modele/classe_page.php
+++ Modele/classe_page.php
@@ -3,6 +3,30 @@ class Page {
 function Page(){ // constructeur
 	
 }
-function Afficher() {
+/* Où ettre cette fonction ?
+ * function Afficher_association($image, $fichier, $extension, $titre) {
+	$page = new Association_image_fichier($this->dossier, $image, $fichier, $extension, $titre);
+	$page->Afficher();
+}
+ */
+function Afficher($script) {
+	switch($script) { // on regarde si script est un mot réservé
+	case 'eclate':
+		//$_SESSION[SUPPORT]->Afficher_eclate();
+		$page = new Eclate($_SESSION[SUPPORT]->dossier, $_SESSION[SUPPORT]->pti_nom, $_SESSION[SUPPORT]->pti_nom);
+		$page->Afficher();
+		break;
+	case 'dessin_densemble':
+		$page = new Dessin_densemble($_SESSION[SUPPORT]->dossier, $_SESSION[SUPPORT]->pti_nom, $_SESSION[SUPPORT]->pti_nom);
+		$page->Afficher();
+		break;
+	case 'nomenclature':
+		include 'Vue/nomenclature.php';
+		break;
+	default:	// ce n'est pas un mot réservé alors on exécute le script s'il existe
+		if (file_exists($_SESSION[SUPPORT]->dossier.$script.'.php'))
+			include $_SESSION[SUPPORT]->dossier.$script.'.php';
+		else include 'Vue/en_construction.php';
+	}
 }
 }
