65ee0a0576b66a508d2a63d5e3f8ad7b054840ac
Eclipse972 <christophe.hervi@gmail.com>
2018-05-03 19:49:58 +0200

    modification pour affichage des gifs

--- Vue/fonctions.php
+++ Vue/fonctions.php
@@ -1,6 +1,6 @@
 <?php // quelques fonctions utiles pour l'affichage. Ces fonctions sont utilisées par les associations image-fichier et certaines pages de certain supports
-function Image($image,$dossier) { // recherche l'image dans le dossier spécifié. Si l'image n'existe pas on renvoie l'image de remplacement
-	return Fichier_existe($image,'.png',$dossier,'Vue/images/pas2photo.png');
+function Image($image,$dossier,$extension = '.png') { // recherche l'image dans le dossier spécifié. Si l'image n'existe pas on renvoie l'image de remplacement
+	return Fichier_existe($image,$extension,$dossier,'Vue/images/pas2photo.png');
 }
 function Fichier($fichier,$extension,$dossier) { // recherche le fichier dans le dossier spécifié. Si le fichier n'existe pas on renvoie un lien vide
 	return Fichier_existe($fichier,$extension,$dossier,'#');
@@ -10,9 +10,9 @@ function Fichier_existe($fichier,$extension,$dossier,$substitution) {
 		$fichier = $substitution; // si le fichier n'existe pas alors on remplace par la substitution
 	return $fichier;
 }
-function Ajouter_image($image, $alt, $class = null, $style = null)	{
-	// ajoute l'image (sans l'extension) du support courant. Cette images se trouve dans le répertoire /images.
-	echo '<img src="', Image($image,$_SESSION[DOSSIER].'images/'),'"';
+function Ajouter_image($image, $alt, $class = null, $style = null, $extension = '.png')	{
+	// ajoute l'image du support courant. Cette image se trouve dans le répertoire /images.
+	echo '<img src="', Image($image, $_SESSION[DOSSIER].'images/', $extension),'"';
 
 	if (isset($class)) echo ' class="', $class, '"'; // class
 
