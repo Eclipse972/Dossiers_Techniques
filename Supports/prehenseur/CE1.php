d831f24b08b340db1613d97763eef5c054f39364
Eclipse972 <christophe.hervi@gmail.com>
2018-05-11 07:39:21 +0200

    ajout des CE du préhenseur

---
+++ Supports/prehenseur/CE1.php
@@ -0,0 +1,3 @@
+<?php
+$CE = new Association_image_fichier($_SESSION[DOSSIER], 'CE1', 'CE1', '.EASM', 'Le b&acirc;ti');
+$CE->Afficher('Dans e-Drawing, cliquez sur l&apos;ic&ocirc;ne <img src="Vue/images/icone_eclater_rassembler.png" alt = "icone"> pour &eacute;clater/rassembler la maquette num&eacute;rique');
---
+++ Supports/prehenseur/CE5.php
@@ -0,0 +1,3 @@
+<?php
+$CE = new Association_image_fichier($_SESSION[DOSSIER], 'CE5', 'CE5', '.EASM', 'un doigts');
+$CE->Afficher('Dans e-Drawing, cliquez sur l&apos;ic&ocirc;ne <img src="Vue/images/icone_eclater_rassembler.png" alt = "icone"> pour &eacute;clater/rassembler la maquette num&eacute;rique');
