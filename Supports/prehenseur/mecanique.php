d1181cbebf395b3a433385d7cb26a02ebbaa8270
Eclipse972 <christophe.hervi@gmail.com>
2018-05-19 01:42:42 +0200

    nomenclature quasi complète

--- Supports/prehenseur/mecanique.php
+++ Supports/prehenseur/mecanique.php
@@ -1,2 +1,5 @@
 <h1>Simulations m&eacute;caniques</h1>
 <p>Courbes des d&eacute;placements obtenues à l&apos;un logiciel de calcul et de simulation pour une vitesse en sortie de tige du piston V = 60mm/s</p>
+<p>Vue du dessus du pr&eacute;henseur en position ferm&eacute;e</p>
+<?php
+Ajouter_image('vue2dessus', 'Vue de dessus du pr&eacute;henseur');
