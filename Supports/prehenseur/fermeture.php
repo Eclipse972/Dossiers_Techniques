8720247847282b5fbbf4f5e4661f8449247080c2
Eclipse972 <christophe.hervi@gmail.com>
2018-05-20 03:13:29 +0200

    animations pour le fonctionnement du préhenseur de culasse

--- Supports/prehenseur/fermeture.php
+++ Supports/prehenseur/fermeture.php
@@ -1,4 +1,4 @@
 <h1>Fermeture</h1>
 <?php  Ajouter_image('fermeture', 'fermeture',null,null,'.gif'); ?>
-<p></p>
-<p></p>
+<p>La tige de v&eacute;rin rentre. La noix monte et entra&icirc;ne les biellettes</p>
+<p>Les biellettes rapprochent les deux bras. Les ressorts se rel&acirc;chent</p>
--- Supports/prehenseur/ouverture.php
+++ Supports/prehenseur/ouverture.php
@@ -1,4 +1,4 @@
 <h1>Ouverture</h1>
 <?php  Ajouter_image('ouverture', 'ouverture',null,null,'.gif'); ?>
-<p></p>
-<p></p>
+<p>La tige de v&eacute;rin sort. La noix descend et pousse les biellettes</p>
+<p>Les biellettes &eacute;cartent les deux bras. Les ressorts se compriment</p>
