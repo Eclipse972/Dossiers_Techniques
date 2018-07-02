bd1ead98905e43e2fba5c6cb85a6a14f599ce21b
Eclipse972 <christophe.hervi@gmail.com>
2018-07-03 01:16:40 +0200

    changement du texte de présentation

--- Vue/listeDsupports.php
+++ Vue/listeDsupports.php
@@ -1,7 +1,7 @@
 <header><p class="font-effect-outline">Liste des dossiers techniques</p></header>
 
 <section>
-	<h1>Cliquez sur l&apos;image ou le nom du support pour ouvrir son dossier technique</h1>
+	<h1>Cliquez sur l&apos;image ou le nom du support pour acc&eacute;der &agrave; son dossier technique</h1>
 	<table>
 	<?php
 	$_SESSION = null; // on détruit le support en cours
