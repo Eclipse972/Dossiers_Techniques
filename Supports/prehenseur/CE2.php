bce236b85d05ae6bfd31530aff830c6ade6d9457
Eclipse972 <christophe.hervi@gmail.com>
2018-07-03 20:46:59 +0200

    suppresion des scripts qui sont remplacé par une intruction dans la BD

--- Supports/prehenseur/CE2.php
+++
@@ -1,3 +0,0 @@
-<?php
-$CE = new Association_image_fichier($_SESSION[DOSSIER], 'CE2', 'CE2', '.EASM', 'La tige de v&eacute;rin');
-$CE->Afficher('Dans e-Drawing, cliquez sur l&apos;ic&ocirc;ne <img src="Vue/images/icone_eclater_rassembler.png" alt = "icone"> pour &eacute;clater/rassembler la maquette num&eacute;rique');
