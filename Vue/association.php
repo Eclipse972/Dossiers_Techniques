07d762e96929678731e6005b71d85b1c35ea21b0
Eclipse972 <christophe.hervi@gmail.com>
2018-07-04 18:47:31 +0200

    correction du nom de la classe

--- Vue/association.php
+++ Vue/association.php
@@ -1,6 +1,6 @@
 <?php
 function Association($image, $fichier, $titre, $commentaire) {
-$association = new image_fichier($_SESSION[DOSSIER], $image, $fichier, $titre);
+$association = new Association_image_fichier($_SESSION[DOSSIER], $image, $fichier, $titre);
 $association->Afficher($commentaire);
 }
 Association($T_instruction['param1'], $T_instruction['param2'], $T_instruction['param3'], $T_instruction['param4']);
