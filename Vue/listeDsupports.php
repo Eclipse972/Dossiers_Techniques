6249a2af1f159844dff0b874b42b3fc10a401e4d
Eclipse972 <christophe.hervi@gmail.com>
2018-07-06 01:21:06 +0200

    suppression du cache pour la liste des support

--- Vue/listeDsupports.php
+++ Vue/listeDsupports.php
@@ -1,9 +1,24 @@
 <section>
-	<h1>Cliquez sur l&apos;image ou le nom du support pour acc&eacute;der &agrave; son dossier technique</h1>
-	<table>
-	<?php
-	$_SESSION = null; // on détruit le support en cours
-	GererCache('Vue/sommaire', 'Vue/listeDsupports');
-	?>
-	</table>
+<h1>Cliquez sur l&apos;image ou le nom du support pour acc&eacute;der &agrave; son dossier technique</h1>
+<table>
+<?php
+function Sommaire($NB_colonne) {
+	$id = 0;
+	$connexionBD = new base2donnees();
+	$ListeDvignettes = $connexionBD->ListeDVignettes();
+	while (isset($ListeDvignettes[$id])) {
+		$No_colonne = $id % $NB_colonne;
+		if($No_colonne==0)	echo "\n\t", '<tr>'; // nouvelle ligne
+		echo "\n\t\t", '<td>', $ListeDvignettes[$id],'</td>';
+		if($No_colonne==$NB_colonne-1) echo "\n\t", '</tr>';	// fin de ligne si dernière colonne atteinte
+		$id++;
+	}
+	// si en sortie on s'arrete sur une colonne autre que la dernière
+	if($No_colonne!=$NB_colonne-1) echo "\n\t", '</tr>', "\n";
+}
+
+$_SESSION = null; // on détruit le support en cours
+Sommaire(6); // tableau avec 6 colonnes
+?>
+</table>
 </section>
