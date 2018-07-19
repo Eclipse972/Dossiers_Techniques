8f8b2c7e93be47b8b4087f9339cf4df45237b812
Eclipse972 <christophe.hervi@gmail.com>
2018-07-19 13:30:03 +0200

    mise en page de la pag enconstruction

--- Vue/en_construction.php
+++ Vue/en_construction.php
@@ -1,3 +1,5 @@
+<div align="center">
 <h1>D&eacute;sol&eacute; !</h1>
-<p>Il semblerai que je n&apos;ai pas cr&eacute;&eacute; cette page.</p>
-<?php echo $_SESSION[IMAGE];
+<p>Il semblerai que je n&apos;ai pas r&eacute;dig&eacute; cette page.</p>
+<?php echo $_SESSION[IMAGE];?>
+</div>
