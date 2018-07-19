d7d9ad4aca5c799b4ac8fd089c6fbfb1ee781305
Eclipse972 <christophe.hervi@gmail.com>
2018-07-19 14:50:26 +0200

    suppression lignes inutiles

--- Vue/image_dessus.php
+++ Vue/image_dessus.php
@@ -1,3 +1,2 @@
 <?php //		titre						texte					image					texte alternatif	au dessus
 Page_image($T_instruction['param1'], $T_instruction['param4'], $T_instruction['param2'], $T_instruction['param3'], true);
-
