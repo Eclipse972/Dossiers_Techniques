fd6b02c582cfd6d2c8472e872452c3f5e88a48aa
Eclipse972 <christophe.hervi@gmail.com>
2018-07-21 04:03:40 +0200

    utilisation de PDO

--- Modele/classe_BD.php
+++ Modele/classe_BD.php
@@ -1,34 +1,49 @@
 <?php
 class base2donnees {
-var $serveur;
-var $identifiant;
-var $mot2passe;
-var $base;
-var $connexion;
-var $base_selectionnee;
+var $BD;
 var $resultat;
 
 function base2donnees() { // constructeur
-	include 'connexion.php';
-	$this->connexion = mysql_connect($this->serveur, $this->identifiant, $this->mot2passe); // ouverture de la base
-	$this->base_selectionnee = mysql_select_db($this->base, $this->connexion); // connexion à la base
+	try
+	{	// On se connecte à MySQL
+		include 'connexion.php'; // script non suivi par git
+	//	$this->BD = new PDO('mysql:host=hote;dbname=base;charset=utf8', 'identifiant', 'mot2passe'); 
+	}
+	catch (Exception $e)
+	{	// En cas d'erreur, on affiche un message et on arrête tout
+		die('Erreur : '.$e->getMessage());
+	}
 }
-function Fermer() { // il est inutile de fermer la BD en dehors des scripts de ce fichier
-	mysql_free_result($this->resultat); // Libère le résultat de la requête de la mémoire
-	mysql_close($this->connexion); // fermeture de la base de données
+
+function Fermer() { $this->resultat->closeCursor(); }	 // Termine le traitement de la requête
+
+function ListeDvignettes() { // seule fonction à utiliser une requête sans paramètre
+	$tableau = null;
+	$this->resultat = $this->BD->query('SELECT ID, nom, pti_nom, dossier FROM Supports ORDER BY pti_nom ASC');
+	while ($ligne = $this->resultat->fetch()) {
+		$image  ='<img src="'.Image($ligne['pti_nom'],'Supports/'.$ligne['dossier'].'/images/').'" alt = "'.$ligne['nom'].'">';
+		$tableau[] = Lien($ligne['nom'].'<br>'.$image, $ligne['ID']);
+	}
+	$this->Fermer();
+	return $tableau;
 }
-function Requete($requete) {
-	$this->resultat = mysql_query($requete);
+/*	**********************************************************************
+	Toutes les fonctions qui suivent font appel à des requêtes paramétrées
+	**********************************************************************
+*/
+function Requete($requete, $T_parametre) { // utilisation des paramètres à venir
+	$this->resultat = $this->BD->prepare($requete);
+	// la liste de paramètres sous forme d'un tableau dans le même ordre que les ? dans la requête
+	$this->resultat->execute($T_parametre);
 }
+
 function Support($id) {
 	$T_du = array ('du ', 'de la ', 'de l&apos;');
 	$T_le = array ('le ', 'la ',	'l&apos;');
-	
-	$this->Requete('SELECT nom, pti_nom, dossier, article_ID 
-					FROM Supports
-					WHERE Supports.ID='.$id);
-	if (mysql_num_rows($this->resultat)>0) {
-		$ligne = mysql_fetch_assoc($this->resultat);
+	$ligne = null;
+	$this->Requete('SELECT nom, pti_nom, dossier, article_ID FROM Supports WHERE Supports.ID= ?', array($id));
+	$ligne = $this->resultat->fetch();
+	if ($ligne != null) { // la ligne est non vide
 		$support[ID]		= $id;
 		$support[PTI_NOM]	= $ligne['pti_nom'];
 		$support[DOSSIER]	= 'Supports/'.$ligne['dossier'].'/';
@@ -38,25 +53,14 @@ function Support($id) {
 	$this->Fermer();
 	return $support;
 }
-function ListeDvignettes() {
-	$tableau = null;
-	$this->Requete('SELECT ID, nom, pti_nom, dossier 
-					FROM Supports 
-					ORDER BY pti_nom ASC');
-	while ($ligne = mysql_fetch_assoc($this->resultat)) {
-		$image  ='<img src="'.Image($ligne['pti_nom'],'Supports/'.$ligne['dossier'].'/images/').'" alt = "'.$ligne['nom'].'">';
-		$tableau[] = Lien($ligne['nom'].'<br>'.$image, $ligne['ID']);
-	}
-	$this->Fermer();
-	return $tableau;
-}
+
 function Nomenclature($support_ID) {
 $tableau = null;
 	$this->Requete('SELECT repere, quantite, Pieces.nom AS nom, formule AS matiere, URL_wiki, observation, fichier, assemblage, dossier
 					FROM Supports, Pieces, Materiaux
-					WHERE Pieces.matiere_ID=Materiaux.ID AND Supports.ID=Pieces.support_ID AND support_ID='.$support_ID.' 
-					ORDER BY repere ASC');
-	while ($ligne = mysql_fetch_assoc($this->resultat)) {
+					WHERE Pieces.matiere_ID=Materiaux.ID AND Supports.ID=Pieces.support_ID AND support_ID= ?
+					ORDER BY repere ASC', array($support_ID));
+	while ($ligne = $this->resultat->fetch()) {
 		$ligne['extension'] = ($ligne['assemblage']>0) ? '.EASM' : '.EPRT'; // la valeur numérique pour l'extension est remplacée par la version texte
 		$tableau[] = new Piece($ligne);
 	}
@@ -65,26 +69,23 @@ $tableau = null;
 }
 // Gestion du menu
 function Page_existe($support, $item, $sous_item) {
-	$this->Requete('SELECT * 
-					FROM Items_menu 
-					WHERE support_ID='.$support.' AND item='.$item.' AND sous_item=' .$sous_item);
-	$test = (mysql_num_rows($this->resultat)>0);
+	$this->resultat = null;
+	$this->Requete('SELECT * FROM Items_menu WHERE support_ID= ? AND item= ? AND sous_item= ?', array($support, $item, $sous_item));
 	$this->Fermer();
-	return $test;
+	return ($this->resultat != null);
 }
 function Script($support, $item, $sous_item) { // nom du script à exécuter
-	$this->Requete('SELECT script, param1, param2, param3, param4 
-					FROM Items_menu 
-					WHERE support_ID='.$support.' AND item='.$item.' AND sous_item=' .$sous_item);
-	$T_reponse = mysql_fetch_assoc($this->resultat); // la réponse est un tableau
+	$this->Requete('SELECT script, param1, param2, param3, param4 FROM Items_menu WHERE support_ID= ? AND item= ? AND sous_item= ?',
+					array($support, $item, $sous_item));
+	$T_reponse = $this->resultat->fetch(); // la réponse est un tableau
 	$this->Fermer();
 	return $T_reponse;
 }
 function Liste_item($support, $item) {	
-	$this->Requete('SELECT texte FROM Items_menu WHERE support_ID='.$support.' AND sous_item=0');
+	$this->Requete('SELECT texte FROM Items_menu WHERE support_ID= ? AND sous_item=0', array($support));
 	$i=1;
 	$tableau = null;
-	while ($ligne = mysql_fetch_assoc($this->resultat)) {
+	while ($ligne = $this->resultat->fetch()) {
 		$tableau[$i] = ($i != $item) ? Lien($ligne['texte'], $support, $i) : Lien_item_selectionne($ligne['texte'], $support, $i);
 		$i++;
 	}
@@ -92,10 +93,10 @@ function Liste_item($support, $item) {
 	return $tableau;
 }
 function Liste_sous_item($support,$item,$sous_item) {
-	$this->Requete('SELECT texte FROM Items_menu WHERE support_ID='.$support.' AND item='.$item.' AND sous_item>0');
+	$this->Requete('SELECT texte FROM Items_menu WHERE support_ID= ? AND item= ? AND sous_item>0', array($support, $item));
 	$i=1;
 	$tableau = null;
-	while ($ligne = mysql_fetch_assoc($this->resultat)) {
+	while ($ligne = $this->resultat->fetch()) {
 		$tableau[$i] = ($i != $sous_item) ? Lien($ligne['texte'], $support, $item, $i) : $ligne['texte'];
 		//	si sous_item est le sélectonné alors lien 									sinon texte seul
 		$i++;
@@ -104,4 +105,3 @@ function Liste_sous_item($support,$item,$sous_item) {
 	return $tableau;
 }
 }
-?>
