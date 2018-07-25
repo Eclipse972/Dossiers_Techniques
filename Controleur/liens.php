99d8443a0637b26752e52241370751a1c42fc387
Eclipse972 <christophe.hervi@gmail.com>
2018-07-25 20:52:28 +0200

    utilisation d'une liste e caractères persmis pour le paramètre p

--- Controleur/liens.php
+++ Controleur/liens.php
@@ -1,22 +1,22 @@
 <?php
-define("origine",	64); // code ascii de @ 
-// ATTENTION: les fonction lien et Extraire_parametres utilisent cette constantE
+$LISTE = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'; // 62 possibilités
 
 function Lien($texte, $support, $item = null, $sous_item = null) { // l'existence de la page correpondante doit être vérifiée en amont
-	$lien = '<a href="index.php?p=';
-	$lien .= chr(origine+$support);
+	global $LISTE;
+	$lien = '<a href="index.php?p='.$LISTE[$support];
 	if (isset($item)) {
-		$lien .= chr(origine+$item);
-		if (isset($sous_item)) $lien .=chr(origine+$sous_item);
-	} // Si l'item n'existe pas il n'y a pas de sous-item
+		$lien .= $LISTE[$item];
+		if (isset($sous_item))	$lien .= $LISTE[$sous_item];
+	}
 	$lien .= '">'.$texte.'</a>';
 	return $lien;
 }
 function Extraire_parametre(&$id, &$item, &$sous_item) { // passage d'argumentS par référence
+	global $LISTE;
 	$param = substr((string) $_GET["p"],0,3);	// le paramètre est converti en nombre chaîne de 3 caractères maxi
-	$id			= (isset($param[0])) ? ord($param[0])-origine : -1; //  aucun support a comme identifiant -1 => liste des supports
-	$item		= (isset($param[1])) ? ord($param[1])-origine : 1;	// 
-	$sous_item	= (isset($param[2])) ? ord($param[2])-origine : 0;
+	$id			= (isset($param[0])) ? strpos($LISTE, $param[0]) : -1; //  aucun support a comme identifiant -1 => liste des supports
+	$item		= (isset($param[1])) ? strpos($LISTE, $param[1]) : 1;
+	$sous_item	= (isset($param[2])) ? strpos($LISTE, $param[2]) : 0;
 }
 
 function Lien_item_selectionne($texte, $support, $item) { return '<a id="item_selectionne" '.substr(Lien($texte, $support, $item), 3); }
