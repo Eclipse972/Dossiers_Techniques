bb2d11f34ffae26091caf8484201a3569f57b807
Eclipse972 <christophe.hervi@gmail.com>
2018-07-25 21:44:03 +0200

    redimensionnement de l'image pour oups.php

--- Vue/oups.php
+++ Vue/oups.php
@@ -1,5 +1,5 @@
-<div align="center">
+<div id="page_image">
 <h1>D&eacute;sol&eacute; !</h1>
-<p>Il semblerai que je n&apos;ai pas r&eacute;dig&eacute; cette page.</p>
+<p align=center>Il semblerai que je n&apos;ai pas r&eacute;dig&eacute; cette page.</p>
 <?php echo $_SESSION[IMAGE];?>
 </div>
