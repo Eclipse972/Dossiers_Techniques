83f0f39b8a47f8856aa7d8d4ee30b287ae47d23a
Eclipse972 <christophe.hervi@gmail.com>
2018-07-28 04:42:20 +0200

    suppression de tirets inutiles

--- Modele/classe_traceur.php
+++ Modele/classe_traceur.php
@@ -10,7 +10,7 @@ public function message($message) { // ajout d'un message dans le rapport
 	$this->rapport[] = $message;
 }
 
-public function lieu($fonction) { // pour affihcer la fonction dans laquelle est lancée le traceur
+public function lieu($fonction) { // pour afficher la fonction dans laquelle est lancée le traceur
 	$this->message('Lieu : '.$fonction.' ----------------------');
 }
 
@@ -23,15 +23,15 @@ public function afficher_tableau($nom, $tableau) {
 	foreach($tableau as $cle => $valeur) {
 		$this->message($cle.' => '.$valeur);
 	}
-	$this->message('fin de tableau '.$nom." ----------------------\n");
+	$this->message('fin de tableau '.$nom."\n\n");
 }
 
 public function afficher_rapport() {
 	echo '<!-- '; // début du bloc de commentaire
 	if (isset($this->rapport)) {
-		echo '--------------------- RAPPORT DU ', date("d/m/Y \à H:i:s"),"--------------------- \n";
+		echo 'RAPPORT DU ', date("d/m/Y \à H:i:s"),"\n";
 		foreach($this->rapport as $ligne) { echo $ligne, "\n"; }
-	} else echo "--------------------- RAPPORT VIERGE ---------------------";
+	} else echo "RAPPORT VIERGE";
 	echo ' -->',"\n";
 }
 }
