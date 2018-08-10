df807004b3d0a98b6f9c520994bac7a4e3032830
Eclipse972 <christophe.hervi@gmail.com>
2018-08-10 02:48:45 +0200

    création du lien avec gestion d'erreur

--- Vue/a_propos.php
+++ Vue/a_propos.php
@@ -5,11 +5,13 @@
 		echo 'D&eacute;sol&eacute;! l&apos;archive n&apos;est pas encore disponible';
 	else {
 		echo '<a href="'.$archive.'">Cliquez ici pour t&eacute;l&eacute;charger l&apos;archive ZIP de la maquette numérique</a>';
-		echo '<p><u>Informations sur la maquette num&eacute;rique</u></p>';
+?>
+<p><u><b>Informations sur la maquette num&eacute;rique</b></u></p>
+<?php
 		echo '<p>N&eacute;ant</p>'; // pour le moment
 	}
 ?>
-<p><u>Liens divers</u></p>
+<p><u><b>Liens divers</b></u></p>
 <?php // aller chercher la liste des liens dans la BD. Notamment le site de l'auteur
 	echo '<p>N&eacute;ant</p>'; // pour le moment
 	echo Lien('Retour au dossier technique '.$_SESSION[DU], $_SESSION[ID]);
