1d5f4acf40f1f36ee2bd0002d8cf9c25a11139e9
Eclipse972 <christophe.hervi@gmail.com>
2018-08-19 20:44:29 +0200

    création de la classe fichier

---
+++ Modele/classe_fichier.php
@@ -0,0 +1,37 @@
+<?php
+class Fichier {
+private $fichier;
+
+public __construct($fichier, $dossier, $substitution = '#') {
+	$this->fichier = (file_exists($dossier.$fichier)) $dossier.$fichier : $substitution;
+}
+
+public Lien($texte) {
+	return '<a href="'.$this->fichier.'">'.$texte.'</a>';
+}
+}
+
+class Image extends Fichier {
+
+public __construct($fichier, $dossier) {
+	parent::__construct($fichier, $dossier, 'Vue/pas2photo.png');
+}
+
+public function Lien($alt, $supplement = '') {
+	return '<img src="'.$this->fichier.' '.$supplement.' alt="'.$alt.'">';
+}
+
+public Page_image($titre, $texte, $alt, $dessus, $hauteur) {
+	$code = '<div id="page_image">'."\n".'<h1>'.$titre.'</h1>'."\n";
+	$texte = ($texte != '') ? '<p>'.$texte.'</p>'."\n" : '';
+	/* Remarques
+		mettre plusieurs paragraphes comme ceci: parag1</p><p>parag2</p><p>parag3
+		mettre du code html: </p>code html<p>. les balises qui entourent le code vont créé 2 paragraphes vides
+	*/
+	$code .= (!$dessus) ? $texte : '';
+	$hauteur = ($hauteur == '') ? 400 : $hauteur;
+	$code .= $this->Lien($alt, 'class="association" style=height:'.$hauteur.'px;');
+	$code .= ($dessus) ? $texte : '';
+	return $code.'</div>',"\n";
+}
+}
