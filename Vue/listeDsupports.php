a7208142ffd316e110885e9abccd937bcc31db02
Eclipse972 <christophe.hervi@gmail.com>
2018-08-21 08:49:55 +0200

    suppression du cache individuel

--- Vue/listeDsupports.php
+++ Vue/listeDsupports.php
@@ -1,27 +1,18 @@
 <?php
 function Gerer_index($NB_colonne) { 
 // l'index ne change pas très souvent. Il sera renouvelé seulement en cas de suppression manuelle sur le serveur
-	global $_BD; // accès à la base de données
-	$cache = 'Vue/cache/index.cache';
-	if(file_exists($cache))
-        readfile($cache); // lecture du cache
-	else { // création du cache
-		$page = ''; // page vide
-		$id = 0;
-		$T_vignettes = $_BD->ListeDVignettes();
-		while (isset($T_vignettes[$id])) {
-			$No_colonne = $id % $NB_colonne;
-			if($No_colonne==0)	$page .= "\t".'<tr>'."\n"; // nouvelle ligne
-			$page .= "\t\t".'<td>'.$T_vignettes[$id].'</td>'."\n";
-			if($No_colonne==$NB_colonne-1) $page .= "\t".'</tr>'."\n";	// fin de ligne si dernière colonne atteinte
-			$id++;
-		}
-		// si en sortie on s'arrete sur une colonne autre que la dernière
-		if($No_colonne!=$NB_colonne-1) $page .= "\t".'</tr>'."\n"; // on termine la ligne
-		$page .= "\t".'<!-- Cache créé le '.date('j/n/Y à G:i:s').' -->'."\n";
-		file_put_contents($cache, $page); // écriture du cache
-		echo $page; // on affiche notre page
-	}
+global $_BD; // accès à la base de données
+$id = 0;
+$T_vignettes = $_BD->ListeDVignettes();
+while (isset($T_vignettes[$id])) {
+	$No_colonne = $id % $NB_colonne;
+	if($No_colonne==0)	echo "\t".'<tr>'."\n"; // nouvelle ligne
+	echo "\t\t".'<td>'.$T_vignettes[$id].'</td>'."\n";
+	if($No_colonne==$NB_colonne-1) echo "\t".'</tr>'."\n";	// fin de ligne si dernière colonne atteinte
+	$id++;
+}
+// si en sortie on s'arrete sur une colonne autre que la dernière
+if($No_colonne!=$NB_colonne-1) echo "\t".'</tr>'."\n"; // on termine la ligne
 }
 ?>
 
@@ -29,8 +20,8 @@ function Gerer_index($NB_colonne) {
 <h1>Cliquez sur l&apos;image ou le nom du support pour acc&eacute;der &agrave; son dossier technique</h1>
 <table>
 <?php
-	$_SESSION = null;	// on détruit le support en cours
-	Gerer_index(6);		// affichage du tableau avec le nombre de colonnes en paramètre
+$_SESSION = null;	// on détruit le support en cours
+Gerer_index(6);		// affichage du tableau avec le nombre de colonnes en paramètre
 ?>
 </table>
 </section>
--- Vue/pageHTML.php
+++ Vue/pageHTML.php
@@ -1,13 +1,3 @@
-<?php // la session contient l'objet support
-$_SESSION->setPosition($_ITEM, $_SOUS_ITEM);
-$cache = 'Vue/cache/'.$_SESSION->Pti_nom().' '.$_SESSION->ID().'-'.$_SESSION->Item().'-'.$_SESSION->Sous_item().'.cache';
-$vie = 2; // durée de vie en heure
-
-if(file_exists($cache) && time()-filemtime($cache) < $vie * 3600)
-	readfile($cache);// le cache est lu s'il existe et qu'il n'est pas trop vieux
-else {
-	ob_start(); // ouverture du tampon
-?>
 <div id="corps">
 <nav>
 <?php	echo $_SESSION->Generer_menu();	?>
@@ -16,14 +6,7 @@ else {
 <section>
 <?php
 	$T_instruction = $_BD->Parametres_script($_SESSION->ID(), $_SESSION->Item(), $_SESSION->Sous_item()); // tableau dans lequel est stocké le script avec ses paramètres
-	include $_SESSION->Script($erreur);
+	include $_SESSION->Script();
 ?>
 </section>
 </div>
-<?php
-	$page = ob_get_contents(); // copie du contenu du tampon
-	ob_end_clean(); // effacement du contenu du tampon et arrêt 
-	$page .= '<!-- Cache créé le '.date('j/n/Y à G:i').' -->'."\n";
-	file_put_contents($cache, $page) ; // on écrit la page comme fichier cache
-	echo $page ; // on affiche notre page qu'il y ai une erreur ou pas
-} // else
