ae88a3a351a8f56bdb7ad2a1b63c9f3c013e8adf
Eclipse972 <christophe.hervi@gmail.com>
2018-09-15 15:20:53 +0200

    ajout du champ code de validation

--- Vue/formulaire.php
+++ Vue/formulaire.php
@@ -4,15 +4,11 @@
 <p>Tous les champs sont obligatoires</p>
 	<form method="post" action="Controleur/traitement_formulaire.php">
 	<p>Nom		<br><input type="text" name="nom"		style="width: 100%;" value="<?=$_SESSION['nom']?>" /></p>
-
 	<p>Courriel	<br><input type="text" name="courriel"	style="width: 100%;" value="<?=$_SESSION['courriel']?>" /></p>
-
 	<p>Objet	<br><input type="text" name="objet"		style="width: 100%;" placeholder="<?=$CONFIG['objet']?>" /></p>
-
-	<p>Message	<br><textarea name="message" rows="6" style="width: 100%;"></textarea></p>
-
-	<!-- CAPTCHA à venir-->
-	<p style="text-align: center;"><input type="submit" value="Envoyer" /></p>
+	<p>Message	<br><textarea name="message" rows="6"	style="width: 100%;"></textarea></p>
+	<p>Code validation	<input type="text" name="code"/></p>
+	<p style="text-align:center;"><input type="submit" value="Envoyer" /></p>
 </form>
 <a href="<?=Parametres_support_courant()?>">Page pr&eacute;c&eacute;dente</a>
 </div>
