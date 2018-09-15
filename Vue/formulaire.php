566cf2bde27caeb081cbea54cf308c599e8a1ec8
Eclipse972 <christophe.hervi@gmail.com>
2018-09-15 18:49:31 +0200

    quelques amélioration de l'aspect du formulaire

--- Vue/formulaire.php
+++ Vue/formulaire.php
@@ -1,19 +1,18 @@
 <section>
-<div id=formulaire>
-<form method="post" action="Controleur/traitement_formulaire.php">
+<form method="post" action="Controleur/traitement_formulaire.php" id=formulaire>
 	<p>Nom		<br><input type="text" name="nom"		value="<?=$_SESSION['nom']?>" /></p>
 	<p>Courriel	<br><input type="text" name="courriel"	value="<?=$_SESSION['courriel']?>" /></p>
 	<p>Objet	<br><input type="text" name="objet"		placeholder="<?=$CONFIG['objet']?>" /></p>
 	<p>Message	<br><textarea name="message" rows="6"></textarea></p>
 	<div id=validation>
 		<p>Validation du formulaire</p>
-		<ul>
-			<li>1er caract&egrave;re: </li>
-			<li>2e caract&egrave;re: </li>
-			<li>3e caract&egrave;re: </li>
-			<li>4e caract&egrave;re: </li>
-			<li>5e caract&egrave;re: </li>
-		</ul>
+		<ol>
+			<li>premier caract&egrave;re du nom</li>
+			<li>deuxi&egrave;me caract&egrave;re: de votre courriel</li>
+			<li>avant dernier caract&egrave;re: de l&apos;objet</li>
+			<li>dernier caract&egrave;re: du message</li>
+			<li>troisi&egrave;me caract&egrave;re: du code de validation</li>
+		</ol>
 		<p>Code	<input type="text" name="code" style="width:100px;" /></p>
 	</div>
 	<p style="text-align:center;">
@@ -21,5 +20,4 @@
 		<a href="<?=Parametres_support_courant()?>">Page pr&eacute;c&eacute;dente</a>
 	</p>
 </form>
-</div>
 </section>
