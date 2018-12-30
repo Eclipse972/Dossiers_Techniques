607b372f23c53193b6c253da2026bb9aaffc1dde
Eclipse972 <christophe.hervi@gmail.com>
2018-12-30 04:57:56 +0100

    utilisation du script contexte

--- Controleur/formulaire.php
+++ Controleur/formulaire.php
@@ -1,21 +1,15 @@
 <?php // controleur du formulaire
+include 'contexte.php';
+
 function Configurer() { // détermine l'objet par défaut du message du formulaire
 $BD = new base2donnees();
 $valideur = new Valideur();
 $_SESSION['validation'] = serialize($valideur);
 $_SESSION['temps'] = time();
 
-if (isset($_SESSION['support'])) {
-	$oSupport = unserialize($_SESSION['support']);
-	if ($oSupport->ID() > 0)
-		$objet = 'la page &laquo;'.$BD->Texte_item($oSupport->ID(), $oSupport->Item(), $oSupport->Sous_item()).'&raquo; '.$oSupport->Du_support();
-	else
-		$objet = 'l&apos;archive ZIP';
-} else	$objet = 'la liste de supports';
-
 return ['css'	=> 'style_page',
 		'logo'	=> '<img src="Vue/images/logo.png" alt="logo">',
 		'titre'	=> 'Formulaire de contact (en construction)',
 		'page'	=> 'formulaire',
-		'objet' => 'Exemple: &agrave; propos de '.$objet];
+		'objet' => 'Exemple: &agrave; propos de '.Contexte()];
 }
