2fe9468a2c7cf2aa45c345c6329b89fe3796438f
Eclipse972 <christophe.hervi@gmail.com>
2018-12-30 05:18:40 +0100

    MAJ date du pied de page

--- Vue/pied2page.php
+++ Vue/pied2page.php
@@ -1,3 +1,3 @@
 <p>Site optimis&eacute; pour <img src="Vue/images/chrome.png" alt="Chrome"> et <img src="Vue/images/firefox.png" alt="Firefox">
 <?php if ($MODE == 'DT')	echo ' - <a href="?f=1">Me contacter</a>'; ?>
- - derni&egrave;re mise à jour: 27 ao&ucirc;t 2018</p>
+ - derni&egrave;re mise à jour: 30 d&eacute;cembre 2018</p>
