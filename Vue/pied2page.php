bd8fa3f0e3b0e84a59578dbae3d1218c664bda79
Eclipse972 <christophe.hervi@gmail.com>
2019-01-17 00:29:19 +0100

    ajout d'un lien vers ma FAQ SW

--- Vue/pied2page.php
+++ Vue/pied2page.php
@@ -1,3 +1,4 @@
 <p>Site optimis&eacute; pour <img src="Vue/images/chrome.png" alt="Chrome"> et <img src="Vue/images/firefox.png" alt="Firefox">
 <?php if ($MODE == 'DT')	echo ' - <a href="?f=1">Me contacter</a>'; ?>
+ - <a target="_blank" href="http://faq.sw.free.fr">Ma FAQ SolidWorks</a>
  - derni&egrave;re mise à jour: 30 d&eacute;cembre 2018</p>
