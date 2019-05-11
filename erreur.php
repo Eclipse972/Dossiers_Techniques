42404edb10a22ed6449790b8240caf518e84b4fd
Eclipse972 <christophe.hervi@gmail.com>
2019-05-12 01:00:23 +0200

    création de la page d'erreur et ses conséquences

--- erreur.php
+++ erreur.php
@@ -1,61 +1,52 @@
 <?php
 /*********************************************************************************************************************************** 
-	contrôleur principal
+	page d'erreur
 ************************************************************************************************************************************/
-require 'Modele/classe_associations.php';
-require 'Modele/classe_fichier.php';
-require 'Modele/classe_BD.php';
-require 'Modele/classe_menu.php';
-require 'Modele/classe_traceur.php';
-require 'Modele/classe_support.php';
-require 'Modele/classe_valideur.php';
-require 'Controleur/liens.php';
-require 'Controleur/cache.php';
-
 session_start(); // On démarre la session AVANT toute chose
 
-$TRACEUR = new Traceur; // voir avant dernière ligne pour affichage du rapport
-// détermination du mode pour le traitement et l'affichage
-if ((empty($_GET)) || (preg_match("#^[a-zA-Z0-9]{1,3}$#", $_GET["p"])))
-	$MODE = 'DT';
-elseif (isset($_GET["f"]))
-	$MODE = 'formulaire';
-else
-	$MODE = 'erreur';
-include 'Controleur/'.$MODE.'.php';
-/* chacun des controleur renvoie la configuration sous la forme d'un tableau associatif
- * css
- * logo
- * titre
- * page
- * cache: si non défini => pas de cache
- * paramètre supplémentaire dans ertain cas
- */
-$CONFIG = Configurer();
+$code = (int) $_GET['code'];
+switch ($code) { // voir .htaccess
+case 0:
+	$message = 'Un probl&egrave;me est survenu lors de l&apos;envoi de votre message'."\n".'R&eacute;essayez plus tard!';
+	break;
+case 403:
+	$message = 'Acc&egrave;s interdit';
+	break;
+case 404:
+	$message = 'Cette page n&apos;existe pas';
+	break;
+case 500:
+	$message = 'Serveur satur&eacute;: essayez de recharger la page';
+	break;
+default:
+	$message = 'Erreur inconnue';
+	$code = '';
+}
 ?>
 <!doctype html>
 <html lang="fr">
 <head>
 	<?php include('Vue/head_commun.html'); ?>
-	<link rel="stylesheet" href="Vue/<?=$CONFIG['css']?>.css" />
+	<link rel="stylesheet" href="Vue/style_page.css" />
 </head>
 
 <body>
 
 <header>
-	<div id="logo"><?=$CONFIG['logo']?></div>
-	<div id="titre"><p class="font-effect-outline"><?=$CONFIG['titre']?></p></div>
+	<div id="logo"><img src="Vue/images/logo.png" alt="logo"></div>
+	<div id="titre"><p class="font-effect-outline">Erreur <?=$code?></p></div>
 </header>
 
-<?php
-if (isset($CONFIG['cache']))
-	Gérer_cache($CONFIG['cache'], 0, $CONFIG['page']); // durée de vie en heure. Mettre à zéro lorsque j'interviens sur le site
-else include 'Vue/'.$CONFIG['page'].'.php';
-?>
+<section>
+<div style="width:600px; margin:auto;">
+<p><?=$message?></p>
+<a href="index.php">Retour au sommaire</a>
+</div>
+</section>
+
 <footer>
 <?php include 'Vue/pied2page.php'; ?>
 </footer>
 
 </body>
-<?php $TRACEUR->afficher_rapport();?>
 </html>
