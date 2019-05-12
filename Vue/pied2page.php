cf2ccb6b01c8111b8d2bca6cff84169d8cd3bb0f
Eclipse972 <christophe.hervi@gmail.com>
2019-05-12 03:02:35 +0200

    création du formulaire de contact et ses conséquences

--- Vue/pied2page.php
+++ Vue/pied2page.php
@@ -1,4 +1,4 @@
 <p>Site optimis&eacute; pour <img src="Vue/images/chrome.png" alt="Chrome"> et <img src="Vue/images/firefox.png" alt="Firefox">
-<?php if ($MODE == 'DT')	echo ' - <a href="?f=1">Me contacter</a>'; ?>
+<?php if (isset($_SESSION['support']))	echo ' - <a href="formulaire.php">Me contacter</a>'; ?>
  - <a target="_blank" href="http://faq.sw.free.fr">Ma FAQ SolidWorks</a>
- - derni&egrave;re mise à jour: 30 d&eacute;cembre 2018</p>
+ - derni&egrave;re mise à jour: 12 mai 2019</p>
--- formulaire.php
+++ formulaire.php
@@ -1,61 +1,68 @@
 <?php
 /*********************************************************************************************************************************** 
-	contrôleur principal
+	formumaire de contact
 ************************************************************************************************************************************/
 require 'Modele/classe_associations.php';
 require 'Modele/classe_fichier.php';
 require 'Modele/classe_BD.php';
-require 'Modele/classe_menu.php';
-require 'Modele/classe_traceur.php';
 require 'Modele/classe_support.php';
 require 'Modele/classe_valideur.php';
 require 'Controleur/liens.php';
-require 'Controleur/cache.php';
 
 session_start(); // On démarre la session AVANT toute chose
 
-$TRACEUR = new Traceur; // voir avant dernière ligne pour affichage du rapport
-// détermination du mode pour le traitement et l'affichage
-if ((empty($_GET)) || (preg_match("#^[a-zA-Z0-9]{1,3}$#", $_GET["p"])))
-	$MODE = 'DT';
-elseif (isset($_GET["f"]))
-	$MODE = 'formulaire';
-else
-	$MODE = 'erreur';
-include 'Controleur/'.$MODE.'.php';
-/* chacun des controleur renvoie la configuration sous la forme d'un tableau associatif
- * css
- * logo
- * titre
- * page
- * cache: si non défini => pas de cache
- * paramètre supplémentaire dans ertain cas
- */
-$CONFIG = Configurer();
+$BD = new base2donnees();
+$valideur = new Valideur();
+$_SESSION['validation'] = serialize($valideur);
+$_SESSION['temps'] = time();
+
+// contexte
+if (isset($_SESSION['support'])) {
+	$oSupport = unserialize($_SESSION['support']);
+	if ($oSupport->ID() > 0)
+		$objet = 'la page &laquo;'.$BD->Texte_item($oSupport->ID(), $oSupport->Item(), $oSupport->Sous_item()).'&raquo; '.$oSupport->Du_support();
+	else
+		$objet = 'l&apos;archive ZIP';
+} else	$objet = 'la liste de supports';
 ?>
+
 <!doctype html>
 <html lang="fr">
 <head>
 	<?php include('Vue/head_commun.html'); ?>
-	<link rel="stylesheet" href="Vue/<?=$CONFIG['css']?>.css" />
+	<link rel="stylesheet" href="Vue/style_page.css" />
 </head>
 
 <body>
 
 <header>
-	<div id="logo"><?=$CONFIG['logo']?></div>
-	<div id="titre"><p class="font-effect-outline"><?=$CONFIG['titre']?></p></div>
+	<div id="logo"><img src="Vue/images/logo.png" alt="logo"></div>
+	<div id="titre"><p class="font-effect-outline">Formulaire de contact</p></div>
 </header>
 
-<?php
-if (isset($CONFIG['cache']))
-	Gérer_cache($CONFIG['cache'], 0, $CONFIG['page']); // durée de vie en heure. Mettre à zéro lorsque j'interviens sur le site
-else include 'Vue/'.$CONFIG['page'].'.php';
-?>
+<section>
+<form method="post" action="Controleur/traitement_formulaire.php" id=formulaire>
+	<p>Nom : <input 	 type="text" name="nom"		 value="<?=$_SESSION['nom']?>" /></p>
+	<p>Courriel : <input type="text" name="courriel" value="<?=$_SESSION['courriel']?>" /></p>
+	<p>Objet : <input	 type="text" name="objet"	 placeholder="<?=$objet?>" /></p>
+	<p>Message : <textarea name="message" rows="6"></textarea></p>
+	<div id=validation>
+		<p>Validation du formulaire</p>
+		<ul>
+		<?php $valideur->Affiche();?>
+		</ul>
+		<p>Code	<input type="text" name="code" style="width:100px;" /></p>
+	</div>
+	<p style="text-align:center;">
+		<input type="submit" value="Envoyer" style="width:100px; margin-right:200px" />
+		<a href="<?=Parametres_support_courant()?>">Page pr&eacute;c&eacute;dente</a>
+	</p>
+</form>
+</section>
+
 <footer>
 <?php include 'Vue/pied2page.php'; ?>
 </footer>
 
 </body>
-<?php $TRACEUR->afficher_rapport();?>
 </html>
