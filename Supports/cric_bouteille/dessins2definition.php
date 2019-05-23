51e706aa16b81c91d875eed64f7030b6c4179ef4
Eclipse972 <christophe.hervi@gmail.com>
2019-05-23 13:58:50 +0200

    erreur de frappe

--- Supports/cric_bouteille/dessins2definition.php
+++ Supports/cric_bouteille/dessins2definition.php
@@ -1,2 +1,2 @@
 <h1>Dessins de d&eacute;finition</h1>
-<p>Les dessins de d&eacute;finition de certaines pi&eagrave;ces du cric sont disponibles. La liste est affich&eacute;e dans le menu.</p>
+<p>Les dessins de d&eacute;finition de certaines pi&egrave;ces du cric sont disponibles. La liste est affich&eacute;e dans le menu.</p>
