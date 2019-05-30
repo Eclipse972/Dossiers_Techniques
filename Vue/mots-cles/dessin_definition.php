5428c040b356a731d8098280bb8711b5e1796cab
Eclipse972 <christophe.hervi@gmail.com>
2019-05-30 21:30:15 +0200

    test dessin de définition concluant

--- Vue/mots-cles/dessin_definition.php
+++ Vue/mots-cles/dessin_definition.php
@@ -1,3 +1,3 @@
 <?php // association dessin d'ensemble
-$page = new Dessin_de_definition($oSupport->Dossier(), $T_instruction['param1'], $T_instruction['param2']);
-echo $page->Code();
+$page = new Page_dessin2définition('', $T_instruction['param1'], $T_instruction['param2']);
+echo $page->Afficher();
