dc72517cc14dafed322a3d219e74f1747a61f784
Eclipse972 <christophe.hervi@gmail.com>
2019-06-06 15:23:14 +0200

    dico des mots-clés et des pages à créer

--- Controleur/code_section.php
+++ Controleur/code_section.php
@@ -1,7 +1,19 @@
 <?php
-// détermination du script
+// détermination du type de page
 $BD = new base2donnees();
+$Tmots_clé = array(
+//	mot-clé	issu de la BD	=> type de page à créer
+	'association'			=> 'Page_association',
+	'classe_equivalence'	=> 'Page_CE',
+	'dessin_definition'		=> 'Page_dessin_def',
+	'dessin-densemble'		=> 'Page_dessin_densemble',
+	'eclate'				=> 'Page_éclaté',
+	'image_dessous'			=> 'Page_image_dessous',
+	'image_dessus'			=> 'Page_image_dessus',
+	'nomenclature'			=> 'Page_nomenclature'
+);
 $script = $BD->Script($oSupport->Id(), $oSupport->Item(), $oSupport->Sous_item());
+
 if (file_exists($oSupport->Dossier().$script)) // si le script dans le dossier du support existe
 	$script = $oSupport->Dossier().$script;
 elseif (file_exists('Vue/mots-cles/'.$script)) // sinon c'est un mot clé
