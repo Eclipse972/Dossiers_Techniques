8badc3c9be98bfdf5b90dbe1c65b7ae603568e01
Eclipse972 <christophe.hervi@gmail.com>
2019-06-09 18:46:04 +0200

    conséquences de la création de la Page_courbe

--- Supports/prehenseur/effort_articulation.php
+++
@@ -1,4 +0,0 @@
-<h1>&Eacute;volution de l&apos;effort d&eacute;velopp&eacute; dans l&apos;articulation biellette-noix en fonction du temps.</h1>
-<img src="<?=$Dossier_images?>effort_articulation.png"  alt="courbe">
-<p>tableau</p>
-<img src="<?=$Dossier_images?>tableau_effort_articulation.png"  alt="tableau de valeurs">
--- Supports/prehenseur/effort_verin.php
+++
@@ -1,4 +0,0 @@
-<h1>&Eacute;volution de l&apos;effort d&eacute;velopp&eacute; de la tige de v&eacute;rin en fonction du temps.</h1>
-<img src="<?=$Dossier_images?>effort_tige.png" alt="courbe">
-<p>tableau</p>
-<img src="<?=$Dossier_images?>tableau_effort_tige.png" alt="tableau de valeurs">
