88b66af91265c279231dd487a2b059af5a04b208
Eclipse972 <christophe.hervi@gmail.com>
2019-11-21 04:55:27 +0100

    erreur de syntaxe

--- Supports/moteur2modelisme/fonctionnement.php
+++ Supports/moteur2modelisme/fonctionnement.php
@@ -2,7 +2,7 @@
 <h2>Point mors bas (PMB)</h2>
 <p>le piston est en position basse</p>
 
-<h2>Mont&eacute;e du piston<h2>
+<h2>Mont&eacute;e du piston</h2>
 <p>phase de compression</p>
 
 <h2>Point mort haut (PMH)</h2>
