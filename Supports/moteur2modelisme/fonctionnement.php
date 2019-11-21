e2839c62eaf047527849e6bfc67b7cdf46dc6288
Eclipse972 <christophe.hervi@gmail.com>
2019-11-21 04:04:04 +0100

    ébauche de page de fonctionement

---
+++ Supports/moteur2modelisme/fonctionnement.php
@@ -0,0 +1,12 @@
+<h1>Cycle de fonctionnement d&apos;un moteur thermique 2 temps</h1>
+<h2>Point mors bas (PMB)</h2>
+<p>le piston est en position basse</p>
+
+<h2>Mont&eacute;e du piston<h2>
+<p>phase de compression</p>
+
+<h2>Point mort haut (PMH)</h2>
+<p>explosion du m&eacute;lange air-essence</p>
+
+<h2>Descente du piston</h2>
+<p>phase d&apos;&eacute;chappement</p>
