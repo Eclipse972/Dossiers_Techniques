5f4a2291a9cc756688f2d5eca15b4cbfa2053ee5
Eclipse972 <christophe.hervi@gmail.com>
2019-11-27 23:10:04 +0100

    ajout es images pour la page fonctionnement

--- Supports/moteur2modelisme/fonctionnement.php
+++ Supports/moteur2modelisme/fonctionnement.php
@@ -1,12 +1,16 @@
 <h1>Cycle de fonctionnement d&apos;un moteur thermique 2 temps</h1>
-<h2>Point mors bas (PMB)</h2>
+<h2>Point mort bas (PMB)</h2>
 <p>le piston est en position basse</p>
+<img src="<?php echo $_SESSION['support']->Dossier().'images/';?>PMB.png" alt ="point mort bas">
 
 <h2>Mont&eacute;e du piston</h2>
 <p>phase de compression</p>
+<img src="<?php echo $_SESSION['support']->Dossier().'images/';?>montée.png" alt ="mont&eacute;e du poston">
 
 <h2>Point mort haut (PMH)</h2>
 <p>explosion du m&eacute;lange air-essence</p>
+<img src="<?php echo $_SESSION['support']->Dossier().'images/';?>PMH.png" alt ="point mort haut">
 
 <h2>Descente du piston</h2>
 <p>phase d&apos;&eacute;chappement</p>
+<img src="<?php echo $_SESSION['support']->Dossier().'images/';?>descente.png" alt ="descente du piston">
