1464a6417140daed20320b04712bf31cd5ee89ef
Eclipse972 <christophe.hervi@gmail.com>
2019-12-05 13:12:51 +0100

    petite erreur d'indentation

--- Modele/classe_page.php
+++ Modele/classe_page.php
@@ -94,10 +94,10 @@ class Page_nomenclature extends Page_abstraite {
 	<th>Rep</th>
 	<th>Image</th>
 	<th>D&eacute;signation (x quantit&eacute;)</th>
-<?php
+	<?php
 	if ($this->colonne_matière) echo '<th>Mati&egrave;re</th>',"\n";
 	if ($this->colonne_observation) echo "\t",'<th>Observations</th>',"\n";
-?>
+	?>
 </tr>
 </thead>
 
