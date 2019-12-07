876a966f67d0d17f4a341bb3f341d29f8a941e46
Eclipse972 <christophe.hervi@gmail.com>
2019-12-07 02:16:38 +0100

    date de mise à jour

--- Vue/pied2page.php
+++ Vue/pied2page.php
@@ -1,4 +1,4 @@
 <p>Site optimis&eacute; pour <img src="Vue/images/chrome.png" alt="Chrome"> et <img src="Vue/images/firefox.png" alt="Firefox">
 <?php if (isset($_SESSION['support']))	echo ' - <a href="formulaire.php">Me contacter</a>'; ?>
  - <a target="_blank" href="http://faq.sw.free.fr">Ma FAQ SolidWorks</a>
- - derni&egrave;re mise à jour: 12 mai 2019</p>
+ - derni&egrave;re mise à jour: 7 d&eacute;cembre 2019</p>
