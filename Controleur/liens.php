58a7757a96d58c93b7e246c335ee802b304fedb3
Eclipse972 <christophe.hervi@gmail.com>
2019-12-08 13:52:10 +0100

    quelques erreurs de syntaxe

--- Controleur/liens.php
+++ Controleur/liens.php
@@ -1,5 +1,4 @@
 <?php
-$LISTE = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'; // 62 possibilités
 /*
  A propos des 3 paramètres. Ce sont des entiers
  * support: identifiant du support.-1 signifie aucun support
@@ -8,27 +7,27 @@ $LISTE = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'; // 62
  */
 
 function Lien($texte, $support, $item = null, $sous_item = null) { // l'existence de la page correpondante doit être vérifiée en amont
-	return '<a href="pageDT.php?p='.Creer_parametre($support, $item, $sous_item).'">'.$texte.'</a>';
+	return '<a href="pageDT.php?'.Creer_parametre($support, $item, $sous_item).'">'.$texte.'</a>';
 }
 
 // Lecture des paramètres
-function Lire_parametre($nom, $defaut_id = 0, $defaut_item = 0, $defaut_sous_item = 0) {
-	global $LISTE;
-	$param = substr((string) $_GET[$nom],0,3);	// le paramètre est converti en nombre chaîne de 3 caractères maxi
-	$id			= (isset($param[0])) ? strpos($LISTE, $param[0]) : $defaut_id; //  aucun support a comme identifiant -1 => liste des supports
-	$item		= (isset($param[1])) ? strpos($LISTE, $param[1]) : $defaut_item;
-	$sous_item	= (isset($param[2])) ? strpos($LISTE, $param[2]) : $defaut_sous_item;
+function Lire_parametre($defaut_id, $defaut_item = 1, $defaut_sous_item = 0) {
+	$id	= (int)$_GET["s"];
+	
+	$menu		= substr((string) $_GET["m"],0,2);	// le paramètre est converti en nombre chaîne de 2 caractères maxi
+	$item		= (isset($menu[0])) ? ord($menu[0])-ord('a'): $defaut_item;
+	$sous_item	= (isset($menu[1])) ? ord($menu[1])-ord('a'): $defaut_sous_item;
 	return [$id, $item, $sous_item];
 }
 
 // Ecriture des paramètres
-function Creer_parametre($param1, $param2, $param3) {
+function Creer_parametre($param1, $param2 = null, $param3 = null) {
 	// ne peut pas être exporté vers la classe support car car cette fonction crée les menus
-	global $LISTE;
-	$parametre = $LISTE[(int)$param1];
+	// elle ne peut être intégrée dans Lien car elle est utilisée seule dans pageDT pour créer les caches
+	$parametre = 's='.(string)$param1;
 	if (isset($param2)) {
-		$parametre .= $LISTE[(int)$param2];
-		if (isset($param3))	$parametre .= $LISTE[(int)$param3];
+		$parametre .= '&m='.chr($param2+ord('a'));
+		if (isset($param3))	$parametre .= chr($param3+ord('a'));
 	}
 	return $parametre;
 }
