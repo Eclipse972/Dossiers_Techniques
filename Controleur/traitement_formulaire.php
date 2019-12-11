74c41a20463d529c9875e558b4393d1a55755cb4
Eclipse972 <christophe.hervi@gmail.com>
2019-12-11 18:00:19 +0100

    récupérer les données et renvoyer vers la page d'accueil

--- Controleur/traitement_formulaire.php
+++ Controleur/traitement_formulaire.php
@@ -1,24 +1,12 @@
 <?php
+require "../Modele/classe_formulaire.php";
 session_start();
 // ce script est exécuté independament de index.php donc il faut inclure les classes utiles
-include "liens.php";
-include "../Modele/classe_support.php";
-include "../Modele/classe_BD.php";
+//include "liens.php";
+//include "../Modele/classe_support.php";
 
-list($objet, $message, $code) = Récupérer_paramètres();
+$_SESSION['formulaire']->Récupérer_données();
 
-//$validation = unserialize($_SESSION['validation']);
-
-if ((isset($_SESSION['nom'])) && 
-	(isset($_SESSION['courriel'])) && 
-	(strlen($objet) > 1) && 
-	(strlen($message) > 1) && 
-	$validation->OK($objet, $message, $code))
-{	// enregistrement du message
-	if (Envoyer_message($objet, $message))
-			$parametre = "index.php";		// retour sur la page index au leu de celle précédant le formulaire
-	else	$parametre = "erreur.php";		// problème avec l'envoi du mail
-} else		$parametre = "formulaire.php";	// retour au formulaire car non validé
-
+$parametre = "index.php";
 header("Location: http://dossiers.techniques.free.fr/".$parametre);
 exit;
