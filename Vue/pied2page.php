95a531ac5f447b330323e5b6e63c905441fc934e
Eclipse972 <christophe.hervi@gmail.com>
2019-12-11 11:24:39 +0100

    le formulaire accessible depuis toutes les pages du site

--- Vue/pied2page.php
+++ Vue/pied2page.php
@@ -1,4 +1,4 @@
 <p>Site optimis&eacute; pour <img src="Vue/images/chrome.png" alt="Chrome"> et <img src="Vue/images/firefox.png" alt="Firefox">
-<?php if (isset($_SESSION['support']))	echo ' - <a href="formulaire.php">Me contacter</a>'; ?>
+ - <a href="formulaire.php">Me contacter</a>
  - <a target="_blank" href="http://faq.sw.free.fr">Ma FAQ SolidWorks</a>
  - derni&egrave;re mise à jour: 7 d&eacute;cembre 2019</p>
