127024e17e70e73359f22eb2831893be95f9ec8e
Eclipse972 <christophe.hervi@gmail.com>
2019-12-12 14:12:44 +0100

    ébauche de traitement

--- Controleur/traitement_formulaire.php
+++ Controleur/traitement_formulaire.php
@@ -1,12 +1,19 @@
 <?php
 require "../Modele/classe_formulaire.php";
 session_start();
-// ce script est exécuté independament de index.php donc il faut inclure les classes utiles
-//include "liens.php";
-//include "../Modele/classe_support.php";
-
 $_SESSION['formulaire']->Récupérer_données();
 
+if ($_SESSION['formulaire']->OK()) // remplissage du formulaire validé?
+	if ($_SESSION['formulaire']->Envoyer_message())	// on essaie d'envoyer le message
+		$suite = $this->lien;
+	else {
+		$_SESSION['erreur'] = 0;
+		$suite = "erreur";
+	}
+else {
+	$suite = 'formulaire';
+}
+	
 $parametre = "index.php";
-header("Location: http://dossiers.techniques.free.fr/".$parametre);
+header('Location: http://dossiers.techniques.free.fr/'.$suite.'.php');
 exit;
