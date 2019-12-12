ed2119e913791234bce1cbcf95160f3351e14a8f
Eclipse972 <christophe.hervi@gmail.com>
2019-12-12 14:40:51 +0100

    seul le script doit être conservé dans la varibe lien du formulaire

--- Controleur/traitement_formulaire.php
+++ Controleur/traitement_formulaire.php
@@ -1,19 +1,7 @@
 <?php
 require "../Modele/classe_formulaire.php";
 session_start();
-$_SESSION['formulaire']->Récupérer_données();
-
-if ($_SESSION['formulaire']->OK()) // remplissage du formulaire validé?
-	if ($_SESSION['formulaire']->Envoyer_message())	// on essaie d'envoyer le message
-		$suite = $this->lien;
-	else {
-		$_SESSION['erreur'] = 0;
-		$suite = "erreur";
-	}
-else {
-	$suite = 'formulaire';
-}
 	
-$parametre = "index.php";
-header('Location: http://dossiers.techniques.free.fr/'.$suite.'.php');
+$suite = $_SESSION['formulaire']->Traitement();
+header('Location: http://dossiers.techniques.free.fr/'.$suite);
 exit;
