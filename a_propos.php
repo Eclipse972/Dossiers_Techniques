ef5741b40239ec11ab5c1ce8840d45e512dda23c
Eclipse972 <christophe.hervi@gmail.com>
2019-12-15 06:57:53 +0100

    déplacement de la fonction Lien dans a_propos.php

--- a_propos.php
+++ a_propos.php
@@ -11,8 +11,6 @@ if (!isset($_SESSION['support'])) {	// s'il ny a pas de support en cours
 	header("Location: http://dossiers.techniques.free.fr/erreur.php");	// page d'erreur
 	exit;
 }
-require 'Controleur/liens.php';
-require 'Modele/classe_BD.php';
 
 function Code_Liste($Liste, $description_maquette){
 	switch(count($Liste)) {
@@ -31,6 +29,28 @@ function Code_Liste($Liste, $description_maquette){
 	}
 	return $code;
 }
+
+function Lien($texte, $support, $item = null, $sous_item = 0) {
+/*
+ A propos des paramètres transmis par l'URL
+ * s : identifiant du support sous for d'un nombre avec 2 chiffres maximum.
+ * m : la position dans le menu transmis sous la forme d'un ou deux minuscules
+ * 		la premire pour l'item et la seconde pour le sous-item
+ * 		a->0, b->1, ...
+ * Remarques:
+ * - Le code ASCII de 'a' est 97
+ * - une minuscule offre 26 possibilités pour item et sous-item ce qui est largement suffisant
+ * - dans le BD la vue nommée Vue_code_menu utilise la même structure
+ * - l'existence de la page correpondante doit être vérifiée en amont
+ */
+	if (isset($item)) {
+		$menu = '&m='.chr($item+97);
+		if ($sous_item > 0)	$menu .= chr($sous_item+97);
+	} else $menu = '';
+	return '<a href="pageDT.php?s='.(string)$support.$menu.'">'.$texte.'</a>';
+}
+
+require 'Modele/classe_BD.php';
 $BD = new base2donnees();
 $zip = ($_SESSION['support']->Zip_existe()) ?
 		$_SESSION['support']->Zip()."\n".Code_Liste($BD->Description_maquette(), true) : // lien + description
