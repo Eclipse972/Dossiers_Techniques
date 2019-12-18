49dc43bfd70ee99bc194aec9205f6133c8c05e6e
Eclipse972 <christophe.hervi@gmail.com>
2019-12-18 15:02:02 +0100

    quelques erreurs à propos de la variable pti_nom

--- Modele/classe_support.php
+++ Modele/classe_support.php
@@ -6,7 +6,7 @@ private $sous_item;	// sous-item actuel
 private $nom;
 private $du_support;
 private $le_support;
-/*private $pti_nom; /* C'est le nom par défaut de certains fichiers.
+private $pti_nom; /* C'est le nom par défaut de certains fichiers.
 le dessin d'ensemble.Ex dessin_cric
 la maquette numérique: cric.EASM
 l'image du support: cric.png
@@ -29,8 +29,8 @@ if ($ligne != null) { // la ligne est non vide
 	$this->setDossier($ligne['dossier']);
 	$this->setZip($ligne['zip']);
 	$this->setImage($ligne['image']);
-
-	//$this->setPti_nom($ligne['pti_nom']);
+	$this->setPti_nom($ligne['pti_nom']);
+	
 	$this->setTypeNomenclature($ligne['type_nomenclature']);
 }
 }
