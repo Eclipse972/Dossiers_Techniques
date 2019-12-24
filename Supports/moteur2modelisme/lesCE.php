f195855da01cc71f126c3b24fc2257fe0ead0c18
Eclipse972 <christophe.hervi@gmail.com>
2019-12-24 17:31:05 +0100

    présentation des CE

---
+++ Supports/moteur2modelisme/lesCE.php
@@ -0,0 +1,3 @@
+<h1>Les classes d&apos;&eacute;quivalence</h1>
+<p>Le moteur de mod&eacute;lisme est compos&eacute; de 4 classes d&apos;&eacute;quivalence.</p>
+<p>Vois le menu ci-contre pour y acc&eacute;der</p>
