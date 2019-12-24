0376161f895560aee1f3c4d902936dff52ceb2b6
Eclipse972 <christophe.hervi@gmail.com>
2019-12-24 02:54:35 +0100

    demander au visiteur de me contacter si la page qu'il visionne n'est pas rédigée

--- Modele/classe_page.php
+++ Modele/classe_page.php
@@ -131,6 +131,7 @@ class Page_script extends Page_abstraite {
 	<h1>D&eacute;sol&eacute; !</h1>
 	<p align=center>Il semblerai que je n&apos;ai pas r&eacute;dig&eacute; cette page.</p>
 	<?=$_SESSION['support']->Image()?>
+	<p align=center>N&apos;h&eacute;sitez pas &agrave; me contacter (lien en bas de page) si le probl&egrave;me persiste.</p>
 	</div>
 <?php
 		}
