3a6820dd0ab1faeba5e45a03575ba0cc9da33dfa
Eclipse972 <christophe.hervi@gmail.com>
2019-12-24 04:29:28 +0100

    requête inutile

--- BD/essai.sql
+++
@@ -1,9 +0,0 @@
-DROP VIEW Vue_hydrate_supports;
-CREATE VIEW Vue_hydrate_supports AS 
-SELECT ID, nom, pti_nom, type_nomenclature, 
-	CONCAT(IF(article_ID=1,'du ', IF(article_ID=2,'de la ', 'de l&apos;')),nom) AS du_support,
-	CONCAT(IF(article_ID=1,'le ', IF(article_ID=2,'la ', 'l&apos;')),nom) AS le_support,
-	CONCAT('Supports/', dossier, '/') AS dossier,
-	CONCAT('Supports/', dossier, '/fichiers/', pti_nom, '.zip') AS zip,
-	CONCAT('Supports/', dossier, '/images/', pti_nom, '.png') AS image
-FROM Supports
