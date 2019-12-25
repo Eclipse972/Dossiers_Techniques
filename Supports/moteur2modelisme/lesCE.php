4db27e055ba93676bda66fc0e595937c0c231a73
Eclipse972 <christophe.hervi@gmail.com>
2019-12-25 05:43:26 +0100

    les images présentant les CE

--- Supports/moteur2modelisme/lesCE.php
+++ Supports/moteur2modelisme/lesCE.php
@@ -7,22 +7,23 @@
 
 <div class="colonne">
 <p>CE2: le vilebrequin</p>
-<img src="<?=$_SESSION['support']->Dossier().'images/'?>CE2-vilebrequin.png" alt ="CE2: le vilebrequin">
+<img src="<?=$_SESSION['support']->Dossier().'images/'?>CE2-vilebrequin.png" width=305px alt ="CE2: le vilebrequin">
 </div>
 
 <div class="colonne">
 <p>CE3: la bielle</p>
-<img src="<?=$_SESSION['support']->Dossier().'images/'?>CE3-bielle.png" alt ="CE3: la bielle">
+<img src="<?=$_SESSION['support']->Dossier().'images/'?>CE3-bielle.png" width=91px alt ="CE3: la bielle">
 </div>
 
 <div class="colonne">
 <p>CE4: le piston</p>
-<img src="<?=$_SESSION['support']->Dossier().'images/'?>CE4-piston.png" alt ="CE4: le piston">
+<img src="<?=$_SESSION['support']->Dossier().'images/'?>CE4-piston.png" width=131px alt ="CE4: le piston">
 </div>
 
 <h2>Les roulements à billes du moteur</h2>
 <img src="<?=$_SESSION['support']->Dossier().'images/'?>grand_roulement.png" width=200px alt ="grand roulement">
 <img src="<?=$_SESSION['support']->Dossier().'images/'?>petit_roulement.png" width=160px alt ="petit roulement">
-<p>Pendant le fonctionnement du moteur, les bagues ext&eacute;rieures sont solidaires des pi&egrave;ces al&eacute;sages.
-<br>Les bagues int&eacute;rieures sont solidaires des pi&egrave;ces arbres.</p>
-<p>Voir le menu ci-contre pour plus de d&eacute;tails</p>
+<p>Une fois les roulements mont&eacute;s, les bagues ext&eacute;rieures sont solidaires des al&eacute;sages du b&acirc;ti.
+Et les bagues int&eacute;rieures sont solidaires du vilebrequin. Dans les deux cas les bagues des roulements seront 
+repr&eacute;sent&eacute;es en jaune.</p>
+<p>Voir le menu ci-contre pour plus de d&eacute;tails sur chaque classe d&apos;&eacute;quivalence.</p>
