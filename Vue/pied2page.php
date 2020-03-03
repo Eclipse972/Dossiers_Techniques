ff29180bfa54c10eda0ed76ba10bd1097b52079f
Eclipse972 <christophe.hervi@gmail.com>
2020-03-03 23:19:06 +0100

    MAJ pied de page

--- Vue/pied2page.php
+++ Vue/pied2page.php
@@ -4,4 +4,4 @@
 		echo ' - <a href="formulaire.php">Me contacter</a>';
 ?>
  - <a target="_blank" href="http://faq.sw.free.fr">Ma FAQ SolidWorks</a>
- - derni&egrave;re mise à jour: 9 janvier 2020</p>
+ - derni&egrave;re mise à jour: 3 mars 2020</p>
