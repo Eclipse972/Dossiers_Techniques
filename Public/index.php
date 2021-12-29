17a0d29921459bca291938bc05cc440c0075be1a
ChristopHe <christophe.hervi@free.fr>
2021-12-29 19:32:04 +0100

    remplacement du fichier index.php

--- Public/index.php
+++ Public/index.php
@@ -1,48 +1,62 @@
-<?php
-/*********************************************************************************************************************************** 
-	Affiche la liste des supports
-************************************************************************************************************************************/
-session_start(); // On démarre la session AVANT toute chose
-require 'Modele/classe_BD.php';
+<?php	// routeur de PEUNC
+session_start();
+/* contexte sauvegardé dans la session (alpha, beta, gamma) par importance décroissante
+	Si alpha >=0 => pages du site
+	(X;0;0) => page de 1er niveau. 	(0;0;0) -> page d'accueil.
 
-define(DUREE, 8);	// durée du cache en heure
-define(CACHE, 'Vue/cache/index.cache');	// nom du fichier cache
+	(X;Y;0) avec Y>0 => page de 2e niveau
 
-$_SESSION['support'] = null;	// on détruit le support en cours
-?>
-<!doctype html>
-<html lang="fr">
-<head>
-<?php include('Vue/head_commun.html'); ?>
-	<link rel="stylesheet" href="Vue/index.css"/>
-</head>
+	(X;Y;Z) avec Z>0 => page de 3e niveau
 
-<body>
+	si alpha<0 => page spéciales PEUNC
+	(-1;code;0) -> page d'erreur avec son code
+	(-2;0;0) formulaire de contact
+*/
 
-<header>
-	<div id="logo"><img src="Vue/images/logo.png" alt="logo"></div>
-	<div id="titre"><p class="font-effect-outline">Liste des dossiers techniques</p></div>
-</header>
+require 'PEUNC/classes/Page.php';
+require 'PEUNC/classes/BDD.php';
 
-<section>
-<h1>Cliquez sur l&apos;image ou le nom du support pour acc&eacute;der &agrave; son dossier technique</h1>
-<table>
-<?php
-if(file_exists(CACHE) && (time() - filemtime(CACHE) < DUREE * 3600))
-	readfile(CACHE);
-else {	// création du code sans utiliser le tampon de PHP
-	$BD = new base2donnees();	// accès à la base de données
-	$code = $BD->Gerer_index(5);// récupère et agrège le code
-	$code .=  '<!-- cache généré le '.date("d/m/Y \à H:i").' -->'."\n";
-	file_put_contents(CACHE, $code);
-	echo $code;
+// classes utilisateur
+require 'Modele/classe_Page.php';
+//require 'Modele/classe_BDD.php';
+
+try
+{
+	$BD = new PEUNC\classes\BDD;
+	$codeRedirecion = $_SERVER['REDIRECT_STATUS'];
+	switch($codeRedirecion) {	// Toutes les erreurs serveur renvoient ici. Cf .htaccess
+		case 403:	// accès interdit
+		case 500:	// erreur serveur
+			list($_SESSION['alpha'], $_SESSION['beta'], $_SESSION['gamma']) = [-1, $codeRedirecion, 0];	break;
+		case 200:	// le script est lancé sans redirection => page d'accueil
+			$_SESSION['alpha'] = $_SESSION['beta'] = $_SESSION['gamma']	= 0;
+			break;
+		case 404:	// Ma source d'inspiration: http://urlrewriting.fr/tutoriel-urlrewriting-sans-moteur-rewrite.htm Merci à son auteur
+			list($URL, $paramPage, $problem) = explode("?", $_SERVER['REQUEST_URI'], 3);
+			if(isset($problem))	throw new Exception("format URL incorrect");
+			list($alpha, $beta, $gamma) = $BD->CherchePosition($URL);	// compare avec toutes les URL valides du site
+			if (isset($alpha))	{	// adresse valide, on ne touche à rien
+				header("Status: 200 OK", false, 200);	// modification pour dire au navigateur que tout va bien finalement
+				list($_SESSION['alpha'], $_SESSION['beta'], $_SESSION['gamma']) = [$alpha, $beta, $gamma];	// $_SESSION = array('alpha' => $alpha, 'beta' = $beta, 'gamma' => $gamma) détruirait les autres éventuels paramètres
+			} else	list($_SESSION['alpha'], $_SESSION['beta'], $_SESSION['gamma']) = [-1, 404, 0];	// l'adresse invalide reste affichée dans la barre d'adresse'
+			break;
+		default:
+			list($_SESSION['alpha'], $_SESSION['beta'], $_SESSION['gamma']) = [-1, 0, 0];	// erreur inconnue
+	}
+
+	$classePage = $BD->ClassePage($_SESSION['alpha'], $_SESSION['beta'], $_SESSION['gamma']);
+	if (!isset($classePage))	throw new Exception("La classe {$classePage} n&apos;est pas d&eacute;finie dans le squelette.");
+	require"Modele/classe_{$classePage}.php";
+	$PAGE = new $classePage(explode("/", $paramPage));
+	$PAGE->ExecuteControleur($_SESSION['alpha'], $_SESSION['beta'], $_SESSION['gamma']);
 }
-?>
-</table>
-</section>
-<footer>
-<?php include 'Vue/pied2page.php'; ?>
-</footer>
+catch(Exception $e)
+{
+	require"Modele/classe_PageErreur.php";
+	$PAGE = new PageErreur;
+	$PAGE->setCodeErreur("application");
+	$PAGE->setTitreErreur($e->getMessage());
+	$PAGE->setCorpsErreur("<p>Noeud {$_SESSION['alpha']} - {$_SESSION['beta']} - {$_SESSION['gamma']}</p>");
+}
 
-</body>
-</html>
+include $PAGE->getView(); // insertion de la vue
