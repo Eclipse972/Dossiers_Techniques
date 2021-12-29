5ff6a8aa36db493b6280fe8bdefcb4a1972628db
ChristopHe <christophe.hervi@free.fr>
2021-12-29 19:49:58 +0100

    MAJ du dossir Modele

---
+++ Public/Modele/classe_Page.php
@@ -0,0 +1,12 @@
+<?php
+class Page extends PEUNC\classes\Page {
+
+	public function __construct(array $TparamURL = []) {
+		parent::__construct($TparamURL);
+		// valeurs par défaut
+		$this->setTitle("La Foire Aux Questions sur SolidWorks de ChristopHe");
+		$this->setHeaderText("<p class=\"font-effect-outline\">Foire Aux Questions SolidWorks de ChristopHe</p>");
+		$this->setLogo("logo.png");
+		$this->setFooter("");
+	}
+}
