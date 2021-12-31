e5c92a9572c3b9b4653f1e605258835c312431fc
ChristopHe <christophe.hervi@free.fr>
2021-12-31 08:21:17 +0100

    supprime la version précédente de la vue

--- BD/Vue_code_vignettes.sql
+++ BD/Vue_code_vignettes.sql
@@ -1,7 +1,8 @@
+DROP VIEW IF EXISTS Vue_code_vignettes;
 CREATE VIEW Vue_code_vignettes AS
 SELECT CONCAT(
 	'<td><a href="#">'
-	,Supports.nom
+	,nom
 	,'<br><img src="Supports/',dossier,'/images/',pti_nom,'.png" alt="',nom,'"></a></td>'
 ) AS `code`
 
