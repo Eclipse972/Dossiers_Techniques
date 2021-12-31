8e4ad78dd8173332d94e82a4c04286cd6c7cb702
ChristopHe <christophe.hervi@free.fr>
2021-12-31 06:15:41 +0100

    Hello world! fonctionne

--- Public/Modele/classe_Page.php
+++ Public/Modele/classe_Page.php
@@ -5,9 +5,10 @@ class Page extends PEUNC\classes\Page {
 		parent::__construct($TparamURL);
 		// valeurs par défaut
 		$this->setTitle("Les dossiers techniques de ChristopHe");
-		$this->setHeaderText("<p class=\"font-effect-outline\">Les dossiers techniques de ChristopHe</p>");
+		$this->setHeaderText("<p class=\"font-effect-outline\">Les dossiers techniques de ChristopHe version test</p>");
 		$this->setLogo("logo.png");
 		$this->setFooter("");
 		$this->setView("doctype.html");
+		$this->setCSS(array("https://fonts.googleapis.com/css?family=Quicksand:400,700&effect=outline", "commun", "index"));
 	}
 }
