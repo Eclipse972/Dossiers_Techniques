c35d2f0e3723ecc9c529c4c18ac37768b2e0fc4f
ChristopHe <christophe.hervi@free.fr>
2021-12-31 23:40:12 +0100

    Utilisation de la méthode ResultatSQL

--- Public/Modele/classe_Index.php
+++ Public/Modele/classe_Index.php
@@ -15,14 +15,12 @@ class Index extends PEUNC\classes\Page {
 	public function Gerer_index($NB_colonne) {
 		global $BD;
 		$code = '';
-		$BD->Requete('SELECT * FROM Vue_code_vignettes', []);
-		$id = 0;
-		while ($ligne = $BD->resultat->fetch()) {	// récupère et agrège le code
+		$Tréponse = $BD->ResultatSQL('SELECT * FROM Vue_code_vignettes', []);
+		foreach($Tréponse as $id => $ligne) {	// récupère et agrège le code
 			$No_colonne = $id % $NB_colonne;
 			if($No_colonne==0) $code .=  '<tr>'."\n"; // nouvelle ligne
 			$code .= "\t".$ligne['code']."\n";
 			if($No_colonne==$NB_colonne-1) $code .= '</tr>'."\n";	// fin de ligne si dernière colonne atteinte
-			$id++;
 		}
 		// si en sortie on s'arrete sur une colonne autre que la dernière
 		if($No_colonne!=$NB_colonne-1) $code .= "\t</tr>\n"; // on termine la ligne
