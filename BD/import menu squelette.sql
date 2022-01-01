33481d11c4f026d6f57bd1512d051078b2fff842
ChristopHe <christophe.hervi@free.fr>
2022-01-01 12:13:13 +0100

    ajout d'une explication

--- BD/import menu squelette.sql
+++ BD/import menu squelette.sql
@@ -1,3 +1,4 @@
 INSERT INTO Squelette (	alpha,			beta, gamma, 		texteMenu,	classePage)
 SELECT					support_ID+2,	item, sous_item,	texte,		type_page
 FROM Menu
+-- +2 car dans la table Squelette il y la page d'accueil (alpha=0) et de contact (alpha=1)
