013721237c786841d5a6b940a9e3a74a5b12b746
ChristopHe <christophe.hervi@free.fr>
2022-01-02 20:13:59 +0100

    création de la Vue support

---
+++ BD/Vue_supports.sql
@@ -0,0 +1,23 @@
+
+DROP VIEW IF EXISTS Vue_supports;
+CREATE VIEW Vue_supports AS
+SELECT
+	ID,
+	nom,
+	pti_nom AS ptiNomSupport,
+	CONCAT(	CASE article_ID
+				WHEN 1 THEN 'du '
+				WHEN 2 THEN 'de la '
+				ELSE 'de l&apos;'
+			END, nom
+		) AS du_support,
+	CONCAT(	CASE article_ID
+				WHEN 1 THEN 'du '
+				WHEN 2 THEN 'de la '
+				ELSE 'de l&apos;'
+			END, nom
+		) AS le_support,
+	CONCAT('Supports/',dossier,'/') AS dossier,
+	CONCAT('Supports/',dossier,'/fichiers/',pti_nom,'.zip') AS zip,
+	CONCAT('Supports/',dossier,'/images/',pti_nom,'.png') AS image
+FROM Supports;
