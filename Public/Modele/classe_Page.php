f1c3a92415154cbf7d4e7dcaf8aabd92f77e7dcb
ChristopHe <christophe.hervi@free.fr>
2022-01-02 16:33:12 +0100

    première méthodes pour la classe Page

--- Public/Modele/classe_Page.php
+++ Public/Modele/classe_Page.php
@@ -1,5 +1,6 @@
 <?php // classe mère de toutes les pages du dossier techniques sauf la page index
 class Page extends PEUNC\classes\Page {
+	private $codeTitre;
 
 	public function __construct(array $TparamURL = []) {
 		parent::__construct($TparamURL);
@@ -9,6 +10,22 @@ class Page extends PEUNC\classes\Page {
 		$this->setLogo("logo.png");
 		$this->setFooter("");
 		$this->setView("doctype.html");
-		$this->setCSS(array("https://fonts.googleapis.com/css?family=Quicksand:400,700&effect=outline", "commun", "index"));
+		//$this->setCSS(array("https://fonts.googleapis.com/css?family=Quicksand:400,700&effect=outline", "commun", "index"));
 	}
+/* ***************************
+ * MUTATEURS (SETTER)
+ * ***************************/
+
+	public function setTitrePage($titre = "Titre non défini")	{ $codeTitre = "<h1>{$titre}</h1>"; }
+
+/* ***************************
+ * ASSESSURS (GETTER)
+ * ***************************/
+
+	public function getTitrePage($titre)	{ return $codeTitre; }
+
+/* ***************************
+ * AUTRE
+ * ***************************/
+
 }
