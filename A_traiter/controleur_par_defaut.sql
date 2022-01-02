b651d17de46d1546dcd7b928fcab616961dc7ac1
ChristopHe <christophe.hervi@free.fr>
2022-01-02 21:17:15 +0100

    explication du -2

--- A_traiter/controleur_par_defaut.sql
+++ A_traiter/controleur_par_defaut.sql
@@ -1,7 +1,7 @@
 -- définir le nom de controleur par défaut
 UPDATE Squelette
 SET controleur = CONCAT(
-		(SELECT dossier FROM Supports WHERE Supports.ID = Squelette.alpha-2),
+		(SELECT dossier FROM Supports WHERE Supports.ID = Squelette.alpha-2), -- -2 à cause de la page d'accueil (alpha=0) et la page de contact (alpha=1)
 		'/',Squelette.ptiNom,'.php'
 	)
 WHERE Squelette.ID > 1;
