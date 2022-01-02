cc7dbd95087f9ba3e3551ab216f70e5adf04c611
ChristopHe <christophe.hervi@free.fr>
2022-01-02 21:19:38 +0100

    dernier commentaire

--- A_traiter/controleur_par_defaut.sql
+++ A_traiter/controleur_par_defaut.sql
@@ -4,4 +4,4 @@ SET controleur = CONCAT(
 		(SELECT dossier FROM Supports WHERE Supports.ID = Squelette.alpha-2), -- -2 à cause de la page d'accueil (alpha=0) et la page de contact (alpha=1)
 		'/',Squelette.ptiNom,'.php'
 	)
-WHERE Squelette.ID > 1;
+WHERE Squelette.ID > 1; -- voir commentaire précédent
