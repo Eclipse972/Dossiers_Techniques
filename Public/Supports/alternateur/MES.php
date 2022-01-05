cd1819fa8f8d4cdc87b06450efc825b13538a93d
ChristopHe <christophe.hervi@free.fr>
2022-01-06 00:33:23 +0100

    le script défini le variable pour le controleur MiseEnSituation

--- Public/Supports/alternateur/MES.php
+++ Public/Supports/alternateur/MES.php
@@ -1,5 +1,4 @@
 <?php
-// Contrôleur de la mise en situaton de l'alternateur
+// Variables pour le controleur MiseEnSituation de l'alternateur
 
-$this->setCommentaire("Les nombreux &eacute;l&eacute;ments &eacute;lectriques et &eacute;lectroniques mont&eacute;s sur la Citro&euml;n C4 HDI n&eacute;cessitent une source d&apos;&eacute;nergie &eacute;lectrique fiable et embarqu&eacute;e. L&apos;alternateur participe &agrave; cette op&eacute;ration, tout comme la batterie.");
-$this->setImage("/Supports/alternateur/images/mes.png");
+$texte = "Les nombreux &eacute;l&eacute;ments &eacute;lectriques et &eacute;lectroniques mont&eacute;s sur la Citro&euml;n C4 HDI n&eacute;cessitent une source d&apos;&eacute;nergie &eacute;lectrique fiable et embarqu&eacute;e. L&apos;alternateur participe &agrave; cette op&eacute;ration, tout comme la batterie.";
