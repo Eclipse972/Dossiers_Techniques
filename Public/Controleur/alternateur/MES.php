540c578a9945a0abeabf6fee22c16da9cc7542ab
ChristopHe <christophe.hervi@free.fr>
2022-01-05 20:25:22 +0100

    premier controleur

---
+++ Public/Controleur/alternateur/MES.php
@@ -0,0 +1,4 @@
+<?php
+// Contrôleur de la mise en situaton de l'alternateur
+
+$this->setCommentaire("Les nombreux &eacute;l&eacute;ments &eacute;lectriques et &eacute;lectroniques mont&eacute;s sur la Citro&euml;n C4 HDI n&eacute;cessitent une source d&apos;&eacute;nergie &eacute;lectrique fiable et embarqu&eacute;e. L&apos;alternateur participe &agrave; cette op&eacute;ration, tout comme la batterie.");
