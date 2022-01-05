bc48a953337399faada8685ed549ca2800a97b3d
ChristopHe <christophe.hervi@free.fr>
2022-01-05 08:52:49 +0100

    première ébauche d'hydratation de page

--- Public/Modele/classe_Page.php
+++ Public/Modele/classe_Page.php
@@ -19,6 +19,14 @@ class Page extends PEUNC\classes\Page {
 		$this->setFooter("");
 		$this->setView("doctype.html");
 		// pas de feuille CSS supplémentaire à déclarer
+
+		// hydratation à partir de la BD
+		$this->BD = new PEUNC\classes\BDD;
+		$Thydrate = $this->BD->ResultatSQL("SELECT nom, texteMenu, ptiNomSupport, du_support, le_support, dossier, zip, image FROM Vue_HydratePage WHERE alpha = ? AND beta = ? AND gamma = ?", array($_SESSION['alpha'], $_SESSION['beta'], $_SESSION['gamma']));
+		if (isset($Thydrate))
+			list($this->nom,	$this->codeTitre,	$this->ptiNomSupport,	$this->du_support,	$this->le_support,	$this->dossier,	$this->zip,	$this->image) = $Thydrate[0];
+			//		nom				texteMenu			ptiNomSupport			du_support			le_support			dossier,		zip,		image
+		else $this->nom = $this->codeTitre = $this->tiNomSupport = $this->du_support = $this->le_support = $this->dossier = $this->zip = $this->image = null;
 	}
 /* ***************************
  * MUTATEURS (SETTER)
