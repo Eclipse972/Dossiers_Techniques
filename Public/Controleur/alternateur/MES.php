f7de0b1aa3affde0cf6d3f413d14d47cba16da9a
ChristopHe <christophe.hervi@free.fr>
2022-01-06 21:15:15 +0100

    rédaction de la description

--- Public/Controleur/alternateur/MES.php
+++ Public/Controleur/alternateur/MES.php
@@ -1,5 +1,4 @@
 <?php
-// Variables pour le controleur MiseEnSituation de l'alternateur
-
+// controleur Mise en situation de l'alternateur
 $this->setCommentaire("Les nombreux &eacute;l&eacute;ments &eacute;lectriques et &eacute;lectroniques mont&eacute;s sur la Citro&euml;n C4 HDI n&eacute;cessitent une source d&apos;&eacute;nergie &eacute;lectrique fiable et embarqu&eacute;e. L&apos;alternateur participe &agrave; cette op&eacute;ration, tout comme la batterie.");
 $this->setImage("/Supports/alternateur/images/mes.png");
