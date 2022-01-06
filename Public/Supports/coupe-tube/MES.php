bd6c013725398f9b577c3537b5a189ac77a0e3bf
ChristopHe <christophe.hervi@free.fr>
2022-01-06 23:09:34 +0100

    controleur MES du mini coupe-tube et c'est la dernière mise en situation de type page_image!

--- Public/Supports/coupe-tube/MES.php
+++
@@ -1,4 +0,0 @@
-<?php
-// Variables pour le controleur MiseEnSituation du cric hydraulique
-$texte = "Le coupe-tube est l&apos;un des outils manuels indispensables du plombier. Ce coupe-tube permet de sectionner des tubes de cuivre de 3 &agrave; 22 mm de diam&egrave;tre ext&eacute;rieur. Il existe des coupe-tubes de diff&eacute;rentes tailles en fonction des diam&egrave;tres de tubes &agrave; sectionner.";
-$image = "mini_coupe-tube.png";
