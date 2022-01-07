42195c0a50d50f58e7e47197e7d3a178d035aa24
ChristopHe <christophe.hervi@free.fr>
2022-01-07 01:49:40 +0100

    controleurs des page image de présentation des axes de la butée 5 axes

---
+++ Public/Controleur/butee5axes/axe1.php
@@ -0,0 +1,5 @@
+<?php
+// présentation de l'axe 1 de la butée 5 axes
+$this->setTitrePage("Pr&eacute;sentation de l&apos;axe 1");
+$this->setCommentaire("Rotation de 360&deg; de la pi&egrave;ce (2) autour de la pi&egrave;ce (1).");
+$this->setImage("/Supports/butee5axes/images/axe1.gif");
---
+++ Public/Controleur/butee5axes/axe3.php
@@ -0,0 +1,5 @@
+<?php
+// présentation de l'axe 3 de la butée 5 axes
+$this->setTitrePage("Pr&eacute;sentation de l&apos;axe 3");
+$this->setCommentaire("Rotation de 90&deg; de la pi&egrave;ce (6) autour de la pi&egrave;ce (3).");
+$this->setImage("/Supports/butee5axes/images/axe3.gif");
