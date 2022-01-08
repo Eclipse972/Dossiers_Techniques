df1f2cf51c918939aa9f029de021673f09bbb8ee
ChristopHe <christophe.hervi@free.fr>
2022-01-08 06:59:37 +0100

    première ébauche de la classe

--- Public/Modele/classe_Page_association.php
+++ Public/Modele/classe_Page_association.php
@@ -7,15 +7,16 @@ class Page_association extends Page {
  * Les fichiers sont au format edrawing de SolidWorks de préférence
  * */
 
-	private $image;		// image à afficher
-	private $fichier;	// fichier associé à l'image'
-	private $type;		// type d'asociation: dessin d'ensemble, éclaté, ...
+	private $image;			// image à afficher
+	private $fichier;		// fichier associé à l'image'
+	private $hauteur = 400;	// hauteur de l'image
 
  	public function __construct(array $TparamURL = []) {
 		parent::__construct($TparamURL);
 		// image et dossier instanciées dans le constructeur de la classe mère
 
 		// valeurs par défaut
+		$this->codeTitre = "Association image-fichier"; // créée dans la classe-mère
 		$this->setCSS(array("pageDT"));
 	}
 /* Le controleur a la structure suivante :
@@ -27,8 +28,26 @@ class Page_association extends Page {
  * MUTATEURS (SETTER)
  * ***************************/
 
+	// les différents types d'association
+	public function setDessinDensemble()	{ $this->codeTitre = "Dessin d&apos;ensemble"; }
+	public function setEclate()				{ $this->codeTitre = "&Eacute;clat&eacute;"; }
+	// fin de la liste
+
+	public function setHauteur($hauteur)	{ $this->hauteur = $hauteur; }
+
+	public function SetImage($image) {	// défini l'image à afficher
+	}
+
+	public function setFichier($fichier) {	// défini le fichier à télécharger avec recherche d'existence du fichier
+	}
+
 /* ***************************
  * ASSESSURS (GETTER)
  * ***************************/
+	public function getSection() { // redéfinition du code pour afficher la page
+		$code = $this->getTitrePage() . "<p>cliquez surl&apos;image pour t&eacute;l&eacute;charger le lichier</p>";
+		// création du lien iamge-fichier
 
+		return $code;
+	}
 }
