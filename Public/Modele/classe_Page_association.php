1b0f60f70b81ddefe32df4744f2d3063c377e7fe
ChristopHe <christophe.hervi@free.fr>
2022-01-09 01:41:46 +0100

    ajout attribut commentaireHTML dans la classe Page_association

--- Public/Modele/classe_Page_association.php
+++ Public/Modele/classe_Page_association.php
@@ -7,8 +7,9 @@ class Page_association extends Page {
  * Les fichiers sont au format edrawing de SolidWorks de préférence
  * */
 
-	private $image;			// nom de l'image à afficher avec son extension
-	private $fichier;		// nom du fichier associé à l'image avec son extension
+	private $image;				// nom de l'image à afficher avec son extension
+	private $fichier;			// nom du fichier associé à l'image avec son extension
+	private $commentaireHTML;	// code html du commentaire ajouté en dessous de l'image'
 
  	public function __construct(array $TparamURL = []) {
 		parent::__construct($TparamURL);
@@ -16,6 +17,7 @@ class Page_association extends Page {
 
 		// valeurs par défaut
 		$this->codeTitre = "Association image-fichier"; // créée dans la classe-mère
+		$this->commentaireHTML = null;
 		// pas de feuille de style supplémentaire à déclarer
 	}
 
@@ -32,6 +34,7 @@ class Page_association extends Page {
 	public function setEclate($titre = null)			{
 		$valeur = isset($titre) ? $titre : "&Eacute;clat&eacute;";
 		$this->setTitreAssociation($valeur);
+		$this->commentaireHTML = "<p style=\"text-align:center\">Dans e-Drawing, cliquez sur l&apos;ic&ocirc;ne <img src=\"/images/icone_eclater_rassembler.png\" alt = \"icone\"> pour &eacute;clater/rassembler la maquette num&eacute;rique</p>";
 	}
 	// fin de la liste
 
@@ -47,6 +50,9 @@ class Page_association extends Page {
 		$fichier = "Supports/{$this->dossier}fichiers/{$fichier}";
 		$this->fichier = (file_exists($fichier)) ? "/" . $fichier : "#";
 	}
+
+	public function setCommentaire($commentaire) { $this->commentaireHTML = $commentaire; }
+
 /* Le controleur a la structure suivante :
  * <?php
  * $this->setDessinDensemble() ou $this->setEclate() ou ...
@@ -62,6 +68,7 @@ class Page_association extends Page {
 	public function getSection() { // redéfinition du code pour afficher la page
 		return	$this->getTitrePage()	// titre de la page
 			.	"<p style=\"text-align:center\">Cliquez sur l&apos;image pour t&eacute;l&eacute;charger le fichier associ&eacute;.</p>\n"	// insctruction
-			.	"<a href=\"{$this->fichier}\">{$this->image}</a>\n";	// image est un lien
+			.	"<a href=\"{$this->fichier}\">{$this->image}</a>\n"					// image est un lien
+			.	(isset($this->commentaireHTML) ? $this->commentaireHTML : "");	// éventuel commentaire sous l'image
 	}
 }
