3fb9c960d69d9f0b3a7f9f281144bd0d3ce55137
ChristopHe <christophe.hervi@free.fr>
2022-01-09 03:38:51 +0100

    le nom de la pièce suffit pour les dessins de définition

--- Public/Controleur/cric_bouteille/axeArticulation.php
+++ Public/Controleur/cric_bouteille/axeArticulation.php
@@ -1,4 +1,4 @@
 <?php // dessin de définition de l'axe d'articulation du cric bouteille
-$this->setDessin2definition("Dessin de d&eacute;finition de l&apos;axe d&apos;articulation");
+$this->setDessin2definition("de l&apos;axe d&apos;articulation");
 $this->setImage("def_axe_articulation.png");
 $this->setFichier("def_axe_articulation.EDRW");
