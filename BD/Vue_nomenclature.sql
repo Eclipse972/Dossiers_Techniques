eae1be1607491d5d67697274accdf5a666467769
ChristopHe <christophe.hervi@free.fr>
2022-01-09 08:07:26 +0100

    création de la clase page nomenclature

--- BD/Vue_nomenclature.sql
+++ BD/Vue_nomenclature.sql
@@ -1,38 +1,26 @@
--- phpMyAdmin SQL Dump
--- version 3.1.5
--- http://www.phpmyadmin.net
---
--- Serveur: dossiers.techniques.sql.free.fr
--- Généré le : Ven 17 Janvier 2020 à 21:01
--- Version du serveur: 5.0.83
--- Version de PHP: 5.3.9
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Base de données: 'dossiers_techniques'
---
-
--- --------------------------------------------------------
-
---
--- Structure de la vue 'Vue_nomenclature'
---
-
-CREATE ALGORITHM=UNDEFINED DEFINER=`dossiers.techniques`@`172.20.%` SQL SECURITY DEFINER VIEW dossiers_techniques.Vue_nomenclature AS select dossiers_techniques.Supports.ID AS support_ID,concat(_utf8'<td>',cast(dossiers_techniques.Pieces.repere as char(2) charset utf8),_utf8'</td>') AS repere,concat(_latin1'<td><a href="Supports/',dossiers_techniques.Supports.dossier,_latin1'/fichiers/',dossiers_techniques.Pieces.fichier,convert(if((dossiers_techniques.Pieces.assemblage = 0),_utf8'.EPRT',_utf8'.EASM') using latin1),_latin1'"><img src="Supports/',dossiers_techniques.Supports.dossier,_latin1'/images/',dossiers_techniques.Pieces.fichier,_latin1'.png" alt="',dossiers_techniques.Pieces.nom,_latin1'"></a>',_latin1'</td>') AS lien_image,concat(_utf8'<td>',convert(dossiers_techniques.Pieces.nom using utf8),if((dossiers_techniques.Pieces.quantite > 1),concat(_utf8' (x',cast(dossiers_techniques.Pieces.quantite as char(2) charset utf8),_utf8')'),_utf8''),_utf8'</td>') AS designation,concat(_latin1'<td>',if((dossiers_techniques.Materiaux.URL_wiki = _latin1''),dossiers_techniques.Materiaux.formule,concat(_latin1'<a href="https://fr.wikipedia.org/wiki/',dossiers_techniques.Materiaux.URL_wiki,_latin1'" target="_blank">',dossiers_techniques.Materiaux.formule,_latin1'</a>')),_latin1'</td>') AS matiere,concat(_latin1'<td>',dossiers_techniques.Pieces.observation,_latin1'</td>') AS observation from ((dossiers_techniques.Supports join dossiers_techniques.Pieces) join dossiers_techniques.Materiaux) where ((dossiers_techniques.Pieces.matiere_ID = dossiers_techniques.Materiaux.ID) and (dossiers_techniques.Supports.ID = dossiers_techniques.Pieces.support_ID)) order by dossiers_techniques.Supports.ID,dossiers_techniques.Pieces.repere;
-
---
--- VIEW  'Vue_nomenclature'
--- Données: aucune
---
-
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+DROP VIEW IF EXISTS Vue_nomenclature;
+CREATE VIEW Vue_nomenclature AS
+SELECT
+	Supports.ID AS support_ID,
+	CONCAT('<td>',Pieces.repere,'</td>') AS repere,
+	CONCAT(
+		'<td><a href="/Supports/',Supports.dossier,'/fichiers/',Pieces.fichier,if((Pieces.assemblage = 0),'.EPRT','.EASM'),
+		'"><img src="/Supports/',Supports.dossier,'/images/',Pieces.fichier,'.png" alt="',Pieces.nom,'"></a>','</td>') AS lien_image,
+	CONCAT(
+		'<td>',
+		Pieces.nom,
+		if((Pieces.quantite > 1),
+			CONCAT(' (x',Pieces.quantite,')'),
+			''),
+		'</td>') AS designation,
+	CONCAT(
+		'<td>',
+		if((Materiaux.URL_wiki = ''),
+			Materiaux.formule,
+			CONCAT('<a href="https://fr.wikipedia.org/wiki/',Materiaux.URL_wiki,'" target="_blank">',Materiaux.formule,'</a>')),
+		'</td>') AS matiere,
+	CONCAT('<td>',Pieces.observation,'</td>') AS observation
+FROM Supports
+INNER JOIN Pieces ON Supports.ID = Pieces.support_ID
+INNER JOIN Materiaux ON Pieces.matiere_ID = Materiaux.ID
+ORDER BY Supports.ID, Pieces.repere
---
+++ Public/Modele/classe_Page_nomenclature.php
@@ -0,0 +1,55 @@
+<?php
+require"Modele/classe_Page.php";
+
+class Page_nomenclature extends Page {
+	private $colonne_matière_vide;		// colone matière existe
+	private $colonne_observation_vide;	// colone observation existe
+
+	public function __construct(array $TparamURL = []) {
+		parent::__construct($TparamURL);
+		//$this->setCSS(["nomenclature.css"]);
+		$this->setView("nomenclature.html");
+	}
+
+/* ***************************
+ * MUTATEURS (SETTER)
+ * ***************************/
+
+	public function SetColonneMatiereVide($booleen)		{ $this->colonne_matière_vide = $booleen; }
+	public function SetColonneObservationVide($booleen)	{ $this->colonne_observation_vide = $booleen; }
+
+/*	exemple de contrôleur:
+ *	<?php
+ * $this->SetColonneMatiereVide(false);
+ * $this->SetColonneObservationVide(true);
+ */
+
+/* ***************************
+ * AUTRE
+ * ***************************/
+	public function InfoMatiere() {
+		$code = ($this->colonne_matière_vide) ? "" : "\t<p>Cliquez sur le nom de la mati&egrave;re pour trouver sa définition sur wikip&eacute;dia dans un nouvel onglet.</p>\n";
+		return $code;
+	}
+
+	public function ColonnesSupplementaires() {
+		$code =		($this->colonne_matière_vide)	 	? "" : "<th>Mati&egrave;re</th>" ;
+		$code .=	($this->colonne_observation_vide)	? "" : "<th>Observations</th>";
+		return $code . "\n";
+	}
+
+	public function CorpsNomenclature() {
+		global $BD;
+		$Tnomenclature = $BD->ResultatSQL('SELECT * FROM Vue_nomenclature WHERE support_ID = ? - 2', [$_SESSION['alpha']]);	// -2 car accueil (alpha =0) et contact (alpha=1)
+		$code = "";
+		if (isset($Tnomenclature))
+			foreach ($Tnomenclature as $ligne)	{
+				$code .= "\t<tr>\n\t\t{$ligne['repere']}\n\t\t{$ligne['lien_image']}\n\t\t{$ligne['designation']}\n";
+				$code .= ($this->colonne_matière_vide)	 	? "" : "\t\t{$ligne['matiere']}\n" ;
+				$code .= ($this->colonne_observation_vide)	? "" : "\t\t{$ligne['observation']}\n" ;
+				$code .= "\t</tr>\n";
+			}
+		else throw new \Exception("Nomenclature inexistante");
+		return $code;
+	}
+}
