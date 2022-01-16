d0ac3fa4c397bb09d0e549e76d6bbcdaf1889694
ChristopHe <christophe.hervi@free.fr>
2022-01-16 04:30:37 +0100

    controleurs pour les classes d'équivalence de l'étau

---
+++ Public/Controleur/etau/CE.php
@@ -0,0 +1,17 @@
+<?php	// présentation es classe d'équivalence de l'étau de modéliste
+ob_start();	// début du code <section>
+?>
+<h1>Classes d&apos;&eacute;quivalence</h1>
+<p>L&apos;&eacute;tau est compos&eacute; de quatre classes d&apos;&eacute;quivalence et de quatre liaisons.</p>
+<p>Mors fixe</p>
+<img src="/Supports/etau/images/CE_mors_fixe.png" class="association" alt="Mors fixe">
+<p>Mors mobile</p>
+<img src="/Supports/etau/images/CE_mors_mobile.png" class="association" alt="Mors mobile">
+<p>Vis de manoeuvre</p>
+<img src="/Supports/etau/images/CE_vis_de_manoeuvre.png" class="association" alt="Vis de manoeuvre">
+<p>Tige de poign&eacute;e</p>
+<img src="/Supports/etau/images/CE_tige_de_poignee.png" class="association" alt="Tige de poignée">
+<?php
+$tampon = ob_get_contents();
+ob_end_clean();
+$this->setSection($tampon);
--- Public/Supports/etau/CE.php
+++
@@ -1,2 +0,0 @@
-<h1>Classe d&apos;&eacute;quivalence</h1>
-<p>l&apos;&eacute;tau est compos&eacute; de quatre classes d&apos;&eacute;quivalence et de quatre liaisons.</p>
