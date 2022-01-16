ad67513f97a9e1c5bdb717eaa8a7e3b1cdedb265
ChristopHe <christophe.hervi@free.fr>
2022-01-16 05:39:35 +0100

    création du titre de la page 'à propos'

--- Public/Controleur/Apropos.php
+++ Public/Controleur/Apropos.php
@@ -2,8 +2,8 @@
 ob_start();	// début du code <section>
 ?>
 	<div id="page_image">
-	<h1>D&eacute;sol&eacute; !</h1>
-	<p align=center>Il semblerai que je n&apos;ai pas r&eacute;dig&eacute; la page "&agrave; propos" de ce support.</p>
+	<h1>A propos <?=$this->getDu_support()?></h1>
+	<p align=center>Il semblerai que je n&apos;ai pas r&eacute;dig&eacute; cette page!</p>
 	<?=$this->getLogo()?>
 	<p align=center>N&apos;h&eacute;sitez pas &agrave; me contacter (lien en bas de page) si le probl&egrave;me persiste.</p>
 	</div>
