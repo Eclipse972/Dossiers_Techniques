4e0ea52ca060e890560ddba987e4709837ca662a
ChristopHe <christophe.hervi@free.fr>
2022-01-20 15:00:41 +0100

    scrits du cric hydraulique remplacés par des controleurs

--- Public/Supports/cric_hydraulique/AF.php
+++
@@ -1,8 +0,0 @@
-<h1>Analyse fonctionnelle</h1>
-<p>Cette partie contient trois documents:</p>
-<ul>
-<li>Diagramme des int&eacute;racteurs</li>
-<li>Diagramme FAST "Levage du v&eacute;hicule"</li>
-<li>Diagramme FAST "D&eacute;pose du v&eacute;hicule"</li>
-</ul>
-<p>Cliquez dans le sous-menu pour plus de pr&eacute;cisions.</p>
