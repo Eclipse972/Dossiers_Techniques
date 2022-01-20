2eda7681578a9c5492590255298b5356271932df
ChristopHe <christophe.hervi@free.fr>
2022-01-20 04:06:16 +0100

    ajout de la mention page en construction

--- Public/Controleur/pageDTenConstruction.php
+++ Public/Controleur/pageDTenConstruction.php
@@ -2,7 +2,7 @@
 ob_start();	// début du code <section>
 ?>
 	<div id="page_image">
-	<h1>D&eacute;sol&eacute; !</h1>
+	<h1>D&eacute;sol&eacute; ! Page en construction</h1>
 	<p align=center>Il semblerai que je n&apos;ai pas r&eacute;dig&eacute; cette page.</p>
 	<?$this->getLogo()?>
 	<p align=center>N&apos;h&eacute;sitez pas &agrave; me contacter (lien en bas de page) si le probl&egrave;me persiste.</p>
