4fd52e08311af768c9b0f2d913da3c0b1cbc4cf6
ChristopHe <christophe.hervi@free.fr>
2022-01-20 15:03:28 +0100

    scrits du préhenseur de culasse remplacés par des controleurs

--- Public/Supports/prehenseur/eclate_CE.php
+++
@@ -1,2 +0,0 @@
-<h1>&Eacute;clat&eacute; en classes d&apos;&eacute;quivalence</h1>
-<img src="<?=$Dossier_images?>eclate_CE.png" alt="&eacute;clat&eacute; en classe d&apos;&eacute;quivalence">
--- Public/Supports/prehenseur/tige.php
+++
@@ -1,2 +0,0 @@
-<h1>&Eacute;volution du d&eacute;placement de la TIGE du v&eacute;rin par rapport au CHÂSSIS en fonction du temps.</h1>
-<img src="Supports/prehenseur/images/tige.png" class="association" style=height:400px; alt="courbe de d&eacute;placement de la tige">
