5e7e784211d665e187bae361d0d15eaaa70e434a
ChristopHe <christophe.hervi@free.fr>
2022-01-20 14:59:46 +0100

    scrits du cric bouteille remplacés par des controleurs

--- Public/Supports/cric_bouteille/descente.php
+++
@@ -1,6 +0,0 @@
-<img src="<?=$Dossier_images?>action_manuelle_descente.png" style="height:400px; float: right;" alt="action pour la descente">
-<h1>Descente</h1>
-<p>La descente de la charge est obtenue par desserrage de la  vis de d&eacute;charge.</p>
-<h1>Du point de vue interne</h1>
-<p>L&apos;huile contenu dans la chambre de v&eacute;rin retourne dans le r&eacute;servoir. La tige de v&eacute;rin descend sous l&apos;action de la charge.</p>
-<img src="<?=$Dossier_images?>decharge.png" class=image_centree style=width:400px alt="Circulation de l&apos;huile">
--- Public/Supports/cric_bouteille/fonctionnement.php
+++
@@ -1,2 +0,0 @@
-<h1>Fonctionnement</h1>
-<p>Le cric bouteille fonctionne suivant deux modes: la mont&eacute;e et la descente (voir le sous-menu). Ils seront d&eacute;crit du point de vue externe (pour l'utilisateur) et du point de vue interne.</p>
