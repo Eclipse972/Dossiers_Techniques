d625e46ac152740d84d47dbbe0c92f5034e811d6
ChristopHe <christophe.hervi@free.fr>
2022-01-20 14:56:08 +0100

    fichiers inutiles de la butée 5 axes

--- Public/Supports/butee5axes/dessins2definition.php
+++
@@ -1,2 +0,0 @@
-<h1>Dessins de d&eacute;finition</h1>
-<p>Les dessins de d&eacute;finition de certaines pi&egrave;ces de la but&eacute;e 5 axes sont disponibles. La liste est affich&eacute;e dans le menu.</p>
