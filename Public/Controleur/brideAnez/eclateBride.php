69f40634e69ac1c9fea3b8fdf50c49dbf26a8e38
ChristopHe <christophe.hervi@free.fr>
2022-01-20 04:04:08 +0100

    utilisation du script pageDTenConstruction

--- Public/Controleur/brideAnez/eclateBride.php
+++
@@ -1,6 +0,0 @@
-<?php // controleur éclaté bride
-$this->setTitrePage("D&eacute;sol&eacute; !");
-$this->setCommentaire("Il semblerai que je n&apos;ai pas r&eacute;dig&eacute; cette page.</p><p align=center>N&apos;h&eacute;sitez pas &agrave; me contacter (lien en bas de page) si le probl&egrave;me persiste.");
-$this->setImage("/Supports/brideAnez/images/bride.png");
-$this->setAlt("&eacute;clat&eacute; de l&apos;alternateur");
-$this->setHauteur("400");
