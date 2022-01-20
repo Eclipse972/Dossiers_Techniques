0013e4b3cf57329d00f0c1268dcfcb7fcbce1292
ChristopHe <christophe.hervi@free.fr>
2022-01-20 02:01:19 +0100

    création de tous les controleurs de la cambreuse

---
+++ Public/Controleur/cambreuse/caracteristiques.php
@@ -0,0 +1,19 @@
+<?php // fonctionnement cambreuse
+ob_start();	// début du code <section>
+?>
+	<h1>Caract&eacute;ristiques &eacute;nerg&eacute;tiques</h1>
+	<h2>V&eacute;rin pneumatique de bridage :</h2>
+	<ul>
+	<li>Ø Piston : 80 mm</li>
+	<li>Pression d&apos;utilisation : 0,6 N/mm²</li>
+	<li>Course du piston : 50 mm</li>
+	</ul>
+	<h2>V&eacute;rin pneumatique de cambrage :</h2>
+	<ul>
+	<li>Ø Piston : 63 mm</li>
+	<li>Pression d&apos;utilisation : 0,6 N/mm²</li>
+	<li>Course du piston : 50 mm</li>
+<?php
+$tampon = ob_get_contents();
+ob_end_clean();
+$this->setSection($tampon);
