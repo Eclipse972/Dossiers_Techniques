44056789f93a1c4139b207b708bcb62bcb543742
ChristopHe <christophe.hervi@free.fr>
2022-01-22 07:09:46 +0100

    ébauche page à propos avec lien zip et retour au dossier technique

--- Public/Controleur/Apropos.php
+++ Public/Controleur/Apropos.php
@@ -1,13 +1,1 @@
 <?php // page à propos en construction
-ob_start();	// début du code <section>
-?>
-	<div id="page_image">
-	<h1>A propos <?=$this->getDu_support()?></h1>
-	<p align=center>Il semblerai que je n&apos;ai pas r&eacute;dig&eacute; cette page!</p>
-	<?=$this->getLogo()?>
-	<p align=center>N&apos;h&eacute;sitez pas &agrave; me contacter (lien en bas de page) si le probl&egrave;me persiste.</p>
-	</div>
-<?php
-$tampon = ob_get_contents();
-ob_end_clean();
-$this->setSection($tampon);
--- Public/Modele/classe_PageApropos.php
+++ Public/Modele/classe_PageApropos.php
@@ -5,9 +5,21 @@ class PageApropos extends Page {
 	private $zip;
 
 	public function __construct(array $TparamURL = []) {
-		global $BD;
 		parent::__construct($TparamURL);
 		$this->setView("Apropos.html");
 		// pas de feuille de style supplémentaire autre que commun.css chargé par la vue
+		$this->zip = "Supports/" . $this->dossier . "fichiers/" . $this->ptiNomSupport . ".zip";
 	}
+
+	public function LienZip() {
+		global $BD;
+		if (file_exists($this->zip)) {
+			$code = "<a href=/{$this->zip}>Cliquez ici pour t&eacute;l&eacute;charger l&apos;archive ZIP de la maquette num&eacute;rique</a>\n";
+			// infos sur l'archive
+			$code .= "\t<p>pas d&apos;infos particuli&egrave;re</p>";
+		} else $code = "<p>Archive zip non disponible</p>\n";
+		return $code;
+	}
+
+	public function LienRetour() { return "/" . $this->dossier . "MES"; }
 }
