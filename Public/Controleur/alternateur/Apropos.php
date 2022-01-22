83dfdfaef3a61c0a90c5df900da92f594875694b
ChristopHe <christophe.hervi@free.fr>
2022-01-23 00:20:46 +0100

    description de l'archive de l'alternateur

--- Public/Controleur/alternateur/Apropos.php
+++ Public/Controleur/alternateur/Apropos.php
@@ -1,1 +1,3 @@
 <?php // à propos de l'alternateur
+// archive définie par défaut
+$this->setDescriptionZip("\t<ul>\n\t\t<li>contient deux configuraions compl&eacute;mentaires</li>\n\t\t<li>dessin de l&apos;&eacute;clat&eacute;</li>\n\t</ul>\n");
