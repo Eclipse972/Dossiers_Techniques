bab2c9c8172c46d71edfdb0546716499e1cf3d9b
ChristopHe <christophe.hervi@free.fr>
2022-01-22 06:04:01 +0100

    création du constructeur

--- Public/Modele/classe_PageApropos.php
+++ Public/Modele/classe_PageApropos.php
@@ -3,4 +3,10 @@ require"Modele/classe_Page.php";
 
 class PageApropos extends Page {
 	private $zip;
+
+	public function __construct(array $TparamURL = []) {
+		global $BD;
+		parent::__construct($TparamURL);
+		$this->setView("Apropos.html");
+	}
 }
