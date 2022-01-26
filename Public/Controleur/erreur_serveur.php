5934cbf36045cf676194283797c0b3f2703a30aa
ChristopHe <christophe.hervi@free.fr>
2022-01-26 23:19:18 +0100

    erreur sur l'espace de nom

--- Public/Controleur/erreur_serveur.php
+++ Public/Controleur/erreur_serveur.php
@@ -1,7 +1,7 @@
 <?php
 $this->setCodeErreur($_SESSION['beta']);
 
-$BD = new\PEUNC\classes\BDD;
+$BD = new\PEUNC\BDD;
 $this->setTitreErreur($BD->TexteErreur($_SESSION['beta']));
 
 $this->setCorpsErreur("<p>Image &agrave; venir</p>");
