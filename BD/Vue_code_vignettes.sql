d43a76ecc31f9cef2b1f13920ebc20c3412d4f19
ChristopHe <christophe.hervi@free.fr>
2022-01-27 14:59:44 +0100

    ajout de l'article

--- BD/Vue_code_vignettes.sql
+++ BD/Vue_code_vignettes.sql
@@ -1,7 +1,13 @@
 DROP VIEW IF EXISTS Vue_code_vignettes;
 CREATE VIEW Vue_code_vignettes AS
 SELECT CONCAT(
-	'<td><a href=/',dossier,'/MES>'
+	'<td><a href=/',dossier,'/MES title="dossier technique ',
+	CASE article_ID
+		WHEN 1 THEN 'du '
+		WHEN 2 THEN 'de la '
+		ELSE 'de l&apos;'
+	END,
+	nom,'">'-- fin du lien
 	,nom
 	,'<br><img src=Supports/',dossier,'/images/',pti_nom,'.png alt="',nom,'"></a></td>'
 ) AS `code`
