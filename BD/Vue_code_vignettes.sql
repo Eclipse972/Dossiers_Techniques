f393079372c32f17c0c32d0a03707ba64f9bada0
ChristopHe <christophe.hervi@free.fr>
2022-01-27 14:44:40 +0100

    suppression des guillemets inutiles

--- BD/Vue_code_vignettes.sql
+++ BD/Vue_code_vignettes.sql
@@ -1,9 +1,9 @@
 DROP VIEW IF EXISTS Vue_code_vignettes;
 CREATE VIEW Vue_code_vignettes AS
 SELECT CONCAT(
-	'<td><a href="/',dossier,'/MES">'
+	'<td><a href=/',dossier,'/MES>'
 	,nom
-	,'<br><img src="Supports/',dossier,'/images/',pti_nom,'.png" alt="',nom,'"></a></td>'
+	,'<br><img src=Supports/',dossier,'/images/',pti_nom,'.png alt="',nom,'"></a></td>'
 ) AS `code`
 
 FROM Supports
