eb8d28f4ad0c44986248ef2546f5b6cd180f6f88
ChristopHe <christophe.hervi@free.fr>
2022-01-29 08:17:02 +0100

    création des variables memebre de la classe

--- Public/PEUNC/classes/Contact.php
+++ Public/PEUNC/classes/Contact.php
@@ -2,6 +2,45 @@
 // classes page de contact de PEUNC
 namespace PEUNC;
 
+// les réponses du formulaire
+protected $nom;
+protected $courriel;
+protected $objet;
+protected $message;
+protected $code;
+// erreurs sur les champs
+protected $Erreur_nom;
+protected $Erreur_courriel;
+protected $Erreur_objet;
+protected $Erreur_message;
+
+protected $spam_détecté; // tentative de spam détectée
+
+/*
+ * Le code de validation est un mot de 5 caractères composé d'une lettre de chaque champ (soit 4 lettres).
+ * Pour le choix du caractère il y a quatre possibilités: premier, deuxième, avant dernier et dernier
+ * la dernière lettre est une des 4 premières du code.
+ *
+ * Exemple de validation:
+ * 	deuxième caractère de l'objet
+ * 	avant dernier caractère du message
+ *  deuxième caractère de votre courriel
+ *  dernier caractère de votre nom
+ *  quatrième caractère de ce code de validation
+ * Si les champs sont :
+ * 		nom =  Eclipse972
+ * 		courriel = christophe.hervi@free.fr
+ * 		objet = Question
+ * 		message = Pourquoi un code si compliqué?
+ * Le code de validation sera uéh22
+*/
+
+// variables nécessaires à la création du code de validation
+protected $T_id_champ;	// tableau contenant les numéros de champ
+protected $T_choix;		// tableau contenant les positions demandées
+protected $dernier_choix; // dernière instruction: position du caractère du code de validation
+protected $top_départ;	// moment où est affiché le formulaire
+
 class Contact extends Page {
 	public function __construct() {
 		parent::__construct();
