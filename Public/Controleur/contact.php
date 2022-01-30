32f74e019765f4877d5955d22260eb865299d4e2
ChristopHe <christophe.hervi@free.fr>
2022-01-30 08:08:36 +0100

    déplacement des méthodes du controleur vers la classe page de contact

--- Public/Controleur/contact.php
+++ Public/Controleur/contact.php
@@ -1,32 +1,21 @@
 <?php	// controleur du formulaire
-if(empty($_POST)) {
+if(empty($_POST))
+{	// préparation du formulaire
 	$_SESSION["formulaire"]["ObjValidation"] = serialize(new PEUNC\CodeValidation);
 	$_SESSION["formulaire"]["TopDépart"] = time();
 	$_SESSION["formulaire"]["URLretour"] = $this->URLprecedente();
-} else {
-	if (time() - $_SESSION["formulaire"]["TopDépart"] < 8 ) { // trop rapide pour être humain
+}
+else
+{	// traitement du formulaire
+	if($this->SpamDétecté())
+	{	// tentattive de spam
 		header("Location:/"); // Redirection
 		exit;
 	}
-	// lecture du tableau $_POST
-	foreach ($_POST as $clé => $valeur)
-		if (in_array($clé, array("nom", "courriel", "objet", "message", "code"))) // clé autorisée ?
-			$_SESSION["formulaire"][$clé] = strip_tags($valeur); // on stocke la valeur dans la session
-		else { // clé inconnue
-			header("Location:/"); // Redirection
-			exit;
-		}
 
-	//test de validité des champs;
-	$_SESSION["formulaire"]["ErreurNom"] = (strlen($_SESSION["formulaire"]["nom"]) < 2);
-	$_SESSION["formulaire"]["ErreurCourriel"] = (preg_match('#^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$#', $_SESSION["formulaire"]["courriel"]) != 1);
-	$_SESSION["formulaire"]["ErreurObjet"] = (strlen($_SESSION["formulaire"]["objet"]) < 2);
-	$_SESSION["formulaire"]["ErreurMessage"] = (strlen($_SESSION["formulaire"]["message"]) < 2);
+	$this->SauvegardeChampsFormulaire();	// lecture du tableau $_POST
 
-	if (	$_SESSION["formulaire"]["ErreurNom"]
-		||	$_SESSION["formulaire"]["ErreurCourriel"]
-		||	$_SESSION["formulaire"]["ErreurObjet"]
-		||	$_SESSION["formulaire"]["ErreurMessage"])
+	if (!$this->ChampsFormulaireValides())
 	{
 		header("Location:/Contact"); // Redirection vers le formulaire de contact car il y a erreur
 		exit;
--- Public/PEUNC/classes/Contact.php
+++ Public/PEUNC/classes/Contact.php
@@ -4,11 +4,9 @@ namespace PEUNC;
 
 require "PEUNC/classes/CodeValidation.php";
 
-class Contact extends Page {
-
-	public function __construct() {
-		parent::__construct();
-	}
+class Contact extends Page
+{
+	public function __construct() { parent::__construct(); }
 /*
  * des informations sur le formulaire sont sauvegardées dans une tableau associatif de la forme $_SESSION["formulaire"]["données"]
  * données =...
@@ -26,7 +24,61 @@ class Contact extends Page {
 // GETTERS ==============================================================================
 
 // AUTRES ==============================================================================
-	public function AfficherCodeValidation() {
+	public function SpamDétecté()
+	{
+		$spam = (time() - $_SESSION["formulaire"]["TopDépart"] < 5); // trop rapide pour être humain
+		if (!$spam) // le premier test est passé?
+		{	// 2e test: pas de champ inconnue
+			foreach ($_POST as $clé => $valeur)
+			{
+				if (!in_array($clé, array("nom", "courriel", "objet", "message", "code")))
+				{	// clé non autorisée
+					$spam = true;
+					break;
+				}
+			}
+		}
+		if (!$spam) // les 2 premiers sont passés?
+		{	// 3e test: tous les champs sont-il là?
+			$tableau = array("nom", "courriel", "objet", "message", "code");
+			foreach($tableau as $clé => $valeur)
+			{
+				if (!isset($_POST[$valeur]))
+				{
+					$spam = true;
+					break;
+				}
+			}
+		}
+		return $spam;
+	}
+
+	public function SauvegardeChampsFormulaire()
+	{	// après avoir exécuté la méthode SpamDétecté, il ne reste que les champs nécessaires
+		foreach ($_POST as $clé => $valeur)
+			$_SESSION["formulaire"][$clé] = strip_tags($valeur); // on stocke la valeur dans la session après l'avoir nettoyée un peu
+	}
+
+	public function ChampsFormulaireValides()
+	{
+		//test de validité des champs;
+		$_SESSION["formulaire"]["ErreurNom"] = (strlen($_SESSION["formulaire"]["nom"]) < 2);
+		$_SESSION["formulaire"]["ErreurCourriel"] = (preg_match('#^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$#', $_SESSION["formulaire"]["courriel"]) != 1);
+		$_SESSION["formulaire"]["ErreurObjet"] = (strlen($_SESSION["formulaire"]["objet"]) < 2);
+		$_SESSION["formulaire"]["ErreurMessage"] = (strlen($_SESSION["formulaire"]["message"]) < 2);
+		$_SESSION["formulaire"]["ErreurCode"] = (strlen($_SESSION["formulaire"]["code"]) <> 5);
+
+		return
+		(		$_SESSION["formulaire"]["ErreurNom"]
+			||	$_SESSION["formulaire"]["ErreurCourriel"]
+			||	$_SESSION["formulaire"]["ErreurObjet"]
+			||	$_SESSION["formulaire"]["ErreurMessage"]
+			||	$_SESSION["formulaire"]["ErreurCode"]
+		);
+	}
+
+	public function AfficherCodeValidation()
+	{
 		$ObjValidation = unserialize($_SESSION["formulaire"]["ObjValidation"]);
 		return $ObjValidation->Afficher();
 	}
