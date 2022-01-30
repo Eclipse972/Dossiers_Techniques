a7dd4eee50cd4781007e539f98946ac6e6141e04
ChristopHe <christophe.hervi@free.fr>
2022-01-30 06:19:45 +0100

    sauvegarde de l'URL de retour de formulaire

--- Public/Controleur/contact.php
+++ Public/Controleur/contact.php
@@ -1,3 +1,4 @@
 <?php	// controleur du formulaire
 $_SESSION["formulaire"]["ObjValidation"] = serialize(new PEUNC\CodeValidation);
 $_SESSION["formulaire"]["TopDépart"] = time();
+$_SESSION["formulaire"]["URLretour"] = $this->URLprecedente();
