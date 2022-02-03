d6024ab323f3caa2f03634c3eba8604fd979c76a
ChristopHe <christophe.hervi@free.fr>
2022-02-03 19:29:46 +0100

    réorganisation du code pour avoir une unique redirection en fin de script

--- Public/Controleur/contact.php
+++ Public/Controleur/contact.php
@@ -8,28 +8,24 @@ if(empty($_POST))
 }
 else
 {	// traitement du formulaire
-	if ($this->SpamDétecté())
-	{	// tentative de spam
-		header("Location:/"); // Redirection
-		exit;
-	}
-
-	$this->SauvegardeChampsFormulaire();	// lecture du tableau $_POST
-
-	if (!$this->FormulaireOK())
+	if ($this->SpamDétecté())	// tentative de spam?
+		$URL = "/";
+	else
 	{
-		header("Location:/Contact"); // Redirection vers le formulaire de contact car il y a erreur
-		exit;
-	}
+		$this->SauvegardeChampsFormulaire();	// lecture du tableau $_POST
 
-	// les drapeaux des erreurs sont baissés
-	$_SESSION["formulaire"]["ErreurNom"] = $_SESSION["formulaire"]["ErreurCourriel"] = $_SESSION["formulaire"]["ErreurObjet"] = $_SESSION["formulaire"]["ErreurMessage"] = false;
+		if ($this->FormulaireOK())
+		{
+			// les drapeaux des erreurs sont baissés
+			$_SESSION["formulaire"]["ErreurNom"] = $_SESSION["formulaire"]["ErreurCourriel"] = $_SESSION["formulaire"]["ErreurObjet"] = $_SESSION["formulaire"]["ErreurMessage"] = false;
 
-	if ($this->EnvoyerMessage())	// l'envoi du courriel s'est bien déroulée?
-	{
-		$_SESSION["formulaire"]["objet"] = $_SESSION["formulaire"]["message"] = ""; // le nom et le courriel sont conservés
-		header("Location:" . $_SESSION["formulaire"]["URLretour"]); // redirection vers page précédente
-	} else {
-		header("Location:/Contact"); // Redirection vers le formulaire de contact car il y a eut un pb avec 'envoi du courriel
+			if ($this->EnvoyerMessage())	// l'envoi du courriel s'est bien déroulée?
+			{
+				$_SESSION["formulaire"]["objet"] = $_SESSION["formulaire"]["message"] = ""; // le nom et le courriel sont conservés
+				$URL = $_SESSION["formulaire"]["URLretour"]; // redirection vers page précédente
+			} else $URL = "/Contact"; // Redirection vers le formulaire de contact car il y a eut un pb avec 'envoi du courriel
+		}
+		else $URL = "/Contact"; // Redirection vers le formulaire de contact car il y a erreur
 	}
+	header("Location:" . $URL); // redirection
 }
