91fb60884100b527900609b59eec3d61524cf585
ChristopHe <christophe.hervi@free.fr>
2022-02-04 21:09:54 +0100

    ajout dans la BD de la méthode de la requête et la liste des paramètres autorisés au format JSON

--- BD/PEUNC/Vue_URLvalides.sql
+++ BD/PEUNC/Vue_URLvalides.sql
@@ -1,4 +1,4 @@
-# génère la totalité des URL valides du site
+# génère la totalité des URL valides du site avec la méthode de la requête et la liste des paramètres autorisés au format JSON
 DROP VIEW IF EXISTS Vue_URLvalides;
 CREATE VIEW Vue_URLvalides AS
 SELECT
@@ -11,6 +11,8 @@ SELECT
 	CONCAT('/',(SELECT ptiNom FROM Squelette WHERE alpha = (SELECT niveau1) AND beta = 0 AND gamma = 0)
 		,IF(ISNULL((SELECT texte2)),'',(SELECT texte2))
 		,IF(ISNULL((SELECT texte3)),'',(SELECT texte3))
-	) AS URL
+	) AS URL,
+	methode,
+	paramAautorise
 FROM Squelette
 ORDER BY niveau1 ASC, niveau2 ASC, niveau3 ASC
