767b374078e2d4f81b7d80f8a11f577ad2743427
ChristopHe <christophe.hervi@free.fr>
2022-02-05 18:52:53 +0100

    nettoyage du code en lien avec l'autoloader

--- Public/index.php
+++ Public/index.php
@@ -6,12 +6,7 @@ require 'PEUNC/classes/BDD.php';
 require 'PEUNC/classes/RequeteHttp.php';
 
 // autoloader pour les classes utilisateur
-spl_autoload_register(
-	function($classe)
-	{
-		require_once"Modele/classe_{$classe}.php";
-	}
-);
+spl_autoload_register(function($classe)	{ require_once"Modele/classe_{$classe}.php"; });
 
 try
 {
@@ -24,15 +19,11 @@ try
 
 	PEUNC\Page::SauvegardeEtat();	// sauvegarde de l'état courant
 	list($_SESSION["PEUNC"]['alpha'], $_SESSION["PEUNC"]['beta'], $_SESSION["PEUNC"]['gamma']) = [$requete->getAlpha(), $requete->getBeta(), $requete->getGamma()];// MAJ de l'état'
-	// $_SESSION = array(...) détruirait les autres éventuels paramètres stockés dans la session
-
-	//require"Modele/classe_{$classePage}.php";
 	$PAGE = new $classePage(explode("/", $paramPage));
 	$PAGE->ExecuteControleur($_SESSION["PEUNC"]['alpha'], $_SESSION["PEUNC"]['beta'], $_SESSION["PEUNC"]['gamma']);
 }
 catch(Exception $e)
 {
-	//require"Modele/classe_PageErreur.php";
 	$PAGE = new PageErreur;
 	$PAGE->setCodeErreur("application");
 	$PAGE->setTitreErreur($e->getMessage());
