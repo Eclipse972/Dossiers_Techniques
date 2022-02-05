9e6888cd05c52d3ccf0ca8581d790406b22e70f0
ChristopHe <christophe.hervi@free.fr>
2022-02-05 18:40:57 +0100

    autoloader pour les classs utilisateur

--- Public/index.php
+++ Public/index.php
@@ -5,7 +5,13 @@ require 'PEUNC/classes/Page.php';
 require 'PEUNC/classes/BDD.php';
 require 'PEUNC/classes/RequeteHttp.php';
 
-// classes utilisateur
+// autoloader pour les classes utilisateur
+spl_autoload_register(
+	function($classe)
+	{
+		require_once"Modele/classe_{$classe}.php";
+	}
+);
 
 try
 {
@@ -20,13 +26,13 @@ try
 	list($_SESSION["PEUNC"]['alpha'], $_SESSION["PEUNC"]['beta'], $_SESSION["PEUNC"]['gamma']) = [$requete->getAlpha(), $requete->getBeta(), $requete->getGamma()];// MAJ de l'état'
 	// $_SESSION = array(...) détruirait les autres éventuels paramètres stockés dans la session
 
-	require"Modele/classe_{$classePage}.php";
+	//require"Modele/classe_{$classePage}.php";
 	$PAGE = new $classePage(explode("/", $paramPage));
 	$PAGE->ExecuteControleur($_SESSION["PEUNC"]['alpha'], $_SESSION["PEUNC"]['beta'], $_SESSION["PEUNC"]['gamma']);
 }
 catch(Exception $e)
 {
-	require"Modele/classe_PageErreur.php";
+	//require"Modele/classe_PageErreur.php";
 	$PAGE = new PageErreur;
 	$PAGE->setCodeErreur("application");
 	$PAGE->setTitreErreur($e->getMessage());
