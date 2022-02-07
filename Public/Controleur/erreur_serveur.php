27e73b5906575e861aa1bab6f004ac205cc06493
ChristopHe <christophe.hervi@free.fr>
2022-02-07 18:40:54 +0100

    un oubli dans erreur_serveur.php

--- Public/Controleur/erreur_serveur.php
+++ Public/Controleur/erreur_serveur.php
@@ -1,7 +1,7 @@
 <?php
-$this->setCodeErreur($_SESSION["PEUNC"]['beta']);
+$this->setCodeErreur($this->beta);
 
 $BD = new\PEUNC\BDD;
-$this->setTitreErreur($BD->TexteErreur($_SESSION["PEUNC"]['beta']));
+$this->setTitreErreur($BD->TexteErreur($this->beta));
 
 $this->setCorpsErreur("<p>Image &agrave; venir</p>");
