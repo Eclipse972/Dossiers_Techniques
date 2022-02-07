5e3e4b87354f2f56baaf20c38702b2528326fabf
ChristopHe <christophe.hervi@free.fr>
2022-02-07 04:48:20 +0100

    modification du texte du lien vers le formulaire de contact

--- Public/Modele/classe_Index.php
+++ Public/Modele/classe_Index.php
@@ -7,7 +7,7 @@ class Index extends PEUNC\Page {
 		$this->setTitle("Les dossiers techniques de ChristopHe");
 		$this->setHeaderText("<p class=\"font-effect-outline\">Les dossiers techniques de ChristopHe version test</p>");
 		$this->setLogo("logo.png");
-		$this->setFooter(" - <a href=/Contact>Contact</a>");
+		$this->setFooter(" - <a href=/Contact>Me contacter</a>");
 		$this->setView("index.html");
 		$this->setCSS(array("index"));
 	}
