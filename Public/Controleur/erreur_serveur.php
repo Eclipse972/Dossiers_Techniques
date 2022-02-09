25355915dbe8840a17aa5584e2f05f9f28c7924f
ChristopHe <christophe.hervi@free.fr>
2022-02-09 01:42:55 +0100

    inutile de créer un nouvel objet BD, il en existe un disponible

--- Public/Controleur/erreur_serveur.php
+++ Public/Controleur/erreur_serveur.php
@@ -1,7 +1,7 @@
 <?php
+global $BD;
 $this->setCodeErreur($this->beta);
 
-$BD = new\PEUNC\BDD;
 $this->setTitreErreur($BD->TexteErreur($this->beta));
 
 $this->setCorpsErreur("<p>Image &agrave; venir</p>");
