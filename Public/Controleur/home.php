a552eab403efa6ed34528f7268d58f68e7d92018
ChristopHe <christophe.hervi@free.fr>
2022-02-10 02:52:01 +0100

    La classe index est inutile

--- Public/Controleur/home.php
+++ Public/Controleur/home.php
@@ -1,3 +1,25 @@
-<?php
+<?php	// page daccueil
+global $BD;
 
-$this->setSection($this->Gerer_index(5));
+$this->setTitle("Les dossiers techniques de ChristopHe");
+$this->setHeaderText("<p class=\"font-effect-outline\">Les dossiers techniques de ChristopHe version test</p>");
+$this->setLogo("logo.png");
+$this->setFooter(" - <a href=/Contact>Me contacter</a>");
+$this->setView("index.html");
+$this->setCSS(array("index"));
+
+$NB_colonne = 5;
+$code = '';
+$Tréponse = $BD->ResultatSQL('SELECT * FROM Vue_code_vignettes', []);
+foreach($Tréponse as $id => $ligne)
+{	// récupère et agrège le code
+	$No_colonne = $id % $NB_colonne;
+	if($No_colonne==0) $code .=  "<tr>\n"; // nouvelle ligne
+	$code .= "\t{$ligne['code']}\n";
+	if($No_colonne==$NB_colonne-1) $code .= "</tr>\n";	// fin de ligne si dernière colonne atteinte
+}
+// si en sortie on s'arrete sur une colonne autre que la dernière
+if($No_colonne!=$NB_colonne-1)
+	$code .= "</tr>\n"; // on termine la ligne
+
+$this->setSection($code);
