89279454298cf91cee523a42e229596a76faa15d
ChristopHe <christophe.hervi@free.fr>
2022-02-10 06:07:06 +0100

    création du bac à sable et de la page du premier test

---
+++ Public/Controleur/bacAsable.php
@@ -0,0 +1,11 @@
+<?php	// page daccueil
+global $BD;
+
+$this->setTitle("Les dossiers techniques de ChristopHe");
+$this->setHeaderText("<p class=\"font-effect-outline\">Les dossiers techniques de ChristopHe version test</p>");
+$this->setLogo("logo.png");
+$this->setFooter(" - <a href=/Contact>Me contacter</a>");
+$this->setView("bacAsable.html");
+$this->setCSS([]);
+
+$this->setSection("<p>Encore rien pour le moment !</p>");
---
+++ Public/Controleur/liste_niveau.php
@@ -0,0 +1,64 @@
+<?php
+function Liste_niveau_v2($alpha = null, $beta = null)
+{
+	global $BD;
+	if(!isset($alpha))
+	{	// pour les onglets
+		$eqAlpha= ">= 0";
+		$eqBeta	= "= 0";
+		$eqGamma= "= 0";
+		$indice	= "alpha";
+		$param	= [];
+	}
+	elseif(!isset($beta))
+	{	// pour le menu
+		$eqAlpha= "= ?";
+		$eqBeta = "> 0";
+		$eqGamma= "= 0";
+		$indice = "beta";
+		$param	= [$alpha];
+	}
+	else
+	{	// pour le sous-menu
+		$eqAlpha= "= ?";
+		$eqBeta = "= ?";
+		$eqGamma= "> 0";
+		$indice = "gamma";
+		$param	= [$alpha, $beta];
+	}
+	$Treponse = $BD->ResultatSQL("SELECT {$indice} AS i, code FROM Vue_code_item
+							WHERE alpha{$eqAlpha} AND beta{$eqBeta} AND gamma{$eqGamma}",
+							$param
+						);
+	$Tableau = [];
+	foreach($Treponse as $ligne)
+	{
+		$i = $ligne["i"];
+		$Tableau[$ligne["i"]] = $ligne["code"];
+	}
+	return $Tableau;
+}
+
+$this->setTitle("Les dossiers techniques de ChristopHe");
+$this->setHeaderText("<p class=\"font-effect-outline\">Les dossiers techniques de ChristopHe version test</p>");
+$this->setLogo("logo.png");
+$this->setFooter(" - <a href=/Contact>Me contacter</a>");
+$this->setView("bacAsable.html");
+$this->setCSS([]);
+
+ob_start();
+?>
+<p>Pour le niveau 3 avec alpha=3 et beta=2</p>
+<ul>
+<?php
+
+foreach(Liste_niveau_v2(3) as $i => $ligne)
+{
+	echo "\t<li>{$i} - {$ligne}</li>\n";
+}
+?>
+</ul>
+<?php
+$tampon = ob_get_contents();
+ob_clean();
+$this->setSection($tampon);
