0b21b1d70c6f673d6b169f91c490d3e386680179
ChristopHe <christophe.hervi@free.fr>
2022-02-10 02:59:03 +0100

    La classe PageErreur est inutile

--- Public/Controleur/erreur_serveur.php
+++ Public/Controleur/erreur_serveur.php
@@ -1,5 +1,12 @@
 <?php
 global $BD;
+$this->setTitle("Les dossiers techniques de ChristopHe");
+$this->setHeaderText("<p class=\"font-effect-outline\">Les dossiers techniques de ChristopHe version test</p>");
+$this->setLogo("logo.png");
+$this->setFooter("");
+$this->setView("erreur.html");
+$this->setCSS(array("erreur"));
+
 $this->setCodeErreur($this->beta);
 
 $this->setTitreErreur($BD->TexteErreur($this->beta));
