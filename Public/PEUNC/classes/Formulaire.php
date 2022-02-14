26251ea7e9d6e8d11f001912a7cafe39344b60a3
ChristopHe <christophe.hervi@free.fr>
2022-02-14 06:15:43 +0100

    ébauche de création du jeton de formulaire

--- Public/PEUNC/classes/Formulaire.php
+++ Public/PEUNC/classes/Formulaire.php
@@ -4,11 +4,42 @@ namespace PEUNC;
 
 abstract class Formulaire extends Page
 {
+	protected $jeton;	// contient la configuration en clair sous la forme d'un objet JSON
+
 	public function __construct($alpha, $beta, $gamma, $methode, array $TparamURL = [])
 	{
 		parent::__construct($alpha, $beta, $gamma, $methode, $TparamURL);
 		if ($methode == "GET")
-			Formulaire::SauvegarderPosition($alpha, $beta, $gamma);
+		{
+			global $BD;
+			// recherche du noeud qui traite le formulaire
+			$ID = ResultatSQL("SELEC ID WHERE alpha=? ANS beta=? gamma=? AND methode = 'POST'",[$alpha, $beta, $gamma]);
+			$this->jeton = '{"ID":' . $ID .', "depart":' . time() . '}';
+		}
+		else
+		{
+
+		}
+	}
+
+// Fonctions pour le jeton ====================================================================
+
+	public function InsérerJeton()
+	{	// insère le champ caché jeton dans le formulaire
+		// chiffrement du jeton
+		// code html du champ caché jeton XRSF
+	}
+
+	public function AjouterOjetAuJeton($nom, $valeurJSON)
+	{
+		$this->jeton .= '{"' . $nom . '":' . $valeurJSON . '}';	// les 2 objets ont mis cote à cote
+		$this->jeton = str_replace("}{", ", ", $this->jeton);	// fusionne les deux objets
+	}
+
+	public function LireJeton($jeton)
+	{
+		// dechiffrement jeton
+		// si erreur renvoyer null sinon renvoyer l'objet
 	}
 
 // Fonctions abstraites =======================================================================
@@ -22,23 +53,4 @@ abstract class Formulaire extends Page
 	abstract public function Traitement();	// traitement si tout est OK. Par exemple envoyer un courriel, modifier une BD
 
 	abstract public function TraitementAvantRepresentation();	// prépare le formulaire pour un réaffichage en  générant des messages d'erreur par exemple'
-
-// fonctions statiques ========================================================================
-
-	public static function SauvegarderPosition($alpha, $beta, $gamma)
-	{
-		$_SESSION["PEUNC"]['formAlpha'] = $alpha;
-		$_SESSION["PEUNC"]['formBeta'] = $beta;
-		$_SESSION["PEUNC"]['formGamma'] = $gamma;
-	}
-
-	public static function DonnerPosition()
-	{
-		return array($_SESSION["PEUNC"]['formAlpha'], $_SESSION["PEUNC"]['formBeta'], $_SESSION["PEUNC"]['formGamma']);
-	}
-
-	public static function EffacerPosition()
-	{
-		$_SESSION["PEUNC"]['formAlpha'] = $_SESSION["PEUNC"]['formBeta'] = $_SESSION["PEUNC"]['formGamma'] = null;
-	}
 }
