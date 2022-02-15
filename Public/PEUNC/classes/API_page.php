1f44954c84e8124a305df15ccafb7b0554fdfa24
ChristopHe <christophe.hervi@free.fr>
2022-02-15 03:52:37 +0100

    déplacement accolade

--- Public/PEUNC/classes/API_page.php
+++ Public/PEUNC/classes/API_page.php
@@ -1,12 +1,12 @@
 <?php		// API de la classe Page de PEUNC
 namespace PEUNC;
 
-interface iPage	{
+interface iPage
 /* Chaque page est entièrement construite avant le moindre affichage. L'hydratation de la page se fait à partir d'un controleur.
  * Une fois la page construite on injecte le code dans le fichier doctype.html. Ce fichier ne fait qu'utiliser les différentes
  * assesseurs (getter)) de la classe.
 */
-
+{
 // Mutateur (getters)
 	public function getCSS();		// affiche le code pour utiliser toutes les feuilles CSS associée à la page
 	public function getTitle();		// affiche le titre du document (qui est affiché dans la barre de titre du navigateur ou dans l'onglet de la page)
