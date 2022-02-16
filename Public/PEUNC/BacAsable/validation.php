ed504818085bdb9ebdd78538a190abd54494733b
ChristopHe <christophe.hervi@free.fr>
2022-02-16 03:54:08 +0100

    fonction AfficherConfiguration validée

--- Public/PEUNC/BacAsable/validation.php
+++ Public/PEUNC/BacAsable/validation.php
@@ -2,5 +2,13 @@
 require"PEUNC/BacAsable/commun.php";
 $CodeValidation = new PEUNC\CodeValidation;
 
-$code = "<p>JSON = " . $CodeValidation->AfficherConfiguration() . "</p>";
+$contenuJSON =  $CodeValidation->AfficherConfiguration();
+$code = "<p>JSON = " . $contenuJSON . "</p>\n";
+
+$objetJSON = json_decode($contenuJSON);
+
+$code .= "<p>2e élément 1er tableau = " . $objetJSON->T_id_champ[1] . "</p>\n";
+$code .= "<p>3e élément 2e tableau = " . $objetJSON->T_choix[2] . "</p>\n";
+$code .= "<p>dernier choix = " . $objetJSON->dernier_choix . "</p>\n";
+
 $this->setSection("\t<h1>code de validation et JSON</h1>" . $code . "\n");
