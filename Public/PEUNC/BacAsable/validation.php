b9fbd23072372e5d3dd76cf37c27eaaf1619d14b
ChristopHe <christophe.hervi@free.fr>
2022-02-16 04:07:37 +0100

    le constructeur avec des paramètres fonctionne

--- Public/PEUNC/BacAsable/validation.php
+++ Public/PEUNC/BacAsable/validation.php
@@ -11,4 +11,10 @@ $code .= "<p>2e élément 1er tableau = " . $objetJSON->T_id_champ[1] . "</p>\n"
 $code .= "<p>3e élément 2e tableau = " . $objetJSON->T_choix[2] . "</p>\n";
 $code .= "<p>dernier choix = " . $objetJSON->dernier_choix . "</p>\n";
 
+$code .= "<h2>test du constructeur avec des paramètres</h2>";
+
+$_2e_CodeValidation = new PEUNC\CodeValidation($objetJSON->T_id_champ, $objetJSON->T_choix, $objetJSON->dernier_choix);
+$_2e_contenuJSON =  $_2e_CodeValidation->getConfiguration();
+$code .=  "<p>JSON 2 = " . $_2e_contenuJSON . "</p>\n";
+
 $this->setSection("\t<h1>code de validation et JSON</h1>" . $code . "\n");
