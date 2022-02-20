10bece3a689fd2796a9e1879b5b83528bf26a780
ChristopHe <christophe.hervi@free.fr>
2022-02-20 05:55:32 +0100

    erreur gramaticale

--- BD/PEUNC/Vue_ptiNomInvalides.sql
+++ BD/PEUNC/Vue_ptiNomInvalides.sql
@@ -4,5 +4,5 @@ CREATE VIEW Vue_ptiNomInvalides AS
 SELECT ID, ptiNom
 FROM Squelette
 WHERE NOT(ptiNom REGEXP("^[a-zA-Z0-9][a-zA-Z_0-9_-]+$"))
--- Commence par une caractère non accentué ou un chiffre suivi de la même chose avec en plus les caractères - et _
+-- Commence par un caractère non accentué ou un chiffre suivi de la même chose avec en plus les caractères - et _
 ORDER BY ID;
