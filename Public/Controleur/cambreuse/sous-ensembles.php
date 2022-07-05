daf0c6c980d46bd7918114db53455f2fcdec04fc
Christophe HERVI <christophe.hervi@free.fr>
2022-07-05 20:05:59 +0200

    amélioration de la page des sous-ensembles de la cambreuse

--- Public/Controleur/cambreuse/sous-ensembles.php
+++ Public/Controleur/cambreuse/sous-ensembles.php
@@ -2,3 +2,4 @@
 $this->setEclate("Les sous-ensemble de la cambreuse");
 $this->SetImage("eclate_cambreuse2.png");
 $this->setFichier("cambreuse.EASM");
+$this->setCommentaire("<p>La cambreuse est compos&eacute;e de 5 classes d&apos;&eacute;quivalences ou sous ensembles.</p>\n<p style=\"text-align:center\">Dans e-Drawing, cliquez sur l&apos;ic&ocirc;ne <img src=\"/images/icone_eclater_rassembler.png\" alt = \"icone\"> pour &eacute;clater/rassembler la maquette num&eacute;rique</p>");
