39edfd7ec622463dd4dc3541d7c6176ee213cfb1
Christophe HERVI <christophe.hervi@free.fr>
2022-11-15 22:20:34 +0100

    ajout de dessin d'ensemble et l'éclaté du frein à disque

---
+++ Public/Controleur/freinAdisque/dessin_densemble.php
@@ -0,0 +1,4 @@
+<?php // controleur dessin d'ensemble du frein de Mégane
+$this->setDessinDensemble();
+$this->setImage("dessin_frein.png");
+$this->setFichier("dessin_frein.EDRW");
---
+++ Public/Controleur/freinAdisque/eclate.php
@@ -0,0 +1,4 @@
+<?php // controleur éclaté du frein de Mégane
+$this->setEclate();
+$this->setImage("eclate_frein.png");
+$this->setFichier("eclate_frein.EDRW");
