76a4cbc1e9175d11e933266a591effaa365ee0cf
Christophe HERVI <christophe.hervi@free.fr>
2022-11-18 01:08:05 +0100

    remplacement setEclate par setPiece

--- Public/Controleur/brideAnez/plaquette.php
+++ Public/Controleur/brideAnez/plaquette.php
@@ -1,5 +1,5 @@
-<?php // éclasse d'équivalence plaquette de bride à nez'
-$this->setEclate("Classe d&apos;&eacute;quivalence: nez de la bride");
+<?php // classe d'équivalence plaquette de bride à nez
+$this->setPiece("Classe d&apos;&eacute;quivalence: nez de la bride");
 $this->SetImage("plaquette_plastique.png");
 $this->setCommentaire("<p style=\"text-align:center\">Cette classe d&apos;&eacute;quivalence est compos&eacute;e d&apos;une seule pi&egrave;ce</p>");
 $this->setFichier("plaquette_plastique.EPRT");
--- Public/Controleur/brideAnez/ressort.php
+++ Public/Controleur/brideAnez/ressort.php
@@ -1,5 +1,5 @@
-<?php // ressort de bride à nez'
-$this->setEclate("Le ressort de compression de la bride");
+<?php // ressort de bride à nez
+$this->setPiece("Le ressort de compression de la bride");
 $this->SetImage("ressort.png");
 $this->setCommentaire("<p style=\"text-align:center\">Remarque: cette pi&egrave;ce est d&eacute;formable donc ne constitue pas une classe d&apos;&eacute;quivalence.</p>
 ");
