ea55d7ab4607f122f05d9391bda4ade97afa6c49
Christophe HERVI <christophe.hervi@free.fr>
2022-11-28 13:30:53 +0100

    la bonne vidéo pour la page fonctionnement

--- Public/Controleur/freinAdisque/fonctionnement.php
+++ Public/Controleur/freinAdisque/fonctionnement.php
@@ -5,7 +5,7 @@ ob_start();	// d&eacute;but du code <section>
 	<div align=center>
 		<iframe
 			width="896" height="504"
-			src="https://www.youtube-nocookie.com/embed/OzJxJXWfn9M"
+			src="https://www.youtube.com/embed/4ZCPfgWiX2s"
 			title="YouTube video player"
 			frameborder="0"
 			allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
