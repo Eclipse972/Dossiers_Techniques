a880ff174949521fbef9e8b3e0827d2fe81f245e
Christophe HERVI <christophe.hervi@free.fr>
2024-10-04 22:22:47 +0200

    versio locale de PEUNC

---
+++ public/PEUNC/BacAsable/JSON.php
@@ -0,0 +1,19 @@
+<?php // apprendre à créer et utilise un fichier JSON
+require"PEUNC/BacAsable/commun.php";
+
+function AjouterOjetAuJeton($jeton, $nom, $valeurJSON)
+{
+	$jeton .= '{"' . $nom . '":' . $valeurJSON . '}';	// les 2 objets ont mis cote à cote
+	$jeton = str_replace("}{", ", ", $jeton);	// fusionne les deux objets
+	return $jeton;
+}
+
+$fichier = '{"ID":125}';
+$fichier = AjouterOjetAuJeton($fichier,"depart", time());
+
+
+$objet = json_decode($fichier);
+
+$code = "<p>fichier :" .$fichier . "</p><p>Utilisation</p><p>ID = " . $objet->ID . "<br>temps " .  $objet->depart . "</p>";
+
+$this->setSection("\t<h1>Utilisation fichier JSON</h1>" . $code . "\n");
---
+++ public/PEUNC/BacAsable/bacAsable.php
@@ -0,0 +1,16 @@
+<?php	// page daccueil
+require"PEUNC/BacAsable/commun.php";
+
+$Liste = PEUNC\BDD::SELECT("code FROM Vue_code_item WHERE alpha = -3 AND beta > 0 AND gamma =0", []);
+if ($Liste == null)
+	$code = "<p>Rien pour le moment !</p>";
+elseif (is_array($Liste))
+{
+	$code = "<p>Liste des tests en cours:</p>\n\t<ul>\n";
+	foreach ($Liste as $valeur)
+		$code .= "\t\t<li>{$valeur["code"]}</li>\n";
+	$code .= "\t</ul>";
+}
+else $code = "<p>un seul test: {$Liste}</p>";
+
+$this->setSection("\t<h1>Le bac &agrave; sable</h1>\n\t" . $code . "\n");
