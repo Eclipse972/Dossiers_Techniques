eaef16b89cc8c67492554cf91f9813a9dee1ea6a
Christophe HERVI <christophe.hervi@free.fr>
2024-10-04 23:13:19 +0200

    suivi des fichiers de configuration chez free

---
+++ public/config_chiffrement.php
@@ -0,0 +1,4 @@
+<?php	// configuration chiffrement
+$cipher = "AES-128-CBC";
+$key =",P{4!8GyJ\[b7^#b";
+$iv = ">4.ND5\@u>riv8EV";
---
+++ public/connexion.php
@@ -0,0 +1,8 @@
+<?php // fichier non suivi par git
+$developpement = false;	// site en developpement ?
+
+$dbname = $developpement ? 'dossier_te_ch_nique'	: 'dossiers_techniques';
+$user	= $developpement ? 'dossier.te.ch.nique'	: 'dossiers.techniques';
+$pwd	= $developpement ? 'Wma7agKHxyyS4LAr'		: 'E19wldwnDDbruWEO';
+
+$host='sql.free.fr';
