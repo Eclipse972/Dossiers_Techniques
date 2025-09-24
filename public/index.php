e8ad8ea5165bc432db8cf298ac536cc8169dcb5d
Christophe HERVI <christophe.hervi@free.fr>
2025-09-24 23:18:12 +0200

    MAJ

--- public/index.php
+++ public/index.php
@@ -1,31 +1,35 @@
 <?php
-// Active les erreurs en production (à désactiver après les tests)
+// Active les erreurs en développement (à désactiver en prod)
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 // Charge l'autoloader
 require __DIR__ . '/../vendor/autoload.php';
 
-// Importations nécessaires
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 use Twig\Loader\FilesystemLoader;
 use Twig\Environment;
-use Eclipse972\DossiersTechniques\Controllers\HomeController;
-use Eclipse972\DossiersTechniques\Controllers\ListeController;
+use Eclipse972\DossiersTechniques\Controllers\PageController;
 
 // Initialise Slim
 $app = AppFactory::create();
 
-// Configuration de Twig
+// Configuration de Twig (avec cache pour la prod)
 $loader = new FilesystemLoader(__DIR__ . '/../templates');
-$twig = new Environment($loader);
+$twig = new Environment($loader, [
+    'cache' => __DIR__ . '/../cache',
+]);
 
-// Ajoute Twig à l'application (pour les contrôleurs)
+// Ajoute Twig au conteneur
 $app->getContainer()->set('twig', $twig);
 
-// Routes
-$app->get('/', [HomeController::class, 'index']);
+// Route pour l'accueil (version avec typage explicite)
+$app->get('/', function (Request $request, Response $response) use ($twig) {
+    $controller = new PageController($twig);
+    return $controller->accueil($response);
+});
+
 // Démarre l'application
 $app->run();
