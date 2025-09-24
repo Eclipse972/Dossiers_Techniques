48618dc6c6dc569866986f04f0106dbfc9f9bd2b
Christophe HERVI <christophe.hervi@free.fr>
2025-09-24 22:44:25 +0200

    script de connexion à la BDD

--- public/connexion.php
+++ public/connexion.php
@@ -1,8 +1,5 @@
-<?php # configuration chez free
-$developpement = false;	// site en developpement ?
-
-$dbname = $developpement ? 'dossier_te_ch_nique'	: 'dossiers_techniques';
-$user	= $developpement ? 'dossier.te.ch.nique'	: 'dossiers.techniques';
-$pwd	= $developpement ? 'Wma7agKHxyyS4LAr'		: 'E19wldwnDDbruWEO';
-
-$host='sql.free.fr';
+<?php
+$dbname = 'sc2rogu0302_dossiers_techniques';
+$user	= 'sc2rogu0302_DT';
+$pwd	= 'yN1s#%0crmA9';
+$host	= 'localhost';
