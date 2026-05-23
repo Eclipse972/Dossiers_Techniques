306433eadb1bbf99a5e9bb161778903745966aa7
Christophe HERVI <christophe.hervi@free.fr>
2026-05-23 21:45:26 +0200

    indiquer à intelephense que la variable existe Ainsi intelephense ne signalera pas d'erreur de type variable inconnue. On branch hello-world Changes to be committed: 	modified:   src/config/routes.php

--- src/config/routes.php
+++ src/config/routes.php
@@ -1,5 +1,7 @@
 <?php
 
 use DossiersTechniques\Controleur\AutrePageControleur;
+use Slim\App; // ou le type réel de $app
 
+/** @var App $app */
 $app->get('/', [AutrePageControleur::class, 'accueil']);
