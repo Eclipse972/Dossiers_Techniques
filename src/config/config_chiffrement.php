312e319f70727a1820e6d7c0d40c9fa2cd80dbae
Christophe HERVI <christophe.hervi@free.fr>
2026-05-24 03:45:08 +0200

    scriptsinutiles

--- src/config/config_chiffrement.php
+++
@@ -1,4 +0,0 @@
-<?php	// configuration chiffrement
-$cipher = "AES-128-CBC";
-$key =",P{4!8GyJ\[b7^#b";
-$iv = ">4.ND5\@u>riv8EV";
--- src/config/connexion.php
+++
@@ -1,5 +0,0 @@
-<?php
-$dbname = 'sc2rogu0302_dossiers_techniques';
-$user	= 'sc2rogu0302_DT';
-$pwd	= 'yN1s#%0crmA9';
-$host	= 'localhost';
