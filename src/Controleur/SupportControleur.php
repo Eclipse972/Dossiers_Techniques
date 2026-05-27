bfdd97fe295c2c588e13032343580f3f493c9421
Christophe HERVI <christophe.hervi@free.fr>
2026-05-27 13:35:13 +0200

    un controleur est associé à un support

--- src/Controleur/SupportControleur.php
+++ src/Controleur/SupportControleur.php
@@ -1,14 +1,23 @@
 <?php
 /**
- * Classe mère des contrôleur de chaque support
+ * Classe abstraite et mère des contrôleurs de chaque support
  */
+namespace DossiersTechniques\Controleur;
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Views\Twig;
 
-class SupportControleur
+abstract class SupportControleur
 {
+	// propritété à instancier obligatoirement
+	protected string $nom;
+	protected string $article_du; // "du ", "de la " ou "de l'"
+	protected string $dossier;
+
+	// propriété à instancier par les méthodes des classes-filles
+	protected string $template;
+
     /**
      * Constructeur : injection du moteur de templates.
      *
@@ -21,5 +30,37 @@ class SupportControleur
      */
     public function __construct(private Twig $vue) {}
 
-	
+	/**
+	 * Hydratation du support
+	 *
+	 * Fonction que dois appeler chaque constructeur des classes-filles
+	 * pour donner les caractéristiques de chaque support
+	 *
+	 * @param string $nom nom du support
+	 * @param string $du article pour écrire par exemple dossier technique 'du' support
+	 * @param string $dossier racine du dossier pour les images, fichiers et templates.
+	 * ATTENTION: le dossier ne doit pas contenir d'espace
+	 */
+	abstract protected function hydrate(string $nom, string $du, string $dossier): void;
+
+	/**
+	 * Le dossier technique minimal d'un support nécessite les pages suivantes :
+	 * 	- une mise en situation
+	 * 	- un dessin d'ensemble
+	 * 	- une nomenclature
+	 * 	- une page 'à propos' donnant une archive zip et une description
+	 */
+	abstract protected function miseEnSituation(Request $requete, Response $reponse): Response;
+	abstract protected function dessinDensemble(Request $requete, Response $reponse): Response;
+	abstract protected function aPropos(Request $requete, Response $reponse): Response;
+	abstract protected function nomenclature(Request $requete, Response $reponse): Response;
+
+	// outils pour les classes-filles
+    /*public function rendu_miseEnSituation(Response $reponse, string $template): Response {
+		return $this->vue->render($reponse, $template, [
+				'support'	=> $this->nom,
+				'du'		=> $this->article_du,
+				'dossier'	=> $this->dossier,
+  		]);
+	}*/
 }
