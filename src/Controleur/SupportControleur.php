2795f61ee4aa76dce05a58d5c5848853350545f7
Christophe HERVI <christophe.hervi@free.fr>
2026-05-27 11:17:46 +0200

    création de la classe-mère des controleur de support

---
+++ src/Controleur/SupportControleur.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Classe mère des contrôleur de chaque support
+ */
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Views\Twig;
+
+class SuppportControleur
+{
+    /**
+     * Constructeur : injection du moteur de templates.
+     *
+     * Utilise la promotion de propriété PHP 8 : le mot clé
+     * `private` dans les paramètres déclare et initialise
+     * automatiquement la propriété $vue, ce qui évite
+     * d'écrire $this->vue = $vue; dans le corps du constructeur.
+     *
+     * @param Twig $vue Moteur de templates Twig
+     */
+    public function __construct(private Twig $vue) {}
+
+	
+}
