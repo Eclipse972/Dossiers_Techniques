dead2d9ecfb0d8272b804d5c1e93dbba8f70d125
Christophe HERVI <christophe.hervi@free.fr>
2026-05-27 12:24:13 +0200

    orthographe

--- src/Controleur/SupportControleur.php
+++ src/Controleur/SupportControleur.php
@@ -7,7 +7,7 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Views\Twig;
 
-class SuppportControleur
+class SupportControleur
 {
     /**
      * Constructeur : injection du moteur de templates.
