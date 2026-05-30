d08444d2d3cfa5b8a1d3607b10015ea910881cf3
Christophe HERVI <christophe.hervi@free.fr>
2026-05-30 23:53:29 +0200

    oute pour la MES de l'alternateur

--- src/Controleur/AlternateurControleur.php
+++ src/Controleur/AlternateurControleur.php
@@ -24,7 +24,7 @@ class AlternateurControleur extends SupportControleur
     /**
      * Affiche la page de mise en situation de l'alternateur.
      *
-     * @route /alternateur/mise-en-situation
+     * @route /alternateur/MES
      *
      * @param Request  $requete Requête HTTP entrante
      * @param Response $reponse Réponse HTTP à retourner
--- src/config/routes.php
+++ src/config/routes.php
@@ -3,5 +3,11 @@
 use DossiersTechniques\Controleur\AutrePageControleur;
 use Slim\App; // ou le type réel de $app
 
+// Controleurs des supports
+use DossiersTechniques\Controleur\AlternateurControleur;
+
 /** @var App $app */
 $app->get('/', [AutrePageControleur::class, 'accueil']);
+
+// dossier technique de l'alternateur
+$app->get('/alternateur/MES', [AlternateurControleur::class, 'miseEnSituation']);
