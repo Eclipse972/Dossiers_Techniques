58ae8d7e256644096674b296ce64802c536a0431
Christophe HERVI <christophe.hervi@free.fr>
2026-05-30 02:48:29 +0200

    tabulation au lieu d'espaces

--- src/Controleur/AutrePageControleur.php
+++ src/Controleur/AutrePageControleur.php
@@ -38,34 +38,34 @@ class AutrePageControleur
     {
         $listeSupport = [ // données de chaque support classés par ordre alphabétique
             ['nom' => 'Alternateur',			'image' => 'alternateur.png',       'dossier' => 'alternateur'],
-            ['nom' => 'Bouton poussoir',        'image' => 'BP.png',                'dossier' => 'BP'],
-            ['nom' => 'Bride à nez',            'image' => 'bride.png',				'dossier' => 'brideAnez'],
-            ['nom' => 'Bride hydraulique',      'image' => 'bride.png',				'dossier' => 'bride_hydraulique'],
-            ['nom' => 'Butée 5 axes',           'image' => 'butee.png',		        'dossier' => 'butee5axes'],
-            ['nom' => 'Cambreuse',              'image' => 'cambreuse.png',         'dossier' => 'cambreuse'],
-            ['nom' => 'Casse-noix',             'image' => 'casseNoix.png',			'dossier' => 'casse_noix'],
-            ['nom' => 'Coupe-tube',             'image' => 'mini_coupe-tube.png',	'dossier' => 'coupe-tube'],
-            ['nom' => 'Cric bouteille',         'image' => 'cric.png',				'dossier' => 'cric_bouteille'],
-            ['nom' => 'Cric hydraulique',       'image' => 'cric.png',				'dossier' => 'cric_hydraulique'],
+            ['nom' => 'Bouton poussoir',		'image' => 'BP.png',                'dossier' => 'BP'],
+            ['nom' => 'Bride à nez',			'image' => 'bride.png',				'dossier' => 'brideAnez'],
+            ['nom' => 'Bride hydraulique',		'image' => 'bride.png',				'dossier' => 'bride_hydraulique'],
+            ['nom' => 'Butée 5 axes',			'image' => 'butee.png',		        'dossier' => 'butee5axes'],
+            ['nom' => 'Cambreuse',				'image' => 'cambreuse.png',         'dossier' => 'cambreuse'],
+            ['nom' => 'Casse-noix',				'image' => 'casseNoix.png',			'dossier' => 'casse_noix'],
+            ['nom' => 'Coupe-tube',				'image' => 'mini_coupe-tube.png',	'dossier' => 'coupe-tube'],
+            ['nom' => 'Cric bouteille',			'image' => 'cric.png',				'dossier' => 'cric_bouteille'],
+            ['nom' => 'Cric hydraulique',		'image' => 'cric.png',				'dossier' => 'cric_hydraulique'],
             ['nom' => 'Électrovanne',			'image' => 'electrovanne.png',      'dossier' => 'electrovanne'],
             ['nom' => 'Étau',					'image' => 'etau.png',				'dossier' => 'etau'],
             ['nom' => 'Extracteur de roulement','image' => 'extracteur.png',		'dossier' => 'extracteur2roulement'],
-            ['nom' => 'Frein à disque',         'image' => 'frein.png',				'dossier' => 'freinAdisque'],
-            ['nom' => 'Moteur de modélisme',    'image' => 'moteur.png',			'dossier' => 'moteur2modelisme'],
-            ['nom' => 'Pince de marquage',      'image' => 'pince.png', 			'dossier' => 'pince2marquage'],
-            ['nom' => 'Pince de robot',         'image' => 'pince.png',				'dossier' => 'pince2robot'],
-            ['nom' => 'Pompe à palettes',       'image' => 'pompe.png',				'dossier' => 'pompeApalettes'],
-            ['nom' => 'Préhenseur de cuasse',	'image' => 'prehenseur.png',        'dossier' => 'prehenseur'],
-            ['nom' => 'Unité de marquage',      'image' => 'unite2marquage.png',    'dossier' => 'unite2marquage'],
+            ['nom' => 'Frein à disque',			'image' => 'frein.png',				'dossier' => 'freinAdisque'],
+            ['nom' => 'Moteur de modélisme',	'image' => 'moteur.png',			'dossier' => 'moteur2modelisme'],
+            ['nom' => 'Pince de marquage',		'image' => 'pince.png', 			'dossier' => 'pince2marquage'],
+            ['nom' => 'Pince de robot',			'image' => 'pince.png',				'dossier' => 'pince2robot'],
+            ['nom' => 'Pompe à palettes',		'image' => 'pompe.png',				'dossier' => 'pompeApalettes'],
+            ['nom' => 'Préhenseur de culasse',	'image' => 'prehenseur.png',        'dossier' => 'prehenseur'],
+            ['nom' => 'Unité de marquage',		'image' => 'unite2marquage.png',    'dossier' => 'unite2marquage'],
             ['nom' => 'Vanne sphérique',		'image' => 'vanne.png',             'dossier' => 'vanne'],
         ];
 
         return $this->vue->render($reponse, '121-home.html.twig',  [
-                    'title'            => 'Dossiers techniques de ChristopHe',
-                    'header'           => 'Les dossiers techniques en ligne de ChristopHe',
-                    'logo_url'         => '/images/logo.png',
-                    'logo_description' => 'LOGO',
-                    'app_data'          => $listeSupport
+                    'title'				=> 'DT de ChristopHe',
+                    'header'			=> 'Les dossiers techniques en ligne de ChristopHe',
+                    'logo_url'			=> '/images/logo.png',
+                    'logo_description'	=> 'LOGO',
+                    'app_data'			=> $listeSupport
                 ]);
     }
 }
