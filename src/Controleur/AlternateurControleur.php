0215f3a10424edc3a1b88cff86d0c4ae9c4a5379
Christophe HERVI <christophe.hervi@free.fr>
2026-05-30 23:23:27 +0200

    création du conroleur de l'alternateur

---
+++ src/Controleur/AlternateurControleur.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * Contrôleur de l'alternateur
+ */
+namespace DossiersTechniques\Controleur;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Views\Twig;
+
+class AlternateurControleur extends SupportControleur
+{
+    /**
+     * Constructeur : initialise le support Alternateur.
+     *
+     * @param Twig $vue Moteur de templates Twig
+     */
+    public function __construct(Twig $vue)
+    {
+        parent::__construct($vue);
+        $this->hydrate('alternateur', "de l'", 'alternateur');
+    }
+
+    /**
+     * Hydratation pour l'alternateur.
+     *
+     * @param string $nom     Nom du support
+     * @param string $du      Article défini contracté (ex : "de l'")
+     * @param string $dossier Dossier racine du support (sans espace)
+     */
+    protected function hydrate(string $nom, string $du, string $dossier): void
+    {
+        $this->nom        = $nom;
+        $this->article_du = $du;
+        $this->dossier    = $dossier;
+    }
+
+    /**
+     * Affiche la page de mise en situation de l'alternateur.
+     *
+     * @route /alternateur/mise-en-situation
+     *
+     * @param Request  $requete Requête HTTP entrante
+     * @param Response $reponse Réponse HTTP à retourner
+     * @return Response
+     */
+    protected function miseEnSituation(Request $requete, Response $reponse): Response
+    {
+        return $this->renduMES($reponse);
+    }
+
+    /**
+     * Affiche la page du dessin d'ensemble de l'alternateur.
+     *
+     * @route /alternateur/dessin-densemble
+     *
+     * @param Request  $requete Requête HTTP entrante
+     * @param Response $reponse Réponse HTTP à retourner
+     * @return Response
+     */
+    protected function dessinDensemble(Request $requete, Response $reponse): Response
+    {
+        return $reponse;
+    }
+
+    /**
+     * Affiche la page de nomenclature de l'alternateur.
+     *
+     * @route /alternateur/nomenclature
+     *
+     * @param Request  $requete Requête HTTP entrante
+     * @param Response $reponse Réponse HTTP à retourner
+     * @return Response
+     */
+    protected function nomenclature(Request $requete, Response $reponse): Response
+    {
+        return $reponse;
+    }
+
+    /**
+     * Affiche la page 'à propos' de l'alternateur (archive zip + description).
+     *
+     * @route /alternateur/a-propos
+     *
+     * @param Request  $requete Requête HTTP entrante
+     * @param Response $reponse Réponse HTTP à retourner
+     * @return Response
+     */
+    protected function aPropos(Request $requete, Response $reponse): Response
+    {
+        return $reponse;
+    }
+}
