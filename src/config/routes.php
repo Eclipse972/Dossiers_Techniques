828bfaf4bd3496bc870b5159c4f145f06ee93a31
Christophe HERVI <christophe.hervi@free.fr>
2026-05-31 02:06:24 +0200

    création du controleur du bouton poussoir

--- src/config/routes.php
+++ src/config/routes.php
@@ -5,9 +5,13 @@ use Slim\App; // ou le type réel de $app
 
 // Controleurs des supports
 use DossiersTechniques\Controleur\AlternateurControleur;
+use DossiersTechniques\Controleur\BoutonPoussoirControleur;
 
 /** @var App $app */
 $app->get('/', [AutrePageControleur::class, 'accueil']);
 
 // dossier technique de l'alternateur
 $app->get('/alternateur/mise-en-situation', [AlternateurControleur::class, 'miseEnSituation']);
+
+// dossier technique du bouton poussoir
+$app->get('/bouton-poussoir/mise-en-situation', [BoutonPoussoirControleur::class, 'miseEnSituation']);
