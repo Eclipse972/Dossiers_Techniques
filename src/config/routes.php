062868b234d9014df39eccc41f1bcd31c3c2960e
Christophe HERVI <christophe.hervi@free.fr>
2026-05-31 03:13:21 +0200

    création du cntroleur de la butée 5 axes

--- src/config/routes.php
+++ src/config/routes.php
@@ -8,6 +8,7 @@ use DossiersTechniques\Controleur\AlternateurControleur;
 use DossiersTechniques\Controleur\BoutonPoussoirControleur;
 use DossiersTechniques\Controleur\BrideAnezControleur;
 use DossiersTechniques\Controleur\BrideHydrauliqueControleur;
+use DossiersTechniques\Controleur\Butee5axesControleur;
 
 /** @var App $app */
 $app->get('/', [AutrePageControleur::class, 'accueil']);
@@ -24,3 +25,6 @@ $app->get('/bride-a-nez/mise-en-situation', [BrideAnezControleur::class, 'miseEn
 
 // dossier technique de la bride à nez
 $app->get('/bride-hydraulique/mise-en-situation', [BrideHydrauliqueControleur::class, 'miseEnSituation']);
+
+// dossier technique de la butée 5 axes
+$app->get('/butee-5-axes/mise-en-situation', [Butee5axesControleur::class, 'miseEnSituation']);
