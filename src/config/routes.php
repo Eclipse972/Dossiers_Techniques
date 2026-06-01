db0fcf2edf6c9c263c01c828076f072ff45b6a62
Christophe HERVI <christophe.hervi@free.fr>
2026-06-01 22:25:55 +0200

    regroupement des routes de l'alternateur

--- src/config/routes.php
+++ src/config/routes.php
@@ -32,8 +32,10 @@ use Slim\App;
 $app->get('/', [AutrePageControleur::class, 'accueil']);
 
 // dossier technique de l'alternateur
-$app->get('/alternateur', [AlternateurControleur::class, 'aPropos']);
-$app->get('/alternateur/mise-en-situation', [AlternateurControleur::class, 'miseEnSituation']);
+$app->group('/alternateur', function($group) {
+    $group->get('',						[AlternateurControleur::class, 'aPropos']);
+    $group->get('/mise-en-situation',	[AlternateurControleur::class, 'miseEnSituation']);
+});
 
 // dossier technique du bouton poussoir
 $app->get('/bouton-poussoir/mise-en-situation', [BoutonPoussoirControleur::class, 'miseEnSituation']);
