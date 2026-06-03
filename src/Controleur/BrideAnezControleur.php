da6eb6ac7102bc9437a26807e0a5899273a49300
Christophe HERVI <christophe.hervi@free.fr>
2026-06-03 18:33:10 +0200

    ajout des méthodes manquantes

--- src/Controleur/BrideAnezControleur.php
+++ src/Controleur/BrideAnezControleur.php
@@ -58,10 +58,11 @@ class BrideAnezControleur extends SupportControleur
     /**
      * Affiche la page du dessin d'ensemble de l'alternateur.
      *
-     * @route /alternateur/dessin-densemble
+     * @route /bride-a-nez/dessin-densemble
      *
      * @param Request  $requete Requête HTTP entrante
      * @param Response $reponse Réponse HTTP à retourner
+	 *
      * @return Response
      */
     public function dessinDensemble(Request $requete, Response $reponse): Response
@@ -70,15 +71,226 @@ class BrideAnezControleur extends SupportControleur
     }
 
     /**
-     * Affiche la page de nomenclature de l'alternateur.
+     * Affiche la page de nomenclature de la bride à nez.
      *
-     * @route /alternateur/nomenclature
+     * @route /bride-a-nez/nomenclature
      *
      * @param Request  $requete Requête HTTP entrante
      * @param Response $reponse Réponse HTTP à retourner
+	 *
      * @return Response
      */
     public function nomenclature(Request $requete, Response $reponse): Response
+    {
+        return $this->renduPageEnConstruction($requete, $reponse);
+    }
+
+    /**
+     * Affiche la page de fonctionnement de la bride à nez.
+     *
+     * @route /bride-a-nez/fonctionnement
+     *
+     * @param Request  $requete Requête HTTP entrante
+     * @param Response $reponse Réponse HTTP à retourner
+	 *
+     * @return Response
+     */
+    public function fonctionnement(Request $requete, Response $reponse): Response
+    {
+        return $this->renduPageEnConstruction($requete, $reponse);
+    }
+
+    /**
+     * Affiche la page du début de la phase 1 du fonctionnement.
+     *
+     * @route /bride-a-nez/fonctionnement/debut-phase1
+     *
+     * @param Request  $requete Requête HTTP entrante
+     * @param Response $reponse Réponse HTTP à retourner
+	 *
+     * @return Response
+     */
+    public function fonctionnementDebutPhase1(Request $requete, Response $reponse): Response
+    {
+        return $this->renduPageEnConstruction($requete, $reponse);
+    }
+
+    /**
+     * Affiche la page de la phase 1 du fonctionnement.
+     *
+     * @route /bride-a-nez/fonctionnement/phase1
+     *
+     * @param Request  $requete Requête HTTP entrante
+     * @param Response $reponse Réponse HTTP à retourner
+	 *
+     * @return Response
+     */
+    public function fonctionnementPhase1(Request $requete, Response $reponse): Response
+    {
+        return $this->renduPageEnConstruction($requete, $reponse);
+    }
+
+    /**
+     * Affiche la page du début de la phase 2 du fonctionnement.
+     *
+     * @route /bride-a-nez/fonctionnement/debut-phase2
+     *
+     * @param Request  $requete Requête HTTP entrante
+     * @param Response $reponse Réponse HTTP à retourner
+	 *
+     * @return Response
+     */
+    public function fonctionnementDebutPhase2(Request $requete, Response $reponse): Response
+    {
+        return $this->renduPageEnConstruction($requete, $reponse);
+    }
+
+    /**
+     * Affiche la page de la phase 2 du fonctionnement.
+     *
+     * @route /bride-a-nez/fonctionnement/phase2
+     *
+     * @param Request  $requete Requête HTTP entrante
+     * @param Response $reponse Réponse HTTP à retourner
+	 *
+     * @return Response
+     */
+    public function fonctionnementPhase2(Request $requete, Response $reponse): Response
+    {
+        return $this->renduPageEnConstruction($requete, $reponse);
+    }
+
+    /**
+     * Affiche la page des caractéristiques de la bride à nez.
+     *
+     * @route /bride-a-nez/caracteristiques
+     *
+     * @param Request  $requete Requête HTTP entrante
+     * @param Response $reponse Réponse HTTP à retourner
+	 *
+     * @return Response
+     */
+    public function caracteristiques(Request $requete, Response $reponse): Response
+    {
+        return $this->renduPageEnConstruction($requete, $reponse);
+    }
+
+    /**
+     * Affiche la page du montage de bridage.
+     *
+     * @route /bride-a-nez/montage-bridage
+     *
+     * @param Request  $requete Requête HTTP entrante
+     * @param Response $reponse Réponse HTTP à retourner
+	 *
+     * @return Response
+     */
+    public function montageBridage(Request $requete, Response $reponse): Response
+    {
+        return $this->renduPageEnConstruction($requete, $reponse);
+    }
+
+    /**
+     * Affiche la page de l'éclaté du montage de la bride à nez.
+     *
+     * @route /bride-a-nez/eclate-montage
+     *
+     * @param Request  $requete Requête HTTP entrante
+     * @param Response $reponse Réponse HTTP à retourner
+	 *
+     * @return Response
+     */
+    public function eclateMontage(Request $requete, Response $reponse): Response
+    {
+        return $this->renduPageEnConstruction($requete, $reponse);
+    }
+
+    /**
+     * Affiche la page des sous-ensembles de la bride à nez.
+     *
+     * @route /bride-a-nez/sous-ensembles
+     *
+     * @param Request  $requete Requête HTTP entrante
+     * @param Response $reponse Réponse HTTP à retourner
+	 *
+     * @return Response
+     */
+    public function sousEnsembles(Request $requete, Response $reponse): Response
+    {
+        return $this->renduPageEnConstruction($requete, $reponse);
+    }
+
+    /**
+     * Affiche la page du sous-ensemble corps de la bride.
+     *
+     * @route /bride-a-nez/SE/corps-bride
+     *
+     * @param Request  $requete Requête HTTP entrante
+     * @param Response $reponse Réponse HTTP à retourner
+	 *
+     * @return Response
+     */
+    public function SEcorpsBride(Request $requete, Response $reponse): Response
+    {
+        return $this->renduPageEnConstruction($requete, $reponse);
+    }
+
+    /**
+     * Affiche la page du sous-ensemble nez de la bride.
+     *
+     * @route /bride-a-nez/SE/nez-bride
+     *
+     * @param Request  $requete Requête HTTP entrante
+     * @param Response $reponse Réponse HTTP à retourner
+	 *
+     * @return Response
+     */
+    public function SEnezBride(Request $requete, Response $reponse): Response
+    {
+        return $this->renduPageEnConstruction($requete, $reponse);
+    }
+
+    /**
+     * Affiche la page du sous-ensemble ensemble piston.
+     *
+     * @route /bride-a-nez/SE/ensemble-piston
+     *
+     * @param Request  $requete Requête HTTP entrante
+     * @param Response $reponse Réponse HTTP à retourner
+	 *
+     * @return Response
+     */
+    public function SEensemblePiston(Request $requete, Response $reponse): Response
+    {
+        return $this->renduPageEnConstruction($requete, $reponse);
+    }
+
+    /**
+     * Affiche la page du sous-ensemble plaquette.
+     *
+     * @route /bride-a-nez/SE/plaquette
+     *
+     * @param Request  $requete Requête HTTP entrante
+     * @param Response $reponse Réponse HTTP à retourner
+	 *
+     * @return Response
+     */
+    public function SEplaquette(Request $requete, Response $reponse): Response
+    {
+        return $this->renduPageEnConstruction($requete, $reponse);
+    }
+
+    /**
+     * Affiche la page du sous-ensemble ressort.
+     *
+     * @route /bride-a-nez/SE/ressort
+     *
+     * @param Request  $requete Requête HTTP entrante
+     * @param Response $reponse Réponse HTTP à retourner
+	 * 
+     * @return Response
+     */
+    public function SEressort(Request $requete, Response $reponse): Response
     {
         return $this->renduPageEnConstruction($requete, $reponse);
     }
