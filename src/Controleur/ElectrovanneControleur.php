d024f026e6d3e5708189eb1983ba54b70b92363e
Christophe HERVI <christophe.hervi@free.fr>
2026-06-07 01:35:51 +0200

    électrovanne

--- src/Controleur/ElectrovanneControleur.php
+++ src/Controleur/ElectrovanneControleur.php
@@ -114,7 +114,7 @@ class ElectrovanneControleur extends SupportControleur
 	 */
 	public function fonctionnement(Request $requete, Response $reponse): Response
 	{
-		return $this->renduPageEnConstruction($requete, $reponse);
+		return $this->renduFonctionnement($reponse);
 	}
 
 	/**
