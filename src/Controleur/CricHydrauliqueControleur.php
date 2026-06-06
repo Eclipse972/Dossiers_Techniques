518e9acbaa4b61e76c416a18019406ab856b345a
Christophe HERVI <christophe.hervi@free.fr>
2026-06-06 15:41:32 +0200

    cric hydraulique

--- src/Controleur/CricHydrauliqueControleur.php
+++ src/Controleur/CricHydrauliqueControleur.php
@@ -7,6 +7,7 @@ namespace DossiersTechniques\Controleur;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Views\Twig;
+use DossiersTechniques\Modele\Nomenclature;
 
 class CricHydrauliqueControleur extends SupportControleur
 {
@@ -69,18 +70,32 @@ class CricHydrauliqueControleur extends SupportControleur
     }
 
     /**
-     * Affiche la page de nomenclature du cric hydraulique.
-     *
-     * @route /cric-hydraulique/nomenclature
-     *
-     * @param Request  $requete Requête HTTP entrante
-     * @param Response $reponse Réponse HTTP à retourner
-     * @return Response
-     */
-    public function nomenclature(Request $requete, Response $reponse): Response
-    {
-        return $this->renduPageEnConstruction($requete, $reponse);
-    }
+	 * Affiche la page de nomenclature du cric hydraulique 2 tonnes.
+	 *
+	 * @route /cric_hydraulique/nomenclature
+	 *
+	 * @param Request  $requete Requête HTTP entrante
+	 * @param Response $reponse Réponse HTTP à retourner
+	 *
+	 * @return Response
+	 */
+	public function nomenclature(Request $requete, Response $reponse): Response
+	{
+		Nomenclature::creer();
+		Nomenclature::ajouterLigne(1,	'Flasque gauche',						1,	'flasque_gauche.EASM');
+		Nomenclature::ajouterLigne(2,	'Flasque droit',						1,	'flasque_droit.EASM');
+		Nomenclature::ajouterLigne(3,	'Roulette directrice assemblée',		2,	'roulette_directrice.EASM');
+		Nomenclature::ajouterLigne(4,	'Roue avant',							2,	'roue_avant.EPRT');
+		Nomenclature::ajouterLigne(5,	'Axe roues avant',						2,	'axe_roues_avant.EPRT');
+		Nomenclature::ajouterLigne(6,	'Axe pivot vérin',						2,	'axe_pivot_verin.EPRT');
+		Nomenclature::ajouterLigne(7,	'Axe pivot bras de levage',				1,	'axe_pivot_bras_levage.EPRT');
+		Nomenclature::ajouterLigne(8,	'Rondelle M12',							6,	'rondelleM12.EPRT');
+		Nomenclature::ajouterLigne(9,	'Écrou hexagonal ISO 4032-M12',			6,	'ecrouM12.EPRT');
+		Nomenclature::ajouterLigne(10,	'Tourillon',							2,	'.EPRT');
+		Nomenclature::ajouterLigne(11,	'Anneau élastique pour arbre 10x1',		2,	'.EPRT');
+		Nomenclature::ajouterLigne(12,	'Unité hydraulique',					1,	'.EPRT');
+		return $this->renduNomenclature($reponse, Nomenclature::preparerVue($this->dossier));
+	}
 
 	/**
 	 * Affiche la page de fonctionnement
