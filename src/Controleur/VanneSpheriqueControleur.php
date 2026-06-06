8a335e98e2790daa140d6116d1b9b13a53e8ea6d
Christophe HERVI <christophe.hervi@free.fr>
2026-06-06 15:45:34 +0200

    vanne sphérique

--- src/Controleur/VanneSpheriqueControleur.php
+++ src/Controleur/VanneSpheriqueControleur.php
@@ -7,6 +7,7 @@ namespace DossiersTechniques\Controleur;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Views\Twig;
+use DossiersTechniques\Modele\Nomenclature;
 
 class VanneSpheriqueControleur extends SupportControleur
 {
@@ -69,18 +70,31 @@ class VanneSpheriqueControleur extends SupportControleur
     }
 
     /**
-     * Affiche la page de nomenclature de la vanne sphérique.
-     *
-     * @route /vanne-spherique/nomenclature
-     *
-     * @param Request  $requete Requête HTTP entrante
-     * @param Response $reponse Réponse HTTP à retourner
-     * @return Response
-     */
-    public function nomenclature(Request $requete, Response $reponse): Response
-    {
-        return $this->renduPageEnConstruction($requete, $reponse);
-    }
+	 * Affiche la page de nomenclature de la vanne Legris.
+	 *
+	 * @route /vanne/nomenclature
+	 *
+	 * @param Request  $requete Requête HTTP entrante
+	 * @param Response $reponse Réponse HTTP à retourner
+	 *
+	 * @return Response
+	 */
+	public function nomenclature(Request $requete, Response $reponse): Response
+	{
+		Nomenclature::creer();
+		Nomenclature::ajouterLigne(1,	'raccord',				1,	'raccord.EPRT');
+		Nomenclature::ajouterLigne(2,	'joint de bague',		1,	'joint2bague.EPRT');
+		Nomenclature::ajouterLigne(3,	'bague',				2,	'bague.EPRT');
+		Nomenclature::ajouterLigne(4,	'bille',				1,	'bille.EPRT');
+		Nomenclature::ajouterLigne(5,	'corps',				1,	'corps.EPRT');
+		Nomenclature::ajouterLigne(6,	'joint entraineur',		1,	'joint_entraineur.EPRT');
+		Nomenclature::ajouterLigne(7,	'entraineur',			1,	'entraineur.EPRT');
+		Nomenclature::ajouterLigne(8,	'guide entraineur',		1,	'guide_entraineur.EPRT');
+		Nomenclature::ajouterLigne(9,	'butée mobile',			1,	'butee_mobile.EPRT');
+		Nomenclature::ajouterLigne(10,	'levier de manœuvre',	1,	'levier.EPRT');
+		Nomenclature::ajouterLigne(11,	'vis',					1,	'vis.EPRT');
+		return $this->renduNomenclature($reponse, Nomenclature::preparerVue($this->dossier));
+	}
 
 	/**
      * Affiche la page de fonctionnement de la vanne sphérique.
