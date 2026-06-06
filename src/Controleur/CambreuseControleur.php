d1b5ce8aa9c636d197785268b280f56ce518795c
Christophe HERVI <christophe.hervi@free.fr>
2026-06-07 01:21:29 +0200

    cambreuse

--- src/Controleur/CambreuseControleur.php
+++ src/Controleur/CambreuseControleur.php
@@ -137,7 +137,7 @@ class CambreuseControleur extends SupportControleur
 	 */
 	public function fonctionnement(Request $requete, Response $reponse): Response
 	{
-		return $this->renduPageEnConstruction($requete, $reponse);
+		return $this->renduFonctionnement($reponse);
 	}
 
 	/**
