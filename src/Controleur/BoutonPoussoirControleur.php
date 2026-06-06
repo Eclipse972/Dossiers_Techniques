9eb95d6762ed4a5de907c3b14709561ba5436ef2
Christophe HERVI <christophe.hervi@free.fr>
2026-06-06 14:42:39 +0200

    nomenclature du bouton poussoir

--- src/Controleur/BoutonPoussoirControleur.php
+++ src/Controleur/BoutonPoussoirControleur.php
@@ -7,6 +7,7 @@ namespace DossiersTechniques\Controleur;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Views\Twig;
+use DossiersTechniques\Modele\Nomenclature;
 
 class BoutonPoussoirControleur extends SupportControleur
 {
@@ -74,18 +75,31 @@ class BoutonPoussoirControleur extends SupportControleur
     }
 
     /**
-     * Affiche la page de nomenclature de l'alternateur.
-     *
-     * @route /alternateur/nomenclature
-     *
-     * @param Request  $requete Requête HTTP entrante
-     * @param Response $reponse Réponse HTTP à retourner
-     * @return Response
-     */
-    public function nomenclature(Request $requete, Response $reponse): Response
-    {
-        return $this->renduPageEnConstruction($requete, $reponse);
-    }
+	 * Affiche la page de nomenclature du bouton poussoir.
+	 *
+	 * @route /bouton-poussoir/nomenclature
+	 *
+	 * @param Request  $requete Requête HTTP entrante
+	 * @param Response $reponse Réponse HTTP à retourner
+	 *
+	 * @return Response
+	 */
+	public function nomenclature(Request $requete, Response $reponse): Response
+	{
+		Nomenclature::creer();
+		Nomenclature::ajouterLigne(1,	'Corps',				1,	'Corps.EPRT',				'PF 21 (Bakélite)');
+		Nomenclature::ajouterLigne(2,	'Borne',				2,	'borne.EPRT',				'Cu Zn 15');
+		Nomenclature::ajouterLigne(3,	'Vis CS M2-4',			4,	'Vis_CS.EPRT',				'Cu Zn 39 Pb 2');
+		Nomenclature::ajouterLigne(4,	'Fourreau',				1,	'Fourreau.EPRT',			'Cu Zn 39 Pb 2');
+		Nomenclature::ajouterLigne(5,	'Écrou H M12-1',		1,	'Ecrou_H.EPRT',				'Cu Zn 15');
+		Nomenclature::ajouterLigne(6,	'Écrou moleté',			1,	'Ecrou_molete.EPRT',		'Cu Zn 15');
+		Nomenclature::ajouterLigne(7,	'Ressort',				1,	'Ressort.EPRT',				'45 Si 8');
+		Nomenclature::ajouterLigne(8,	'Rondelle de contact',	1,	'Rondelle.EPRT',			'Cu Zn 15');
+		Nomenclature::ajouterLigne(9,	'Cylindre de poussée',	1,	'Cylindre.EPRT',			'PF 21 (Bakélite)');
+		Nomenclature::ajouterLigne(10,	'Vis FS M2.5-18',		1,	'Vis_FS.EPRT',				'Cu Zn 39 Pb 2');
+		Nomenclature::ajouterLigne(11,	'Poussoir',				1,	'Poussoir.EPRT',			'S 235 (E24)');
+		return $this->renduNomenclature($reponse, Nomenclature::preparerVue($this->dossier));
+	}
 
 	/**
      * Affiche la page du diagramme pieuvre.
@@ -105,7 +119,7 @@ class BoutonPoussoirControleur extends SupportControleur
      *
      * @param Request  $requete Requête HTTP entrante
      * @param Response $reponse Réponse HTTP à retourner
-	 * 
+	 *
      * @return Response
      */
     public function eclate(Request $requete, Response $reponse): Response
