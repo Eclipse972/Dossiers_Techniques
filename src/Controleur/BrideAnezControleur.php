01e7102f0cba77472aa3d9822cf21d72e0ec4f53
Christophe HERVI <christophe.hervi@free.fr>
2026-06-06 14:45:22 +0200

    nomenclature de la bride à nez

--- src/Controleur/BrideAnezControleur.php
+++ src/Controleur/BrideAnezControleur.php
@@ -7,6 +7,7 @@ namespace DossiersTechniques\Controleur;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Views\Twig;
+use DossiersTechniques\Modele\Nomenclature;
 
 class BrideAnezControleur extends SupportControleur
 {
@@ -70,20 +71,49 @@ class BrideAnezControleur extends SupportControleur
         return $this->renduPageEnConstruction($requete, $reponse);
     }
 
-    /**
-     * Affiche la page de nomenclature de la bride à nez.
-     *
-     * @route /bride-a-nez/nomenclature
-     *
-     * @param Request  $requete Requête HTTP entrante
-     * @param Response $reponse Réponse HTTP à retourner
+/**
+	 * Affiche la page de nomenclature de la bride à nez.
 	 *
-     * @return Response
-     */
-    public function nomenclature(Request $requete, Response $reponse): Response
-    {
-        return $this->renduPageEnConstruction($requete, $reponse);
-    }
+	 * @route /bride-a-nez/nomenclature
+	 *
+	 * @param Request  $requete Requête HTTP entrante
+	 * @param Response $reponse Réponse HTTP à retourner
+	 *
+	 * @return Response
+	 */
+	public function nomenclature(Request $requete, Response $reponse): Response
+	{
+		Nomenclature::creer();
+		Nomenclature::ajouterLigne(1,	'Tête de bride',							1,	'tete2bride.EPRT',			'EN-GJS-600-2');
+		Nomenclature::ajouterLigne(2,	'Corps de vérin',							1,	'corps2verin.EPRT',			'EN-GJS-600-2');
+		Nomenclature::ajouterLigne(3,	'Support lame de ressort',					1,	'support_lame2ressort.EPRT',	'S 235 (E24)');
+		Nomenclature::ajouterLigne(4,	'Vis à tête cylindrique à 6 pans creux M3-6',	3,	'visCHcM3-6.EPRT');
+		Nomenclature::ajouterLigne(5,	'Vis à tête fraisée fendue M4-13',			2,	'visFSM4-13.EPRT');
+		Nomenclature::ajouterLigne(6,	'Vis à tête cylindrique à 6 pans creux M6-20',	4,	'visCHcM6-20.EPRT');
+		Nomenclature::ajouterLigne(7,	'Rondelle Grower W6',						4,	'rondelle_growerW6.EPRT');
+		Nomenclature::ajouterLigne(8,	'Vis sans tête à six pans creux',			1,	'visAHcM10-10.EPRT');
+		Nomenclature::ajouterLigne(9,	'Plaquette avant',							1,	'plaquette_avant.EPRT',		'EN AW 2017');
+		Nomenclature::ajouterLigne(10,	'Vis à tête fraisée fendue M3-8',			4,	'visFSM3-8.EPRT');
+		Nomenclature::ajouterLigne(11,	'Axe',										1,	'axe.EPRT',					'C 55');
+		Nomenclature::ajouterLigne(12,	'Anneau élastique pour alésage 40x1,75',	1,	'anneau_elastique40x1,75.EPRT');
+		Nomenclature::ajouterLigne(13,	'Vis sans tête à six pans creux M6-10',		1,	'visAHcM6-10.EPRT');
+		Nomenclature::ajouterLigne(14,	'Lame de ressort',							1,	'lame2ressort.EPRT',		'55 Cr3');
+		Nomenclature::ajouterLigne(15,	'Nez de bride',								1,	'nez2bride.EPRT',			'E 295');
+		Nomenclature::ajouterLigne(16,	'Piston',									1,	'piston.EPRT');
+		Nomenclature::ajouterLigne(17,	'Segment de guidage de 40',					1,	'segment2guidage40.EPRT',	'téflon');
+		Nomenclature::ajouterLigne(18,	'Bague anti extrusion 34-40',				3,	'bague_anti_extrusion34-40.EPRT','téflon');
+		Nomenclature::ajouterLigne(19,	'Joint torique 35,6x3,6',					2,	'joint_torique35,6x3,6.EPRT');
+		Nomenclature::ajouterLigne(20,	'Douille',									1,	'douille.EPRT');
+		Nomenclature::ajouterLigne(21,	'Joint torique 16,6x2,7',					1,	'joint_torique16,9x2,7.EPRT');
+		Nomenclature::ajouterLigne(22,	'Bague anti-extrusion 18-21',				1,	'bague_anti-extrusion18-21.EPRT','téflon');
+		Nomenclature::ajouterLigne(23,	'Segment de guidage de 18',					1,	'racleur18.EPRT',			'C35');
+		Nomenclature::ajouterLigne(24,	'Goupille élastique 4 x 35',				1,	'goupille4x35.EPRT');
+		Nomenclature::ajouterLigne(25,	'chape',									1,	'chape.EPRT',				'C35');
+		Nomenclature::ajouterLigne(26,	'Axe de chape',								1,	'axe2chape.EPRT',			'C 55');
+		Nomenclature::ajouterLigne(27,	'Plaquette plastique',						1,	'plaquette_plastique.EPRT');
+		Nomenclature::ajouterLigne(28,	'Ressort de compression',					1,	'ressort.EPRT');
+		return $this->renduNomenclature($reponse, Nomenclature::preparerVue($this->dossier));
+	}
 
     /**
      * Affiche la page de fonctionnement de la bride à nez.
@@ -287,7 +317,7 @@ class BrideAnezControleur extends SupportControleur
      *
      * @param Request  $requete Requête HTTP entrante
      * @param Response $reponse Réponse HTTP à retourner
-	 * 
+	 *
      * @return Response
      */
     public function SEressort(Request $requete, Response $reponse): Response
