266b08108a991a7c9f39841f2cd128a25b583ab1
Christophe HERVI <christophe.hervi@free.fr>
2026-06-06 15:44:11 +0200

    pompe à palettes

--- src/Controleur/PompePalettesControleur.php
+++ src/Controleur/PompePalettesControleur.php
@@ -7,6 +7,7 @@ namespace DossiersTechniques\Controleur;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Views\Twig;
+use DossiersTechniques\Modele\Nomenclature;
 
 class PompePalettesControleur extends SupportControleur
 {
@@ -69,18 +70,29 @@ class PompePalettesControleur extends SupportControleur
     }
 
     /**
-     * Affiche la page de nomenclature de la pompe à palettes.
-     *
-     * @route /pompe-a-palettes/nomenclature
-     *
-     * @param Request  $requete Requête HTTP entrante
-     * @param Response $reponse Réponse HTTP à retourner
-     * @return Response
-     */
-    public function nomenclature(Request $requete, Response $reponse): Response
-    {
-        return $this->renduPageEnConstruction($requete, $reponse);
-    }
+	 * Affiche la page de nomenclature de la pompe à palettes.
+	 *
+	 * @route /pompeApalettes/nomenclature
+	 *
+	 * @param Request  $requete Requête HTTP entrante
+	 * @param Response $reponse Réponse HTTP à retourner
+	 *
+	 * @return Response
+	 */
+	public function nomenclature(Request $requete, Response $reponse): Response
+	{
+		Nomenclature::creer();
+		Nomenclature::ajouterLigne(1,	'Corps',									1,	'corps.EPRT');
+		Nomenclature::ajouterLigne(2,	'Coussinet à collerette',					1,	'Coussinet.EPRT');
+		Nomenclature::ajouterLigne(3,	'Arbre',									1,	'arbre.EPRT');
+		Nomenclature::ajouterLigne(4,	'Anneau élastique pour arbre 16x1',			1,	'Anneau_elastique.EPRT');
+		Nomenclature::ajouterLigne(5,	'Embout de tube 3/8e',						2,	'Embout_tube.EPRT');
+		Nomenclature::ajouterLigne(6,	'Palette',									2,	'Palette.EPRT');
+		Nomenclature::ajouterLigne(7,	'Joint torique 50.40x3.53',					1,	'joint_torique.EPRT');
+		Nomenclature::ajouterLigne(8,	'Plaque',									1,	'plaque.EPRT');
+		Nomenclature::ajouterLigne(9,	'Vis ISO 10642-m3X12-8.8',					8,	'Vis.EPRT');
+		return $this->renduNomenclature($reponse, Nomenclature::preparerVue($this->dossier));
+	}
 
 	/**
      * Affiche la page de l'éclaté de la pompe à palettes.
