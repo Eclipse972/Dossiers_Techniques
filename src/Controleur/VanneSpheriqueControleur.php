42521287adc47852af6f03690f1eafbd2533fd8d
Christophe HERVI <christophe.hervi@free.fr>
2026-06-07 01:00:02 +0200

    fonctionnement de la vanne

--- src/Controleur/VanneSpheriqueControleur.php
+++ src/Controleur/VanneSpheriqueControleur.php
@@ -107,7 +107,13 @@ class VanneSpheriqueControleur extends SupportControleur
      */
     public function fonctionnement(Request $requete, Response $reponse): Response
     {
-        return $this->renduPageEnConstruction($requete, $reponse);
+        return $this->vue->render($reponse, '112-pageDT.html.twig', [
+			'support'	=> $this->nom,
+			'du'		=> $this->article_du,
+			'dossier'	=> $this->dossier,
+			'logo'		=> $this->logo,
+			'fichier'	=> 'fonctionnement',
+		]);
     }
 
     /**
