9cf99688eee71715f89824dec0d1c33e1845a888
Christophe HERVI <christophe.hervi@free.fr>
2026-06-06 14:47:43 +0200

    bride hydraulique

--- src/Controleur/BrideHydrauliqueControleur.php
+++ src/Controleur/BrideHydrauliqueControleur.php
@@ -7,6 +7,7 @@ namespace DossiersTechniques\Controleur;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Views\Twig;
+use DossiersTechniques\Modele\Nomenclature;
 
 class BrideHydrauliqueControleur extends SupportControleur
 {
@@ -70,27 +71,38 @@ class BrideHydrauliqueControleur extends SupportControleur
         return $this->renduPageEnConstruction($requete, $reponse);
     }
 
-    /**
-     * Affiche la page de nomenclature de l'alternateur.
-     *
-     * @route /bride-hydraulique/nomenclature
-     *
-     * @param Request  $requete Requête HTTP entrante
-     * @param Response $reponse Réponse HTTP à retourner
+/**
+	 * Affiche la page de nomenclature de la bride hydraulique.
 	 *
-     * @return Response
-     */
-    public function nomenclature(Request $requete, Response $reponse): Response
-    {
-        return $this->renduPageEnConstruction($requete, $reponse);
-    }
+	 * @route /bride-hydraulique/nomenclature
+	 *
+	 * @param Request  $requete Requête HTTP entrante
+	 * @param Response $reponse Réponse HTTP à retourner
+	 *
+	 * @return Response
+	 */
+	public function nomenclature(Request $requete, Response $reponse): Response
+	{
+		Nomenclature::creer();
+		Nomenclature::ajouterLigne(1,	'corps',											1,	'corps.EPRT',			'EN-GJS-600-2');
+		Nomenclature::ajouterLigne(2,	'écrou H M10',										1,	'ecrou_H_M10.EPRT');
+		Nomenclature::ajouterLigne(3,	'vis sans tête fendue à bout téton M10',			1,	'vis_sans_teteM10.EPRT');
+		Nomenclature::ajouterLigne(4,	'levier',											1,	'levier.EPRT',			'S 235 (E24)');
+		Nomenclature::ajouterLigne(5,	'axe de levier',									1,	'axe2levier.EPRT',		'S 235 (E24)');
+		Nomenclature::ajouterLigne(6,	'chape',											1,	'chape.EPRT');
+		Nomenclature::ajouterLigne(7,	'vis de la chape',									1,	'vis-chape.EPRT');
+		Nomenclature::ajouterLigne(8,	'couvercle',										1,	'couvercle.EPRT',		'S 235 (E24)');
+		Nomenclature::ajouterLigne(10,	'piston',											1,	'piston.EPRT',			'S 235 (E24)');
+		Nomenclature::ajouterLigne(11,	'joint torique',									1,	'joint.EPRT');
+		return $this->renduNomenclature($reponse, Nomenclature::preparerVue($this->dossier));
+	}
 
 	/**
      * Affiche la page de fonctionnement de la bride hydraulique.
      *
      * @param Request  $requete Requête HTTP entrante
      * @param Response $reponse Réponse HTTP à retourner
-	 * 
+	 *
      * @return Response
      */
     public function fonctionnement(Request $requete, Response $reponse): Response
