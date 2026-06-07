1c0e62b1a78ee0bd29c3adbb0e0dbc8f6c7097dc
Christophe HERVI <christophe.hervi@free.fr>
2026-06-07 21:21:47 +0200

    éclaté de l'alternateur sans fichier associé

--- src/Controleur/AlternateurControleur.php
+++ src/Controleur/AlternateurControleur.php
@@ -92,7 +92,13 @@ class AlternateurControleur extends SupportControleur
 	 */
 	public function eclate(Request $requete, Response $reponse): Response
 	{
-		return $this->renduPageEnConstruction($requete, $reponse);
+		return $this->vue->render($reponse, '112-pageDT.html.twig', [
+			'support'	=> $this->nom,
+			'du'		=> $this->article_du,
+			'dossier'	=> $this->dossier,
+			'logo'		=> $this->logo,
+			'fichier'	=> 'eclate',
+		]);
 	}
 
 	/**
