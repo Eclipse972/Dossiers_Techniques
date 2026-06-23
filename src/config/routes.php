a8f7ae2c21fb0171bd8d04af681b3d9a3465caea
Christophe HERVI <christophe.hervi@free.fr>
2026-06-23 16:38:39 +0200

    suppression embase/corps

--- src/config/routes.php
+++ src/config/routes.php
@@ -311,7 +311,6 @@ $app->group('/unite-de-marquage', function($group) {
     $group->get('',										[UniteMarquageControleur::class, 'aPropos']);
     $group->get('/mise-en-situation',					[UniteMarquageControleur::class, 'miseEnSituation']);
     $group->get('/element',								[UniteMarquageControleur::class, 'elements']);
-    $group->get('/element/corps',						[UniteMarquageControleur::class, 'elementsCorps']);
     $group->get('/fonctionnement',						[UniteMarquageControleur::class, 'fonctionnement']);
     $group->get('/caracteristiques',					[UniteMarquageControleur::class, 'caracteristiques']);
     $group->get('/dessin-densemble',					[UniteMarquageControleur::class, 'dessinDensemble']);
