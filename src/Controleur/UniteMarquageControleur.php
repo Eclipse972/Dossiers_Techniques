4ce1c46f88474ec1284bb1ff7ce55b32a5c894d8
Christophe HERVI <christophe.hervi@free.fr>
2026-06-23 14:55:37 +0200

    ajout du levier dans la nomenclature de l'unité de marquage

--- src/Controleur/UniteMarquageControleur.php
+++ src/Controleur/UniteMarquageControleur.php
@@ -114,7 +114,7 @@ class UniteMarquageControleur extends SupportControleur
 		Nomenclature::ajouterLigne(25,	'Axe levier',					1,	'axe-levier.EPRT');
 		Nomenclature::ajouterLigne(26,	'Coussinet XFM 3034 26',		2,	'coussinet-XFM-3034-26.EPRT');
 		Nomenclature::ajouterLigne(27,	'Rondelle de butées AS 3047',	2,	'rondelle-AS-3047.EPRT');
-	#	Nomenclature::ajouterLigne(28,	'Levier',						1,	'levier.EPRT');conflit entre l pièce et l'ensemble
+		Nomenclature::ajouterLigne(28,	'Levier',						1,	'levier.EPRT');
 		Nomenclature::ajouterLigne(29,	'Vis CHc M8 x 20',				2,	'vis-CHc-M8x20.EPRT');
 		Nomenclature::ajouterLigne(30,	'Axe biellette-levier',			1,	'axe-biellette-levier.EPRT');
 		Nomenclature::ajouterLigne(31,	'Biellette',					2,	'biellette.EPRT');
